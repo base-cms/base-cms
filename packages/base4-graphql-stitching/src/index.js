7f3e5139c4e74ae901d592299e7aaebbec3cf2db
Jacob Bare <jacob@limit0.io>
2018-10-10 11:07:13 -0500

    Add root exports

--- packages/base4-graphql-stitching/src/index.js
+++ packages/base4-graphql-stitching/src/index.js
@@ -1,1 +1,10 @@
 require('./env');
+const client = require('./client');
+const link = require('./link');
+const schema = require('./schema');
+
+module.exports = {
+  client,
+  link,
+  schema,
+};
