b69696d4aa79d216ca4e90b29bbb0ceb84c1b2bb
Jacob Bare <jacob@limit0.io>
2018-10-10 12:02:58 -0500

    Create router middleware

---
+++ packages/base4-graphql-express-router/src/index.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { createSchema } = require('@base-cms/base4-graphql-stitching');
+
+const { NODE_ENV } = process.env;
+const isProduction = NODE_ENV === 'production';
+
+const router = Router();
+
+const create = async ({ endpoint, serverOpts } = {}) => {
+  if (!endpoint) throw new Error('The mounted GraphQL endpoint must be provided.');
+  const schema = await createSchema();
+  const playground = isProduction ? { endpoint } : false;
+  const server = new ApolloServer({ schema, playground, ...serverOpts });
+  server.applyMiddleware({ app: router, path: '/' });
+};
+
+module.exports = (opts = {
+  endpoint: '/graphql',
+  serverOpts: {},
+}) => {
+  create(opts).catch(e => setImmediate(() => { throw e; }));
+  return router;
+};
