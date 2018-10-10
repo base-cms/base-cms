1254c8752c2ac7a888e402cff40c93d6e14c7535
Jacob Bare <jacob@limit0.io>
2018-10-10 11:00:34 -0500

    Add env file

---
+++ packages/base4-graphql-stitching/src/env.js
@@ -0,0 +1,24 @@
+const { isURL } = require('validator');
+const { cleanEnv, makeValidator } = require('envalid');
+
+const url = makeValidator((v) => {
+  const opts = { protocols: ['http', 'https'], require_tld: false, require_protocol: true };
+  if (isURL(v, opts)) return v;
+  throw new Error('Expected a valid URL with http or https');
+});
+
+const nonemptystr = makeValidator((v) => {
+  const err = new Error('Expected a non-empty string');
+  if (v === undefined || v === null || v === '') {
+    throw err;
+  }
+  const trimmed = String(v).trim();
+  if (!trimmed) throw err;
+  return trimmed;
+});
+
+module.exports = cleanEnv(process.env, {
+  BASE4_GRAPHQL_URL: url({ desc: 'The Base4 GraphQL URL for stitching API requests.' }),
+  BASE4_TENANT_KEY: nonemptystr({ desc: 'The Base4 tenant key to connect to.' }),
+  BASE4_API_TOKEN: nonemptystr({ desc: 'The Base4 GraphQL API token.' }),
+});
