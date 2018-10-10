5a0b8259e3f0851fda03954303b3d77cb5b77189
Jacob Bare <jacob@limit0.io>
2018-10-10 14:40:04 -0500

    Add ability fragment types script and build

---
+++ packages/base4-graphql-stitching/build-fragment-types.js
@@ -0,0 +1,41 @@
+const fetch = require('isomorphic-unfetch');
+const fs = require('fs');
+const env = require('./src/env');
+
+const { BASE4_GRAPHQL_URL, BASE4_TENANT_KEY, BASE4_API_TOKEN } = env;
+
+// @see https://www.apollographql.com/docs/react/advanced/fragments.html
+
+fetch(BASE4_GRAPHQL_URL, {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+    authorization: `Bearer ${BASE4_API_TOKEN}`,
+    'x-tenant-key': BASE4_TENANT_KEY,
+  },
+  body: JSON.stringify({
+    query: `
+      {
+        __schema {
+          types {
+            kind
+            name
+            possibleTypes {
+              name
+            }
+          }
+        }
+      }
+    `,
+  }),
+}).then(result => result.json()).then(result => {
+  // here we're filtering out any type information unrelated to unions or interfaces
+  const filteredData = result.data.__schema.types.filter(
+    type => type.possibleTypes !== null,
+  );
+  result.data.__schema.types = filteredData;
+  fs.writeFile('./fragment-types.json', JSON.stringify(result.data), err => {
+    if (err) console.error('Error writing fragmentTypes file', err);
+    console.log('Fragment types successfully extracted!');
+  });
+});
