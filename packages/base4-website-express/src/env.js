01d8fd1d25bece513c5e8a56a02e5fb0d742c425
Jacob Bare <jacob@limit0.io>
2018-10-10 10:32:34 -0500

    Make graph router a function and config endpoint

--- packages/base4-website-express/src/env.js
+++ packages/base4-website-express/src/env.js
@@ -1,10 +1,5 @@
 const { isURL } = require('validator');
-
-const {
-  port,
-  cleanEnv,
-  makeValidator,
-} = require('envalid');
+const { cleanEnv, makeValidator } = require('envalid');
 
 const url = makeValidator((v) => {
   const opts = { protocols: ['http', 'https'], require_tld: false, require_protocol: true };
@@ -23,7 +18,7 @@ const nonemptystr = makeValidator((v) => {
 });
 
 module.exports = cleanEnv(process.env, {
-  PORT: port({ desc: 'The port that the Express server will run on.', default: 3005 }),
+  LOCAL_GRAPHQL_ENDPOINT: nonemptystr({ desc: 'The local GraphQL path. Default is /graphql', default: '/graphql' }),
   BASE4_GRAPHQL_URL: url({ desc: 'The Base4 GraphQL URL for stitching API requests.' }),
   BASE4_TENANT_KEY: nonemptystr({ desc: 'The Base4 tenant key to connect to.' }),
   BASE4_API_TOKEN: nonemptystr({ desc: 'The Base4 GraphQL API token.' }),
--- packages/base4-website-express/src/graphql/router.js
+++ packages/base4-website-express/src/graphql/router.js
@@ -1,19 +1,18 @@
 const { Router } = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const createSchema = require('./schema');
-const { isProduction } = require('../env');
+const { isProduction, LOCAL_GRAPHQL_ENDPOINT } = require('../env');
 
 const router = Router();
 
-const create = async () => {
+const create = async (opts) => {
   const schema = await createSchema();
-  const playground = isProduction ? { endpoint: '/graphql' } : false;
-  const server = new ApolloServer({ schema, playground });
+  const playground = isProduction ? { endpoint: LOCAL_GRAPHQL_ENDPOINT } : false;
+  const server = new ApolloServer({ schema, playground, ...opts });
   server.applyMiddleware({ app: router, path: '/' });
 };
 
-create().catch(e => setImmediate(() => {
-  throw e;
-}));
-
-module.exports = router;
+module.exports = (opts = {}) => {
+  create(opts).catch(e => setImmediate(() => { throw e; }));
+  return router;
+};
