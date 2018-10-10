480c98986c55f36e3bd6fbef821fc624d1a994ca
Jacob Bare <jacob@limit0.io>
2018-10-10 10:33:23 -0500

    Setup root exports and express+graphql

--- packages/base4-website-express/src/index.js
+++ packages/base4-website-express/src/index.js
@@ -1,1 +1,16 @@
-require('./env');
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const { client: graphql, router } = require('./graphql');
+const env = require('./env');
+
+const { LOCAL_GRAPHQL_ENDPOINT } = env;
+
+module.exports = {
+  env,
+  graphql,
+  express: (opts = {}) => express()
+    .use(helmet(opts.helmet))
+    .use(compression(opts.compression))
+    .use(LOCAL_GRAPHQL_ENDPOINT, router(opts.graphql)),
+};
