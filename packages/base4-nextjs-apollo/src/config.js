4cfcd631e437d1f749720f701673ca016e817de5
Jacob Bare <jacob@limit0.io>
2018-10-11 15:35:54 -0500

    Create apollo client config function

---
+++ packages/base4-nextjs-apollo/src/config.js
@@ -0,0 +1,27 @@
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import fetch from 'isomorphic-unfetch';
+
+const { log } = console;
+
+export default (req) => {
+  const headers = req ? req.headers : {};
+  const uri = (req) ? `${req.protocol}://${req.get('host')}` : '';
+
+  return {
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.map(({ message, locations, path }) => log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
+        }
+        if (networkError) log(`[Network error]: ${networkError}`);
+      }),
+      new HttpLink({
+        uri: `${uri}/graphql`,
+        headers,
+        fetch,
+      }),
+    ]),
+  };
+};
