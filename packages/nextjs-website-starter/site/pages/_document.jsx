aa354606fab5c9257fbcb6daa889b59babda33e6
Jacob Bare <jacob@limit0.io>
2018-10-15 08:05:02 -0500

    Create root _document page

---
+++ packages/nextjs-website-starter/site/pages/_document.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
