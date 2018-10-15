ec1d4c6bdb8a6cf70418030552b77c01bbc6d64f
Jacob Bare <jacob@limit0.io>
2018-10-15 10:58:08 -0500

    Rework rollup config to exclude deps + peers

--- packages/base4-nextjs-apollo/rollup.config.js
+++ packages/base4-nextjs-apollo/rollup.config.js
@@ -3,42 +3,28 @@ import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
 import commonjs from 'rollup-plugin-commonjs';
 
+const pkg = require('./package.json');
+const peerDependencies = Object.keys(pkg.peerDependencies);
+peerDependencies.push('next/head');
+const dependencies = Object.keys(pkg.dependencies);
+
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: 'dist/index.module.js',
-      format: 'es',
+      file: 'dist/index.esm.js',
+      format: 'esm',
     },
   ],
-  external: [
-    'graphql',
-    'react',
-    'react-dom',
-    'react-apollo',
-    'next/head',
-    'isomorphic-unfetch',
-  ],
+  external: peerDependencies.concat(dependencies),
   plugins: [
-    resolve({
-      main: true,
-      module: false,
-    }),
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {
-        'node_modules/apollo-cache-inmemory/lib/bundle.umd.js': ['InMemoryCache', 'IntrospectionFragmentMatcher'],
-        'node_modules/apollo-client/bundle.umd.js': ['ApolloClient'],
-        'node_modules/apollo-link-error/lib/bundle.umd.js': ['onError'],
-        'node_modules/apollo-link-http/lib/bundle.umd.js': ['HttpLink'],
-        'node_modules/apollo-link/lib/bundle.umd.js': ['ApolloLink'],
-      },
-    }),
+    resolve(),
+    commonjs({ include: 'node_modules/**' }),
     babel({ exclude: 'node_modules/**' }),
     json({
       include: 'node_modules/@base-cms/base4-graphql-stitching/fragment-types.json',
