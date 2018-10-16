dba523e6db5c64730150aef147c9fd4cd169de90
Jacob Bare <jacob@limit0.io>
2018-10-16 13:03:16 -0500

    Add initial content query

--- packages/base-content-index-tool/src/import/run.js
+++ packages/base-content-index-tool/src/import/run.js
@@ -1,6 +1,18 @@
-// const base4 = require('../base4');
+const base4 = require('../base4');
+
 const { log } = console;
 
 module.exports = async ({ batchSize }) => {
   log(`Run ${batchSize}`);
+  const coll = await base4.collectionFor('platform.Content');
+  const cursor = await coll.find({}, {
+    projection: {
+      name: 1,
+      teaser: 1,
+      body: 1,
+      taxonomy: 1,
+    },
+  }).limit(5);
+  const docs = await cursor.toArray();
+  log(docs.length);
 };
