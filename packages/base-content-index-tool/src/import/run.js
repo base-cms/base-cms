10a4ab901b34d3d43750f3f559d3e89ce1ce904c
Jacob Bare <jacob@limit0.io>
2018-10-16 13:25:06 -0500

    Use data loader in import runner

--- packages/base-content-index-tool/src/import/run.js
+++ packages/base-content-index-tool/src/import/run.js
@@ -1,18 +1,28 @@
 const base4 = require('../base4');
+const Base4 = require('../base4/client');
+const { taxonomyLoader } = require('../base4/loaders');
+const { eachPromise } = require('../utils/async');
 
 const { log } = console;
 
 module.exports = async ({ batchSize }) => {
   log(`Run ${batchSize}`);
-  const coll = await base4.collectionFor('platform.Content');
-  const cursor = await coll.find({}, {
+  const docs = await base4.find('platform.Content', { taxonomy: { $exists: true } }, {
     projection: {
       name: 1,
       teaser: 1,
       body: 1,
       taxonomy: 1,
     },
-  }).limit(5);
-  const docs = await cursor.toArray();
-  log(docs.length);
+    limit: 5,
+  });
+  log(docs.map(d => d._id));
+
+  await eachPromise(docs, async ({ taxonomy }) => {
+    const ids = Base4.extractRefIds(taxonomy);
+    if (ids.length) {
+      const taxonomyDocs = await taxonomyLoader.loadMany(ids);
+      log(taxonomyDocs.length);
+    }
+  });
 };
