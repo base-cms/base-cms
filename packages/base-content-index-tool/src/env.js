2985e65f560a6f7c70be31eb50722305df1bd5f8
Jacob Bare <jacob@limit0.io>
2018-10-16 13:02:23 -0500

    Add MONGO_LOGGING and TENANT_KEY

--- packages/base-content-index-tool/src/env.js
+++ packages/base-content-index-tool/src/env.js
@@ -1,5 +1,6 @@
 const { isURL } = require('validator');
 const {
+  bool,
   cleanEnv,
   makeValidator,
 } = require('envalid');
@@ -10,6 +11,18 @@ const mongodsn = makeValidator((v) => {
   throw new Error('Expected a Mongo DSN string startng with mongodb://');
 });
 
+const nonemptystr = makeValidator((v) => {
+  const err = new Error('Expected a non-empty string');
+  if (v === undefined || v === null || v === '') {
+    throw err;
+  }
+  const trimmed = String(v).trim();
+  if (!trimmed) throw err;
+  return trimmed;
+});
+
 module.exports = cleanEnv(process.env, {
   MONGO_DSN: mongodsn({ desc: 'The MongoDB DSN to connect to.' }),
+  MONGO_LOGGING: bool({ desc: 'Whether to enable Mongo query logging to the terminal.', default: false }),
+  TENANT_KEY: nonemptystr({ desc: 'The Base4 tenant to connect import from, e.g. cygnus_ofcr' }),
 });
