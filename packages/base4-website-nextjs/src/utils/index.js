a4557c2d5d0528deef93e12189d4d8223ba64ab2
Jacob Bare <jacob@limit0.io>
2018-10-18 08:24:22 -0500

    Move utils to root

--- packages/base4-website-nextjs/src/utils/index.js
+++ packages/base4-website-nextjs/src/utils/index.js
@@ -2,7 +2,6 @@ import cleanPath from './clean-path';
 import componentDisplayName from './component-display-name';
 import createMarkup from './create-markup';
 import httpErrors from './http-errors';
-import redirect from './redirect';
 import sectionPath from './section-path';
 
 export {
@@ -10,6 +9,5 @@ export {
   componentDisplayName,
   createMarkup,
   httpErrors,
-  redirect,
   sectionPath,
 };
