f5ea1e7e9c5f01d2388d7b4ff49374497b172df7
Jacob Bare <jacob@limit0.io>
2018-10-18 10:14:54 -0500

    Remove comments

--- packages/base4-website-nextjs/routing.js
+++ packages/base4-website-nextjs/routing.js
@@ -1,13 +1,3 @@
 const routing = require('./dist/cjs/routing');
 
 module.exports = routing;
-
-// import { routes, redirect } from 'nextjs/routing';
-// const server = require('nextjs/server');
-
-// import { cleanPath } from 'nextjs/utils';
-
-// import { WebsiteApp, ContentPage } from 'nextjs/pages';
-// import { withWebsiteContent } from 'nextjs/hoc';
-
-// import { RelCanonical } from 'nextjs/components';
