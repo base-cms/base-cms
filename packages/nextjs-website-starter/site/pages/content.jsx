1c8c2152a3d6d4fae44ab5f8b918658658a22f2f
Jacob Bare <jacob@limit0.io>
2018-10-19 11:08:52 -0500

    Use FieldValue component on content page

--- packages/nextjs-website-starter/site/pages/content.jsx
+++ packages/nextjs-website-starter/site/pages/content.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
 import { withPlatformContent } from '@base-cms/base4-website-nextjs/hoc';
-import { ContentField } from '@base-cms/base4-website-nextjs/components';
+import { ContentFieldValue as FieldValue } from '@base-cms/base4-website-nextjs/components';
 
 const ContentPage = ({ content }) => (
   <article data-id={content.id} className={classNames('content', 'content--display', `content--${content.type}`)}>
-    <ContentField tag="h1" fieldName="name">
-      {content.name}
-    </ContentField>
-    <ContentField tag="h3" fieldName="teaser">
-      {content.teaser}
-    </ContentField>
-    <hr />
-    <ContentField tag="div" fieldName="body" asHTML>
-      {content.body}
-    </ContentField>
+    <FieldValue prop="name" tag="h1" data={content} className="my-class" />
+    <FieldValue prop="teaser" tag="h3" data={content} />
+    <FieldValue prop="body" tag="div" data={content} asHTML />
   </article>
 );
 
