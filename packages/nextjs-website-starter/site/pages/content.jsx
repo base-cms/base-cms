e83a2417d96537225c705458ecb18f0037e57d4b
Jacob Bare <jacob@limit0.io>
2018-10-19 09:26:18 -0500

    Use content fields on content example

--- packages/nextjs-website-starter/site/pages/content.jsx
+++ packages/nextjs-website-starter/site/pages/content.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import classNames from 'classnames';
 import { withPlatformContent } from '@base-cms/base4-website-nextjs/hoc';
+import { ContentField } from '@base-cms/base4-website-nextjs/components';
 
 const ContentPage = ({ content }) => (
-  <>
-    <h1>Default Content Page</h1>
-    <h2>{content.name}</h2>
-  </>
+  <article data-id={content.id} className={classNames('content', 'content--display', `content--${content.type}`)}>
+    <ContentField tag="h1" fieldName="name">
+      {content.name}
+    </ContentField>
+    <ContentField tag="h3" fieldName="teaser">
+      {content.teaser}
+    </ContentField>
+    <hr />
+    <ContentField tag="div" fieldName="body" asHTML>
+      {content.body}
+    </ContentField>
+  </article>
 );
 
 export default withPlatformContent(ContentPage);
