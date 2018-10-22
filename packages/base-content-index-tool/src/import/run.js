e708865b4afd7599c25623139bf42df6d9a168e7
Jacob Bare <jacob@limit0.io>
2018-10-22 13:33:11 -0500

    Update index to include taxonomy and bool fields

--- packages/base-content-index-tool/src/import/run.js
+++ packages/base-content-index-tool/src/import/run.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const base4 = require('../base4');
+const Base4 = require('../base4/client');
 const elastic = require('../elastic');
+const { taxonomyLoader } = require('../base4/loaders');
 const { whilstPromise } = require('../utils/async');
 const { filter, analyzer } = require('../elastic/index-settings');
 
@@ -37,10 +39,23 @@ module.exports = async ({ batchSize }) => {
     // Create mappings.
     await elastic.putMapping(index, type, {
       properties: {
-        name: { type: 'text' },
-        body: { type: 'text' },
-        teaser: { type: 'text' },
-        taxonomy: { type: 'text' },
+        name: {
+          type: 'text',
+          fields: { boolsim: { type: 'text', similarity: 'boolean' } }
+        },
+        type: { type: 'keyword' },
+        body: {
+          type: 'text',
+          fields: { boolsim: { type: 'text', similarity: 'boolean' } }
+        },
+        teaser: {
+          type: 'text',
+          fields: { boolsim: { type: 'text', similarity: 'boolean' } }
+        },
+        taxonomy: {
+          type: 'text',
+          fields: { boolsim: { type: 'text', similarity: 'boolean' } }
+        },
       },
     });
     log(chalk`{gray Index mappings created.}`);
@@ -53,12 +68,19 @@ module.exports = async ({ batchSize }) => {
   const projection = {
     name: 1,
     deck: 1,
+    teaser: 1,
     body: 1,
     taxonomy: 1,
+    type: 1,
   };
 
   const count = await base4.count('platform.Content', criteria);
   log(chalk`{gray Found} {white ${count}} {gray documents in MongoDB}`);
+
+  log(chalk`{gray Prime the Taxonomy loader...}`);
+  const taxDocs = await base4.find('platform.Taxonomy', {}, {}, true);
+  taxDocs.map(taxDoc => taxonomyLoader.prime(taxDoc._id, taxDoc));
+  log(chalk`{gray Priming complete.}`);
 
   let offset = 0;
   let hasMore = count > offset;
@@ -76,22 +98,28 @@ module.exports = async ({ batchSize }) => {
     log(chalk`{gray Retrieved} {white ${length}} {gray documents from MongoDB.}`);
 
     let firstDoc;
-    await new Promise((resolve, reject) => {
-      cursor.forEach((doc) => {
-        if (!firstDoc) {
-          firstDoc = doc;
-          log(chalk`{gray Beginning bulk body creation at ID} {white ${doc._id}}`);
-        }
-        bulkBody.push({ index: { _index: index, _type: type, _id: doc._id } });
-        bulkBody.push({ name: doc.name || '', body: doc.body || '' });
-      }, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
+    while (await cursor.hasNext()) {
+      const doc = await cursor.next();
+      if (!firstDoc) {
+        firstDoc = doc;
+        log(chalk`{gray Beginning bulk body creation at ID} {white ${doc._id}}`);
+      }
+      const taxonomyIds = Base4.extractRefIds(doc.taxonomy);
+      let taxonomy;
+      if (taxonomyIds.length) {
+        const taxDocs = await taxonomyLoader.loadMany(taxonomyIds);
+        taxonomy = taxDocs.map(t => t.name.replace('-', '').replace(/\W/g, ' ')).join(', ');
+      }
+
+      bulkBody.push({ index: { _index: index, _type: type, _id: doc._id } });
+      bulkBody.push({
+        name: doc.name || undefined,
+        body: doc.body || undefined,
+        teaser: doc.teaser || doc.deck || undefined,
+        type: doc.type || undefined,
+        taxonomy,
       });
-    });
+    }
 
     await cursor.close();
 
