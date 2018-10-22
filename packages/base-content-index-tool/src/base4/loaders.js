c0fdff9b1d1f952cec5646603a29a5a4d44c7835
Jacob Bare <jacob@limit0.io>
2018-10-22 13:32:51 -0500

    Ensure loader returns an array

--- packages/base-content-index-tool/src/base4/loaders.js
+++ packages/base-content-index-tool/src/base4/loaders.js
@@ -3,7 +3,7 @@ const base4 = require('./index');
 
 module.exports = {
   taxonomyLoader: new DataLoader(
-    keys => base4.find('platform.Taxonomy', { _id: { $in: keys } }),
+    keys => base4.find('platform.Taxonomy', { _id: { $in: keys } }, {}, true),
     { cacheKeyFn: key => key.valueOf() },
   ),
 };
