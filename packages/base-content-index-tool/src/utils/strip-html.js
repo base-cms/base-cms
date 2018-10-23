53718e10e2c8b96c000098c79f275358f85e4c0a
Jacob Bare <jacob@limit0.io>
2018-10-23 11:03:01 -0500

    Return empty string when no html

--- packages/base-content-index-tool/src/utils/strip-html.js
+++ packages/base-content-index-tool/src/utils/strip-html.js
@@ -1,6 +1,7 @@
 const h2p = require('html2plaintext');
 
 module.exports = (html) => {
-  const stripped = h2p(!html ? '' : String(html));
+  if (!html) return '';
+  const stripped = h2p(String(html));
   return stripped.replace(/[\r\n\f\v\t]/g, ' ').replace(/\s+/g, ' ').trim();
 };
