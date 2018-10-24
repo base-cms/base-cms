04cf6c2897040280c17835e37c4625df55ca16a2
Jacob Bare <jacob@limit0.io>
2018-10-24 09:21:23 -0500

    Add support for overriding title

--- packages/base4-website-nextjs/src/components/PageTitle.jsx
+++ packages/base4-website-nextjs/src/components/PageTitle.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { isFunction as isFn } from '../utils';
 
-const PageTitle = ({ value, siteName, concateWith }) => {
+const propTypes = {
+  children: PropTypes.func,
+  concateWith: PropTypes.string,
+  siteName: PropTypes.string,
+  value: PropTypes.string.isRequired,
+};
+
+const defaultProps = {
+  children: undefined,
+  concateWith: '|',
+  siteName: null,
+};
+
+const PageTitle = ({
+  children: render,
+  concateWith,
+  siteName,
+  value,
+}) => {
   const title = siteName ? `${value} ${concateWith} ${siteName}` : value;
   return (
     <Head>
-      <title>{title}</title>
+      <title>{isFn(render) ? render() : title}</title>
     </Head>
   );
 };
 
-PageTitle.propTypes = {
-  value: PropTypes.string.isRequired,
-  siteName: PropTypes.string,
-  concateWith: PropTypes.string,
-};
-
-PageTitle.defaultProps = {
-  siteName: null,
-  concateWith: '|',
-};
+PageTitle.propTypes = propTypes;
+PageTitle.defaultProps = defaultProps;
 
 export default PageTitle;
