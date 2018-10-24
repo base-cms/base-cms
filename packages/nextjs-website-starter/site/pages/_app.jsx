9a1c60b53c69f667d40e813f52c27ff97e87b8fd
Jacob Bare <jacob@limit0.io>
2018-10-24 15:10:03 -0500

    Move core _app page to starter

--- packages/nextjs-website-starter/site/pages/_app.jsx
+++ packages/nextjs-website-starter/site/pages/_app.jsx
@@ -1,6 +1,23 @@
-import { WebsiteApp } from '@base-cms/base4-website-nextjs/pages';
+import App, { Container } from 'next/app';
+import React from 'react';
+import withApollo from '@base-cms/base4-nextjs-apollo';
+import { ApolloProvider } from 'react-apollo';
+import { RoutingContext } from '@base-cms/base4-website-nextjs/routing';
+import routeDefs from '../routes.json';
 
-class Site extends WebsiteApp {
+class Website extends App {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+    return (
+      <Container>
+        <RoutingContext.Provider value={routeDefs}>
+          <ApolloProvider client={apollo}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </RoutingContext.Provider>
+      </Container>
+    );
+  }
 }
 
-export default Site;
+export default withApollo(Website);
