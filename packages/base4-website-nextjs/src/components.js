e72d5bc3c0a77ad1712ec401b686b8b948407bef
Jacob Bare <jacob@limit0.io>
2018-10-25 15:27:39 -0500

    Export additional content components

--- packages/base4-website-nextjs/src/components.js
+++ packages/base4-website-nextjs/src/components.js
@@ -1,5 +1,9 @@
 import ContentDateFieldValue from './components/Content/DateFieldValue';
 import ContentFieldValue from './components/Content/FieldValue';
+import ContentLink from './components/Content/Link';
+import ContentLinkShortName from './components/Content/Link/ShortName';
+import ContentRow from './components/Content/Row';
+import ContentShortName from './components/Content/ShortName';
 import Element from './components/Element';
 import FieldValue from './components/FieldValue';
 import FormatDate from './components/FormatDate';
@@ -12,6 +16,10 @@ import RelCanonical from './components/Head/RelCanonical';
 export {
   ContentDateFieldValue,
   ContentFieldValue,
+  ContentLink,
+  ContentLinkShortName,
+  ContentRow,
+  ContentShortName,
   Element,
   FieldValue,
   FormatDate,
