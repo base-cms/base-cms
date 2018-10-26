40ccaac6679e019d5c54d3d615b2ab88f14a25a4
Jacob Bare <jacob@limit0.io>
2018-10-26 08:43:41 -0500

    Add prefix support

--- packages/base4-website-nextjs/src/components/content/Elements/PublishedDate.jsx
+++ packages/base4-website-nextjs/src/components/content/Elements/PublishedDate.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DateFieldValue from './DateFieldValue';
+import { isFunction as isFn } from '../../../utils';
 
 const propTypes = {
+  children: PropTypes.func,
   collapsible: PropTypes.bool,
   content: PropTypes.shape({
     published: PropTypes.string,
   }),
   format: PropTypes.string,
+  prefix: PropTypes.string,
   tag: PropTypes.string,
 };
 
 const defaultProps = {
+  children: undefined,
   collapsible: true,
   content: {},
   format: 'MMM Do, YYYY',
+  prefix: '',
   tag: 'div',
 };
 
-const ContentPublishedDate = ({ content, ...rest }) => (
-  <DateFieldValue path="published" data={content} {...rest} />
+const ContentPublishedDate = ({
+  content,
+  children,
+  prefix,
+  ...rest
+}) => (
+  <DateFieldValue path="published" data={content} {...rest}>
+    {(value) => {
+      const formatted = prefix ? `${prefix} ${value}` : value;
+      if (isFn(children)) return children(formatted);
+      return formatted;
+    }}
+  </DateFieldValue>
 );
 
 ContentPublishedDate.propTypes = propTypes;
