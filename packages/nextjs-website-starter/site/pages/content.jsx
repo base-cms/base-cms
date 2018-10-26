8805cb90f4e9a1d7e283dc23fd605db39deb3503
Jacob Bare <jacob@limit0.io>
2018-10-26 11:58:44 -0500

    Use content elements

--- packages/nextjs-website-starter/site/pages/content.jsx
+++ packages/nextjs-website-starter/site/pages/content.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
 import gql from 'graphql-tag';
 import { withPlatformContent } from '@base-cms/base4-website-nextjs/hoc';
-import { LinkElement } from '@base-cms/base4-website-nextjs/components/core';
 import {
   Body,
-  FieldValue,
   Name,
+  PrimarySectionNameLink,
   PublishedDate,
-  ShortNameLink,
   Row,
   Teaser,
   Type,
@@ -25,22 +22,16 @@ const fragment = gql`
 const ContentPage = ({ content }) => (
   <Wrapper content={content}>
     <Name content={content} />
-    <ShortNameLink content={content} />
     <Teaser tag="h3" content={content} />
-    <FieldValue path="primarySection.name" tag="h4" data={content}>
-      {(value) => {
-        // With a generic link element.
-        const to = `/section/${content.primarySection.alias}`;
-        return <LinkElement className="section__link" to={to} value={`<em>${value}</em>`} asHTML />;
-      }}
-    </FieldValue>
     <hr />
     <Row>
-      <Type content={content}>
-        {(value) => <>Type: {value}</>}
-      </Type>
+      <PrimarySectionNameLink tag="span" content={content}>
+        {(value) => <strong>{value}</strong>}
+      </PrimarySectionNameLink>
       {' | '}
-      <PublishedDate prefix="Published: " content={content} />
+      <Type content={content} />
+      {' | '}
+      <PublishedDate prefix="Published " content={content} />
     </Row>
     <hr />
     <Body content={content} />
