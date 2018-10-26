90323984a5c566c58dc45102ccc2b661d2fc3e92
Jacob Bare <jacob@limit0.io>
2018-10-26 09:22:34 -0500

    Ensure class names are dasherized

--- packages/base4-website-nextjs/src/hoc/withModelFieldClass.jsx
+++ packages/base4-website-nextjs/src/hoc/withModelFieldClass.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { underscore, dasherize } from 'inflected';
 import { componentDisplayName } from '../utils';
+
+const inflect = value => dasherize(underscore(value));
 
 export default modelType => (Component) => {
   const WithModelFieldClass = ({
@@ -11,9 +14,9 @@ export default modelType => (Component) => {
   }) => {
     const types = String(path).split('.');
     const elementTypes = types.shift();
-    const elementClass = `${modelType}__${elementTypes}`;
+    const elementClass = `${modelType}__${inflect(elementTypes)}`;
     const classes = [elementClass];
-    types.forEach(type => classes.push(`${elementClass}--${type}`));
+    types.forEach(type => classes.push(`${elementClass}--${inflect(type)}`));
     return <Component className={classNames(classes, className)} path={path} {...rest} />;
   };
   WithModelFieldClass.displayName = `WithModelFieldClass(${componentDisplayName(Component)})[${modelType}]`;
