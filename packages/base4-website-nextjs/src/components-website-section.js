36c99fd60723c017e8b1e122c62c7b655ae4a695
Jacob Bare <jacob@limit0.io>
2018-10-29 10:26:23 -0500

    Create website section object value & name comps

--- packages/base4-website-nextjs/src/components-website-section.js
+++ packages/base4-website-nextjs/src/components-website-section.js
@@ -1,7 +1,14 @@
 import Link from './components/website-section/Link';
 import Wrapper from './components/website-section/Wrapper';
 
+import Name from './components/website-section/Elements/Name';
+import ObjectValue from './components/website-section/Elements/ObjectValue';
+
 export {
+  // Root
   Link,
   Wrapper,
+  // Elements
+  Name,
+  ObjectValue,
 };
