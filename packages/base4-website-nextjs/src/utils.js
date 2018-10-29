fc6f6f56f4c072f6ab60b85bdbe3d73c5023b0b4
Jacob Bare <jacob@limit0.io>
2018-10-29 09:26:27 -0500

    Create dasherize util

--- packages/base4-website-nextjs/src/utils.js
+++ packages/base4-website-nextjs/src/utils.js
@@ -3,6 +3,7 @@ import asObject from './utils/as-object';
 import cleanPath from './utils/clean-path';
 import componentDisplayName from './utils/component-display-name';
 import createMarkup from './utils/create-markup';
+import dasherize from './utils/dasherize';
 import extractFragmentData from './utils/extract-fragment-data';
 import extractFragmentName from './utils/extract-fragment-name';
 import formatDate from './utils/format-date';
@@ -21,6 +22,7 @@ export {
   cleanPath,
   componentDisplayName,
   createMarkup,
+  dasherize,
   extractFragmentData,
   extractFragmentName,
   formatDate,
--- packages/base4-website-nextjs/src/utils/model-class-names.js
+++ packages/base4-website-nextjs/src/utils/model-class-names.js
@@ -1,8 +1,10 @@
+import dasherize from './dasherize';
+
 export default (modelName, path) => {
   const types = String(path).split('.');
   const elementTypes = types.shift();
-  const elementClass = `${modelName}__${inflect(elementTypes)}`;
+  const elementClass = `${modelName}__${dasherize(elementTypes)}`;
   const classes = [elementClass];
-  types.forEach(type => classes.push(`${elementClass}--${inflect(type)}`));
+  types.forEach(type => classes.push(`${elementClass}--${dasherize(type)}`));
   return types;
 }
