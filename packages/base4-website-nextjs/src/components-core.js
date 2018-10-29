2cad01de1a5a55495205a494395c913c561d1c88
Jacob Bare <jacob@limit0.io>
2018-10-29 12:05:33 -0500

    Remove collection elements

--- packages/base4-website-nextjs/src/components-core.js
+++ packages/base4-website-nextjs/src/components-core.js
@@ -4,11 +4,6 @@ import LinkElement from './components/core/Elements/Link';
 import ObjectValueElement from './components/core/Elements/ObjectValue';
 import ValueElement from './components/core/Elements/Value';
 
-import DateCollection from './components/core/Collections/Date';
-import HTMLCollection from './components/core/Collections/HTML';
-import ObjectValueCollection from './components/core/Collections/ObjectValue';
-import ValueCollection from './components/core/Collections/Value';
-
 export {
   // Elements
   DateElement,
@@ -16,9 +11,4 @@ export {
   LinkElement,
   ObjectValueElement,
   ValueElement,
-  // Collections
-  DateCollection,
-  HTMLCollection,
-  ObjectValueCollection,
-  ValueCollection,
 };
