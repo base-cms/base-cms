ab8d587c57aa8855c4d99350955ea9f4d61a2eeb
Jacob Bare <jacob@limit0.io>
2018-10-29 12:11:16 -0500

    do not export withRouting

--- packages/base4-website-nextjs/src/pages/_app.jsx
+++ packages/base4-website-nextjs/src/pages/_app.jsx
@@ -1,7 +1,6 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import withApollo from '@base-cms/base4-nextjs-apollo';
-import withRouting from '../routing/withRouting';
 
 class WebsiteApp extends App {
   render() {
@@ -17,5 +16,4 @@ class WebsiteApp extends App {
 export {
   WebsiteApp,
   withApollo,
-  withRouting,
 };
