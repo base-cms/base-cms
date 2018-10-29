7a0771bc40680d87cace8165cc9c69fd928ef978
Jacob Bare <jacob@limit0.io>
2018-10-29 10:22:33 -0500

    rebuild

--- packages/base4-website-nextjs/dist/cjs/components-website-section.js
+++ packages/base4-website-nextjs/dist/cjs/components-website-section.js
@@ -2,11 +2,14 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var __chunk_1 = require('./chunk-9e05845b.js');
+var React = require('react');
+var React__default = _interopDefault(React);
+var PropTypes = _interopDefault(require('prop-types'));
+var classNames = _interopDefault(require('classnames'));
 var __chunk_5 = require('./chunk-dc3061c0.js');
-require('./chunk-9e05845b.js');
-require('react');
-require('prop-types');
-require('classnames');
 require('./chunk-d83335f1.js');
 require('./utils.js');
 require('inflected');
@@ -15,6 +18,42 @@ require('object-path');
 require('./routing.js');
 require('./chunk-4b678d5c.js');
 
+var propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  section: PropTypes.shape({
+    id: PropTypes.number,
+    alias: PropTypes.string
+  }),
+  tag: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+};
+var defaultProps = {
+  className: null,
+  section: {},
+  tag: 'section'
+};
 
+var WebsiteSectionWrapper = function WebsiteSectionWrapper(_ref) {
+  var children = _ref.children,
+      className = _ref.className,
+      section = _ref.section,
+      Tag = _ref.tag,
+      attrs = __chunk_1._objectWithoutProperties(_ref, ["children", "className", "section", "tag"]);
+
+  var _ref2 = section || {},
+      id = _ref2.id,
+      alias = _ref2.alias;
+
+  var type = alias ? alias.replace('/', '-') : alias;
+  return React__default.createElement(Tag, __chunk_1._extends({
+    "data-id": id,
+    className: classNames('website-section', 'website-section--display', "content--".concat(type), className)
+  }, attrs), children);
+};
+
+WebsiteSectionWrapper.displayName = 'Content/Wrapper';
+WebsiteSectionWrapper.propTypes = propTypes;
+WebsiteSectionWrapper.defaultProps = defaultProps;
 
 exports.Link = __chunk_5.Link;
+exports.Wrapper = WebsiteSectionWrapper;
--- packages/base4-website-nextjs/dist/esm/components-website-section.js
+++ packages/base4-website-nextjs/dist/esm/components-website-section.js
@@ -1,9 +1,9 @@
+import { c as _extends, b as _objectWithoutProperties } from './chunk-1a4eb17c.js';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { a as Link } from './chunk-2623c9a7.js';
 export { a as Link } from './chunk-2623c9a7.js';
-import './chunk-1a4eb17c.js';
-import 'react';
-import 'prop-types';
-import 'classnames';
 import './chunk-59d72e94.js';
 import './utils.js';
 import 'inflected';
@@ -11,3 +11,42 @@ import 'moment';
 import 'object-path';
 import './routing.js';
 import './chunk-7976a9a0.js';
+
+var propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  section: PropTypes.shape({
+    id: PropTypes.number,
+    alias: PropTypes.string
+  }),
+  tag: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+};
+var defaultProps = {
+  className: null,
+  section: {},
+  tag: 'section'
+};
+
+var WebsiteSectionWrapper = function WebsiteSectionWrapper(_ref) {
+  var children = _ref.children,
+      className = _ref.className,
+      section = _ref.section,
+      Tag = _ref.tag,
+      attrs = _objectWithoutProperties(_ref, ["children", "className", "section", "tag"]);
+
+  var _ref2 = section || {},
+      id = _ref2.id,
+      alias = _ref2.alias;
+
+  var type = alias ? alias.replace('/', '-') : alias;
+  return React.createElement(Tag, _extends({
+    "data-id": id,
+    className: classNames('website-section', 'website-section--display', "content--".concat(type), className)
+  }, attrs), children);
+};
+
+WebsiteSectionWrapper.displayName = 'Content/Wrapper';
+WebsiteSectionWrapper.propTypes = propTypes;
+WebsiteSectionWrapper.defaultProps = defaultProps;
+
+export { WebsiteSectionWrapper as Wrapper };
