c0e0b8c3e2c181367ffe26ca0cdfc30ee6093669
Jacob Bare <jacob@limit0.io>
2018-10-29 15:14:02 -0500

    Add build config and scripts

---
+++ packages/base4-website-nextjs-bootstrap/rollup.config.js
@@ -0,0 +1,37 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json';
+
+const { keys } = Object;
+
+const externals = [
+  ...keys(pkg.dependencies || {}),
+  ...keys(pkg.peerDependencies || {}),
+];
+
+const makeExternalPredicate = (externalsArr) => {
+  if (externalsArr.length === 0) return () => false;
+  const externalPattern = new RegExp(`^(${externalsArr.join('|')})($|/)`);
+  return id => externalPattern.test(id);
+}
+
+export default {
+  experimentalCodeSplitting: true,
+  input: [
+    'src/content.js',
+    'src/core.js',
+    'src/website-scheduled-content.js',
+  ],
+  output: [
+    { dir: 'dist/cjs', format: 'cjs' },
+    { dir: 'dist/esm', format: 'esm' },
+  ],
+  external: makeExternalPredicate(externals),
+  plugins: [
+    nodeResolve({ extensions: ['.js', '.jsx'] }),
+    commonjs({ include: ['node_modules/**'] }),
+    babel({ exclude: 'node_modules/**' }),
+  ],
+}
