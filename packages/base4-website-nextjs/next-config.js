e2207f94f905ec53f563b70713af6261d1391bce
Jacob Bare <jacob@limit0.io>
2018-10-30 11:58:33 -0500

    Move next-config to root and add env plugin

--- packages/base4-website-nextjs/next-config.js
+++ packages/base4-website-nextjs/next-config.js
@@ -1,3 +1,32 @@
-const plugin = require('./src/next-config');
+const { EnvironmentPlugin } = require('webpack');
 
-module.exports = plugin;
+module.exports = (nextConfig = {}) => Object.assign({}, nextConfig, {
+  /**
+   *
+   */
+  publicRuntimeConfig: {
+    contentCanonicalPathFields: ['sectionAlias', 'type', 'id', 'slug'],
+    sectionRoutePrefix: 'section',
+    ...nextConfig.publicRuntimeConfig,
+  },
+
+  /**
+   *
+   * @param {*} config
+   * @param {*} options
+   */
+  webpack(config, options) {
+    config.plugins.push(new EnvironmentPlugin(['NODE_ENV']));
+
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+
+    if (typeof nextConfig.webpack === 'function') {
+      return nextConfig.webpack(config, options);
+    }
+    return config;
+  },
+});
