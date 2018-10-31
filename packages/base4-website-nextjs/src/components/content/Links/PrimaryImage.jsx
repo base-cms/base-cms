47e4a6c0d9599ee3d0eb498f663a51893a0fd7cb
Jacob Bare <jacob@limit0.io>
2018-10-31 13:03:35 -0500

    Use PrimaryImage element

--- packages/base4-website-nextjs/src/components/content/Links/PrimaryImage.jsx
+++ packages/base4-website-nextjs/src/components/content/Links/PrimaryImage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { getAsObject, modelClassNames, get } from '../../../utils';
+import { get } from '../../../utils';
+import PrimaryImage from '../Elements/PrimaryImage';
 import Link from '../Link';
 
 const propTypes = {
-  className: PropTypes.string,
   collapsible: PropTypes.bool,
   content: PropTypes.shape({
     canonicalPath: PropTypes.string,
@@ -14,43 +13,31 @@ const propTypes = {
       alt: PropTypes.string,
     }),
   }),
-  imgAttrs: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   linkAttrs: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  tag: PropTypes.string,
 };
 
 const defaultProps = {
-  className: null,
   collapsible: true,
   content: {},
-  imgAttrs: {},
   linkAttrs: {},
-  tag: 'div',
 };
 
 const PrimaryImageLink = ({
-  className,
   collapsible,
   content,
-  imgAttrs,
   linkAttrs,
-  tag: Tag,
   ...attrs
 }) => {
   const canonicalPath = get(content, 'canonicalPath');
-  const primaryImage = getAsObject(content, 'primaryImage');
-  const { src, alt } = primaryImage;
-  if (collapsible && (!src || !alt)) return null;
-
-  const image = <img src={src} alt={alt} {...imgAttrs} />;
+  if (collapsible && !canonicalPath) return null;
   return (
-    <Tag className={classNames(modelClassNames('content', 'primaryImage'), className)} {...attrs}>
-      {canonicalPath ? (
+    <PrimaryImage content={content} collapsible={collapsible} {...attrs}>
+      {image => (
         <Link canonicalPath={canonicalPath} collapsible={false} {...linkAttrs}>
-          {() => image}
+          {image}
         </Link>
-      ) : image}
-    </Tag>
+      )}
+    </PrimaryImage>
   );
 };
 
