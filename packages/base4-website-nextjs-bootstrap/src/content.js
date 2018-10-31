5c187344bc24e7471a41fd7281c2f4dca0add5b0
Jacob Bare <jacob@limit0.io>
2018-10-31 15:13:37 -0500

    Remove card and list group item

--- packages/base4-website-nextjs-bootstrap/src/content.js
+++ packages/base4-website-nextjs-bootstrap/src/content.js
@@ -1,15 +1,11 @@
-import Card from './content/Card';
 import CardBodyA from './content/CardBody/StyleA';
 import CardImage from './content/CardImage';
 import ListGroupA from './content/ListGroup/StyleA';
-import ListGroupItem from './content/ListGroupItem';
 import ListGroupItemA from './content/ListGroupItem/StyleA';
 
 export {
-  Card,
   CardBodyA,
   CardImage,
   ListGroupA,
-  ListGroupItem,
   ListGroupItemA,
 };
