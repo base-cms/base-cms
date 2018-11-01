302501523f9e221c5b6d7abcfd618cbb85e2fbde
Jacob Bare <jacob@limit0.io>
2018-11-01 08:28:25 -0500

    Load dynamic page

--- packages/base4-website-nextjs-bootstrap/test/app/pages/page.jsx
+++ packages/base4-website-nextjs-bootstrap/test/app/pages/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { withDynamicPage } from '@base-cms/base4-website-nextjs/hoc';
+import {
+  Body,
+  Name,
+  Wrapper,
+} from '@base-cms/base4-website-nextjs/components/dynamic-page';
 import withLayout from '../../../src/layouts/withLayout';
 import DefaultLayout from '../layouts/Default';
 
-const DynamicPage = () => (
-  <h1>Dynamic Page</h1>
+const DynamicPage = ({ page }) => (
+  <Wrapper page={page}>
+    <Name page={page} />
+    <Body page={page} />
+  </Wrapper>
 );
-export default withLayout(DefaultLayout)(DynamicPage);
+
+export default withLayout(DefaultLayout)(
+  withDynamicPage(
+    DynamicPage
+  )
+);
