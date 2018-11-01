308f05c78922bce73652f5800ce9ec4d16467a65
Jacob Bare <jacob@limit0.io>
2018-11-01 08:33:10 -0500

    Add test content display

--- packages/base4-website-nextjs-bootstrap/test/app/pages/content.jsx
+++ packages/base4-website-nextjs-bootstrap/test/app/pages/content.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { withPlatformContent } from '@base-cms/base4-website-nextjs/hoc';
+import {
+  Body,
+  Name,
+  PrimaryImage,
+  PrimarySectionNameLink,
+  PublishedDate,
+  Row,
+  Teaser,
+  Type,
+  Wrapper,
+} from '@base-cms/base4-website-nextjs/components/content';
 import withLayout from '../../../src/layouts/withLayout';
 import DefaultLayout from '../layouts/Default';
 
-const ContentPage = () => (
-  <h1>Content Page</h1>
+// @todo Remove need for CDN!
+const fragment = gql`
+  fragment DefaultContentPage on PlatformContent {
+    primaryImage {
+      id
+      src(input: { host: "cdn.officer.com" })
+      alt
+    }
+  }
+`;
+
+
+const ContentPage = ({ content }) => (
+  <Wrapper content={content}>
+    <Name content={content} />
+    <Teaser tag="h3" content={content} />
+    <PrimaryImage content={content} />
+    <hr />
+    <Row>
+      <PrimarySectionNameLink tag="span" content={content}>
+        {(value) => <strong>{value}</strong>}
+      </PrimarySectionNameLink>
+      {' | '}
+      <Type content={content} />
+      {' | '}
+      <PublishedDate prefix="Published " content={content} />
+    </Row>
+    <hr />
+    <Body content={content} />
+  </Wrapper>
 );
-export default withLayout(DefaultLayout)(ContentPage);
+
+export default withLayout(DefaultLayout)(
+  withPlatformContent({ fragment })(
+    ContentPage
+  )
+);
+
