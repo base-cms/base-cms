0778d73fb11986591e65bceebe9301ded2194a02
Jacob Bare <jacob@limit0.io>
2018-11-12 11:01:15 -0600

    Add initial resolvers, definitions, and schema

---
+++ packages/graphql/src/schema.js
@@ -0,0 +1,12 @@
+const { join } = require('path');
+const { makeExecutableSchema } = require('graphql-tools');
+const { importSchema } = require('graphql-import');
+const resolvers = require('./resolvers');
+
+const typeDefs = importSchema(join(__dirname, 'definitions/index.graphql'));
+
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  schemaDirectives: {},
+});
