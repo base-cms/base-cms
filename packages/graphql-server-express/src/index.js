d73a5cbb57947d5c1c88a5bdfe660af03d6cfc98
Jacob Bare <jacob@limit0.io>
2018-11-12 13:51:15 -0600

    Do not use db library directly

--- packages/graphql-server-express/src/index.js
+++ packages/graphql-server-express/src/index.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-const base = require('./base');
 const { BASECMS_GRAPHQL_PORT: PORT, BASECMS_GRAPHQL_HOST: HOST } = require('./env');
 const pkg = require('../package.json');
 
@@ -7,8 +6,6 @@ const { log } = console;
 
 const run = async () => {
   log(`Starting '${pkg.name}'...`);
-  const db = await base.connect();
-  log(`BaseCMS DB connected to ${db.s.url}`);
   const server = await app(PORT, HOST);
 
   log(`> Ready on on http://${HOST}:${PORT}`);
@@ -17,11 +14,8 @@ const run = async () => {
     log(`Stopping '${pkg.name}'...`);
     server.stop(() => {
       log('Web server stopped.');
-      db.close().then(() => {
-        log('BaseCMS DB closed.');
-        log('> Stopped');
-        process.exit();
-      }).catch(e => setImmediate(() => { throw e; }));
+      log('> Stopped');
+      process.exit();
     });
   };
 
