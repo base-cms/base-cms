7099bd7b9c30018014f97177a43a8afe5c05a628
Jacob Bare <jacob@limit0.io>
2018-11-12 12:15:15 -0600

    Add stoppable/grace to express server

--- packages/graphql-server-express/src/app.js
+++ packages/graphql-server-express/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const stoppable = require('stoppable');
 
 const app = express();
 const CORS = cors();
@@ -9,4 +10,8 @@ app.use(helmet());
 app.use(CORS);
 app.options('*', CORS);
 
-module.exports = app;
+module.exports = (...args) => new Promise((resolve, reject) => {
+  const server = app.listen(...args);
+  server.on('error', (e => reject(e)));
+  server.on('listening', () => resolve(stoppable(server, 1000)));
+});
