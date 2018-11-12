07bd2670e52cffc39d99d687129ea7cec546c50d
Jacob Bare <jacob@limit0.io>
2018-11-12 12:14:58 -0600

    Add express host and port vars

--- packages/graphql-server-express/src/env.js
+++ packages/graphql-server-express/src/env.js
@@ -1,5 +1,7 @@
-const { str, cleanEnv } = require('envalid');
+const { str, cleanEnv, port } = require('envalid');
 
 module.exports = cleanEnv(process.env, {
   BASECMS_MONGODB_URL: str({ desc: 'The BaseCMS MongoDB server URL.' }),
+  BASECMS_GRAPHQL_PORT: port({ desc: 'The port to run the GraphQL server on.', default: 6915 }),
+  BASECMS_GRAPHQL_HOST: str({ desc: 'The host to run the GraphQL server on.', default: 'localhost' }),
 });
