e226d44635124da3861275fd68c954ccd4d5407e
Jacob Bare <jacob@limit0.io>
2018-11-14 09:29:45 -0600

    Add callOnce function

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,9 +1,11 @@
 const async = require('./async');
+const callOnce = require('./call-once');
 const isObject = require('./is-object');
 const stripHtml = require('./strip-html');
 
 module.exports = {
   async,
+  callOnce,
   isObject,
   stripHtml,
 };
