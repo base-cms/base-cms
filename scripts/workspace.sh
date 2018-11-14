acae2ce5b234d758ff28643c6f5e248a28b83075
Jacob Bare <jacob@limit0.io>
2018-11-14 11:55:57 -0600

    Create workspace and terminal scripts

---
+++ scripts/workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker run \
+  --rm \
+  --name base-cms_workspace \
+  --workdir /base-cms \
+  --tty \
+  --env YARN_CACHE_FOLDER=/.yarn-cache \
+  --volume base-cms_yarn_cache:/.yarn-cache \
+  --volume `pwd`:/base-cms:cached \
+  --volume base-cms_node_modules:/base-cms/node_modules \
+  node:10.13-alpine \
+  yarn workspace $@
