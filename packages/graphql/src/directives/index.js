6eb5c33da246d893f173e1b2169af6c3fadfad24
Jacob Bare <jacob@limit0.io>
2018-11-16 10:35:51 -0600

    Add RefOneDirective to export

--- packages/graphql/src/directives/index.js
+++ packages/graphql/src/directives/index.js
@@ -1,7 +1,9 @@
 const ArrayValueDirective = require('./array-value');
+const RefOneDirective = require('./ref-one');
 const ValueDirective = require('./value');
 
 module.exports = {
   arrayValue: ArrayValueDirective,
+  refOne: RefOneDirective,
   value: ValueDirective,
 };
