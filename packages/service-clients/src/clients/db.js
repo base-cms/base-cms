a19f744431c8fd8bf79b27aac828cc96f4eb96b8
Jacob Bare <jacob@limit0.io>
2018-11-16 08:25:00 -0600

    Use extended JSON serializer

--- packages/service-clients/src/clients/db.js
+++ packages/service-clients/src/clients/db.js
@@ -1,3 +1,10 @@
+/* eslint-disable class-methods-use-this */
+const { Serializers } = require('@base-cms/moleculer');
 const Client = require('../client');
 
-module.exports = Client({ serviceName: 'db' });
+module.exports = Client({
+  serviceName: 'db',
+  brokerOpts: {
+    serializer: new Serializers.EJSONSerializer(),
+  },
+});
--- services/db/src/broker.js
+++ services/db/src/broker.js
@@ -1,4 +1,5 @@
 const { ServiceBroker } = require('moleculer');
+const { Serializers } = require('@base-cms/moleculer');
 const { NATS_DSN, NATS_LOGLEVEL } = require('./env');
 
 module.exports = new ServiceBroker({
@@ -6,4 +7,5 @@ module.exports = new ServiceBroker({
   transporter: NATS_DSN,
   logLevel: NATS_LOGLEVEL,
   logFormatter: 'simple',
+  serializer: new Serializers.EJSONSerializer(),
 });
