98d06e31cb846fdbc498c2875773f68f2eeb04ba
Jacob Bare <jacob@limit0.io>
2018-11-16 08:26:29 -0600

    Include website resolvers

--- packages/graphql/src/resolvers/index.js
+++ packages/graphql/src/resolvers/index.js
@@ -2,7 +2,10 @@ const deepAssign = require('deep-assign');
 const GraphQLJSON = require('graphql-type-json');
 const { DateType, CursorType } = require('@limit0/graphql-custom-types');
 
+const website = require('./website');
+
 module.exports = deepAssign(
+  website,
   {
     /**
      * Custom scalar types.
