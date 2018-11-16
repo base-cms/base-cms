ccfbfc2dd8deebbd43f50b6b4aa52d895ff4504c
Jacob Bare <jacob@limit0.io>
2018-11-16 08:26:23 -0600

    Properly export directive keys

--- packages/graphql/src/directives/index.js
+++ packages/graphql/src/directives/index.js
@@ -2,6 +2,6 @@ const ArrayValueDirective = require('./array-value');
 const ValueDirective = require('./value');
 
 module.exports = {
-  ArrayValueDirective,
-  ValueDirective,
+  arrayValue: ArrayValueDirective,
+  value: ValueDirective,
 };
