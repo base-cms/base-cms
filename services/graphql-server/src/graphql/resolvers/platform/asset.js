4926f1fb67c3f33b50cdce5f9f8ba5228de2cd5a
Jacob Bare <jacob@limit0.io>
2018-11-26 13:13:45 -0600

    Create PlatformAssetImage

--- services/graphql-server/src/graphql/resolvers/platform/asset.js
+++ services/graphql-server/src/graphql/resolvers/platform/asset.js
@@ -1,3 +1,51 @@
-module.exports = {
+const inflection = require('inflection');
 
+const altFrom = (value) => {
+  const pos = value.lastIndexOf('.');
+  if (pos === -1) return value;
+  const offset = value.length - pos;
+  if (offset < 6) {
+    const replaced = value.replace(value.substring(pos), '');
+    const titleized = inflection.titleize(replaced);
+    return titleized.replace(/\./g, ' ');
+  }
+  return value;
+};
+
+module.exports = {
+  /**
+   *
+   */
+  PlatformAssetImage: {
+    src: (image, { input }) => {
+      const { host, size, aspectRatio } = input;
+      const { filePath, fileName } = image;
+      const file = fileName.replace(/\.png$/, '.jpg');
+      const ar = aspectRatio ? `/${aspectRatio}` : '';
+      const sz = size !== 'original' ? `/${size}` : '';
+      return `https://${host}/${filePath}${ar}${sz}/${file}`;
+    },
+    alt: (image) => {
+      const { caption, name, fileName } = image;
+      if (name) return altFrom(name);
+      if (caption) return caption;
+      return altFrom(fileName);
+    },
+  },
+
+  /**
+   *
+   */
+  Query: {
+    /**
+     *
+     */
+    platformAssetImage: async (root, { input }, { basedb }) => {
+      const { id } = input;
+      return basedb.findOne('platform.Asset', {
+        _id: id,
+        type: 'Image',
+      });
+    },
+  },
 };
