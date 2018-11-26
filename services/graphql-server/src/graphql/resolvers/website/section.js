b9c863a957d8bc82f911d2a4769e58bd82bed360
Jacob Bare <jacob@limit0.io>
2018-11-26 12:41:58 -0600

    Add WebsiteSection.site

--- services/graphql-server/src/graphql/resolvers/website/section.js
+++ services/graphql-server/src/graphql/resolvers/website/section.js
@@ -1,6 +1,16 @@
 const formatStatus = require('../../utils/format-status');
 
 module.exports = {
+  WebsiteSection: {
+    site: (section, { input }, { basedb }) => basedb.referenceOne({
+      doc: section,
+      relatedModel: 'platform.Product',
+      localField: 'site',
+      foreignField: '_id',
+      query: { type: 'Site', ...formatStatus(input.status) },
+    }),
+  },
+
   /**
    *
    */
