a02d6fd344ad30b637105fe111551d3dabb1dd70
Jacob Bare <jacob@limit0.io>
2018-11-27 10:01:38 -0600

    Only apply input if using+input are objects

--- services/graphql-server/src/graphql/utils/apply-input.js
+++ services/graphql-server/src/graphql/utils/apply-input.js
@@ -1,9 +1,12 @@
+const { isObject } = require('@base-cms/common');
+
 const { isArray } = Array;
 
-module.exports = ({ query, using, input }) => Object.keys(using)
-  .filter(key => typeof input[key] !== 'undefined')
-  .reduce((obj, key) => {
+module.exports = ({ query, using, input }) => {
+  if (!isObject(using) || !isObject(input)) return query;
+  return Object.keys(using).filter(key => typeof input[key] !== 'undefined').reduce((obj, key) => {
     const field = using[key];
     const value = input[key];
     return { ...obj, [field]: isArray(value) ? { $in: value } : value };
   }, query);
+};
