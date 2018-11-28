bd02fbd4903d944d3152064d901b9dd7657a56e8
Jacob Bare <jacob@limit0.io>
2018-11-28 15:42:39 -0600

    Make content criteria easier :)

--- services/graphql-server/src/graphql/utils/criteria-for.js
+++ services/graphql-server/src/graphql/utils/criteria-for.js
@@ -1,11 +1,8 @@
+const contentTypes = require('./content-types');
+
 const criterion = {
   assetImage: () => ({ type: 'Image' }),
-  content: () => ({ type: { $in: ['Article', 'Blog', 'Contact', 'Company', 'PressRelease'] } }),
-  contentArticle: () => ({ type: 'Article' }),
-  contentBlog: () => ({ type: 'Blog' }),
-  contentCompany: () => ({ type: 'Company' }),
-  contentContact: () => ({ type: 'Contact' }),
-  contentPressRelease: () => ({ type: 'PressRelease' }),
+  content: () => ({ type: { $in: contentTypes } }),
   emailNewsletter: () => ({ type: 'Newsletter' }),
   globalMagazineSection: () => ({ 'publication.$id': { $exists: true } }),
   issueMagazineSection: () => ({ 'issue.$id': { $exists: true } }),
@@ -14,6 +11,11 @@ const criterion = {
   rootWebsiteSection: () => ({ 'parent.$id': { $exists: false } }),
   websiteSite: () => ({ type: 'Site' }),
 };
+
+contentTypes.forEach((type) => {
+  const key = `content${type}`;
+  criterion[key] = () => ({ type });
+});
 
 module.exports = (key) => {
   if (!key) return {};
