e179e3da0916667c5f7e55bb049511685fad87f8
Jacob Bare <jacob@limit0.io>
2018-11-29 14:04:04 -0600

    Add apparatus specific fields

--- services/graphql-server/src/graphql/definitions/platform/content/types/apparatus.js
+++ services/graphql-server/src/graphql/definitions/platform/content/types/apparatus.js
@@ -6,8 +6,22 @@ extend type Query {
   contentApparatus(input: ContentApparatusQueryInput!): ContentApparatus @findOne(model: "platform.Content", using: { id: "_id" }, criteria: "contentApparatus")
 }
 
-type ContentApparatus implements Content @applyInterfaceFields {
-  id: Int! @value(localField: "_id")
+type ContentApparatus implements Content & Authorable @applyInterfaceFields {
+  # fields directly on platform.model::Content\Apparatus
+  aerial: String
+  email: String
+  engine: String
+  generator: String
+  pump: String
+  tank: String
+  year: Int
+  manufacturer: String
+  transmission: String
+  apparatusType: String
+  chassisManufacturer(input: ContentApparatusChassisManufacturerInput = {}): ContentCompany @refOne(model: "platform.Content", criteria: "contentCompany")
+  state: String
+  group(input: ContentApparatusGroupInput = {}): ContentGroup @refOne(model: "platform.Content", criteria: "contentGroup")
+  model: String
 }
 
 type ContentApparatusConnection {
@@ -23,6 +37,14 @@ type ContentApparatusEdge {
 
 input ContentApparatusQueryInput {
   id: Int!
+  status: ModelStatus = active
+}
+
+input ContentApparatusChassisManufacturerInput {
+  status: ModelStatus = active
+}
+
+input ContentApparatusGroupInput {
   status: ModelStatus = active
 }
 
