aee959bd610d0b7a410d929d3df9c0bc29187815
Jacob Bare <jacob@limit0.io>
2018-11-29 14:44:14 -0600

    Add event specific fields

--- services/graphql-server/src/graphql/definitions/platform/content/types/event.js
+++ services/graphql-server/src/graphql/definitions/platform/content/types/event.js
@@ -6,8 +6,17 @@ extend type Query {
   contentEvent(input: ContentEventQueryInput!): ContentEvent @findOne(model: "platform.Content", using: { id: "_id" }, criteria: "contentEvent")
 }
 
-type ContentEvent implements Content @applyInterfaceFields {
-  id: Int! @value(localField: "_id")
+type ContentEvent implements Content & Contactable & Addressable @applyInterfaceFields {
+  # fields directly on platform.model::Content\Event
+  cost: String
+  eventType: String
+  venue(input: ContentEventVenueInput = {}): EntityVenue @refOne(model: "platform.Entity", criteria: "entityVenue")
+  organization(input: ContentEventOrganizationInput = {}): EntityOrganization @refOne(model: "platform.Entity", criteria: "entityOrganization")
+  startDate: Date
+  endDate: Date
+  allDay: Boolean
+  contacts: [ContentEventContact]! @arrayValue
+  beneficiary: String
 }
 
 type ContentEventConnection {
@@ -21,8 +30,25 @@ type ContentEventEdge {
   cursor: String!
 }
 
+type ContentEventContact {
+  type: String
+  contact(input: ContentEventContactsContactInput = {}): ContentContact @refOne(model: "platform.Content", criteria: "contentContact")
+}
+
 input ContentEventQueryInput {
   id: Int!
+  status: ModelStatus = active
+}
+
+input ContentEventVenueInput {
+  status: ModelStatus = active
+}
+
+input ContentEventOrganizationInput {
+  status: ModelStatus = active
+}
+
+input ContentEventContactsContactInput {
   status: ModelStatus = active
 }
 
