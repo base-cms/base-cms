e409b0ee82e1aabe1d79be765bcede316a0725fa
Jacob Bare <jacob@limit0.io>
2018-11-29 14:49:43 -0600

    Add in quarters specific fields

--- services/graphql-server/src/graphql/definitions/platform/content/types/in-quarters.js
+++ services/graphql-server/src/graphql/definitions/platform/content/types/in-quarters.js
@@ -6,8 +6,22 @@ extend type Query {
   contentInQuarters(input: ContentInQuartersQueryInput!): ContentInQuarters @findOne(model: "platform.Content", using: { id: "_id" }, criteria: "contentInQuarters")
 }
 
-type ContentInQuarters implements Content @applyInterfaceFields {
-  id: Int! @value(localField: "_id")
+type ContentInQuarters implements Content & Authorable @applyInterfaceFields {
+  # fields directly on platform.model::Content\InQuarters
+  email: String
+  group(input: ContentInQuartersGroupInput = {}): ContentGroup @refOne(model: "platform.Content", criteria: "contentGroup")
+  state: String
+  year: Int
+  engines: Int
+  tankers: Int
+  brushTrucks: Int
+  aerials: Int
+  heavyRescues: Int
+  chiefs: Int
+  blsAls: Int
+  supportOther: Int
+  members: Int
+  servingArea: Int
 }
 
 type ContentInQuartersConnection {
@@ -23,6 +37,10 @@ type ContentInQuartersEdge {
 
 input ContentInQuartersQueryInput {
   id: Int!
+  status: ModelStatus = active
+}
+
+input ContentInQuartersGroupInput {
   status: ModelStatus = active
 }
 
