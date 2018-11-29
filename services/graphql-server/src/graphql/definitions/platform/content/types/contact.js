42c9a476be31c64b5bf78932343e9bf9f938b0f1
Jacob Bare <jacob@limit0.io>
2018-11-29 14:22:52 -0600

    Add contact specific fields

--- services/graphql-server/src/graphql/definitions/platform/content/types/contact.js
+++ services/graphql-server/src/graphql/definitions/platform/content/types/contact.js
@@ -6,8 +6,14 @@ extend type Query {
   contentContact(input: ContentContactQueryInput!): ContentContact @findOne(model: "platform.Content", using: { id: "_id" }, criteria: "contentContact")
 }
 
-type ContentContact implements Content @applyInterfaceFields {
-  id: Int! @value(localField: "_id")
+type ContentContact implements Content & Contactable & Addressable @applyInterfaceFields {
+  # fields directly on platform.model::Content\Contact
+  socialLinks: [EntityStubSocial]! @arrayValue
+
+  # GraphQL-only fields
+  # @todo Implement
+  # @see Cygnus\ApplicationBundle\Icarus\BlockHandler\ContentAuthorQuery
+  ownedContent(input: ContentContactOwnedContentInput = {}): ContentConnection!
 }
 
 type ContentContactConnection {
@@ -40,6 +46,16 @@ enum ContentContactSortField {
 input ContentContactQueryInput {
   id: Int!
   status: ModelStatus = active
+}
+
+# @todo Implement.
+input ContentContactOwnedContentInput {
+  type: ContentContactAuthorType # a null value means all types
+  excludeContentTypes: [ContentType]! = []
+  includeContentTypes: [ContentType]! = []
+  requiresImage: Boolean = false
+  sort: ContentSortInput = {}
+  pagination: PaginationInput = {}
 }
 
 input ContentContactSortInput {
