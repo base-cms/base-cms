a26d7fa6dc8fe6fdb47a4a587fc2327c99a53e12
Jacob Bare <jacob@limit0.io>
2018-12-11 15:58:40 -0600

    Remove commented code

--- packages/nextjs-apollo/src/fragment-matcher.js
+++ packages/nextjs-apollo/src/fragment-matcher.js
@@ -1,6 +1,4 @@
 import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import introspectionQueryResultData from '@base-cms/graphql-stitching/fragment-types.json';
 
-// const { IntrospectionFragmentMatcher } = apolloCache;
-
 export default new IntrospectionFragmentMatcher({ introspectionQueryResultData });
