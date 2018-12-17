f7ed8a85f61dc8491b80496ba65004d12041af0e
Jacob Bare <jacob@limit0.io>
2018-12-17 08:48:14 -0600

    Use nonemptystr for MONGO_DSN
    Allows server seed lists to be used.

--- services/graphql-server/src/env.js
+++ services/graphql-server/src/env.js
@@ -5,10 +5,10 @@ const {
   cleanEnv,
   bool,
 } = envalid;
-const { mongodsn, nonemptystr } = custom;
+const { nonemptystr } = custom;
 
 module.exports = cleanEnv(process.env, {
-  MONGO_DSN: mongodsn({ desc: 'The Base MongoDB connection URL.' }),
+  MONGO_DSN: nonemptystr({ desc: 'The Base MongoDB connection URL.' }),
   TENANT_KEY: nonemptystr({ desc: 'The Base tenant key to connect to, e.g. cygnus_ofcr' }),
   ENABLE_BASEDB_LOGGING: bool({ desc: 'Whether the BaseDB instance should log to the console.', default: false }),
 });
