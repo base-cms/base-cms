6ba2f355e7368cee971cd7e9b41804a4634318f7
Jacob Bare <jacob@limit0.io>
2018-12-17 13:11:57 -0600

    Update remote schema to use `mergeSchemas`

--- packages/graphql-stitching/src/schema.js
+++ packages/graphql-stitching/src/schema.js
@@ -1,14 +1,17 @@
-const { makeRemoteExecutableSchema, introspectSchema } = require('graphql-tools');
+const { makeRemoteExecutableSchema, introspectSchema, mergeSchemas } = require('graphql-tools');
 const link = require('./link');
 
 let promise;
 module.exports = async () => {
   const build = async () => {
     const schema = await introspectSchema(link);
-    return makeRemoteExecutableSchema({
+    const executableSchema = makeRemoteExecutableSchema({
       schema,
       link,
     });
+    // Use `mergeSchemas` to avoid needing the `__typename` field included.
+    // @see https://github.com/apollographql/graphql-tools/issues/441#issuecomment-345423403
+    return mergeSchemas({ schemas: [executableSchema] });
   };
   if (!promise) {
     // This will cache the schema and would require a reload of the app if it changes.
