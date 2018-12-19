e28d91c584d2db3a4f10e6b350a66396fcb80c92
Jacob Bare <jacob@limit0.io>
2018-12-19 11:54:48 -0600

    Enable bootstrap

--- services/example-website/next.config.js
+++ services/example-website/next.config.js
@@ -1,8 +1,9 @@
-const withBaseWebsite = require('@base-cms/nextjs-web/next-config');
+const withWebsite = require('@base-cms/nextjs-web/next-config');
+const withSass = require('@base-cms/nextjs-web-bootstrap/next-config');
 
-module.exports = withBaseWebsite({
+module.exports = withSass(withWebsite({
   distDir: '../.next/build',
   publicRuntimeConfig: {
     sectionRoutePrefix: '',
   },
-});
+}));
--- services/example-website/site/pages/_app.jsx
+++ services/example-website/site/pages/_app.jsx
@@ -1,3 +1,4 @@
+import '../styles/app.scss';
 import {
   WebsiteApp,
   withApollo,
