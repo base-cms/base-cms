0c93c149d645e97d9b5945d6cf91dd56d6fa9571
Jacob Bare <jacob@limit0.io>
2018-12-19 10:35:16 -0600

    Create site config and apply to _app

--- services/example-website/site/pages/_app.jsx
+++ services/example-website/site/pages/_app.jsx
@@ -2,11 +2,15 @@ import {
   WebsiteApp,
   WithApollo,
   WithRouting,
+  WithSiteConfig,
 } from '@base-cms/nextjs-web/app';
 import routeDefs from '../routes';
+import config from '../config';
 
 export default WithApollo(
   WithRouting(routeDefs)(
-    WebsiteApp,
+    WithSiteConfig(config)(
+      WebsiteApp,
+    ),
   ),
 );
