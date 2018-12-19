d645019bb939cc764b5593d607c7b8e2ff706af9
Jacob Bare <jacob@limit0.io>
2018-12-19 10:03:17 -0600

    Create utils

---
+++ packages/nextjs-web/src/utils.js
@@ -0,0 +1,9 @@
+import componentDisplayName from './utils/component-display-name';
+import isFunction from './utils/is-function';
+import isObject from './utils/is-object';
+
+export {
+  componentDisplayName,
+  isFunction,
+  isObject,
+};
