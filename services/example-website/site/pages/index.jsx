8e7874652ff9f5e8161ff3296349dc51bb91d2ee
Jacob Bare <jacob@limit0.io>
2018-12-19 15:11:19 -0600

    Apply default layout to home

--- services/example-website/site/pages/index.jsx
+++ services/example-website/site/pages/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { withLayout } from '@base-cms/website-nextjs-bootstrap/layouts';
+import DefaultLayout from '../layouts/Default';
 
 const IndexPage = () => (
-  <div>
-    <h1>Default Home Page</h1>
-  </div>
+  <h1>Home Page</h1>
 );
 
-export default IndexPage;
+export default withLayout(DefaultLayout)(IndexPage);
