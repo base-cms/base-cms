2160a22d8ab702b22d498b5dd3aabfcf382c9d58
Jacob Bare <jacob@limit0.io>
2018-12-19 15:52:14 -0600

    Add withWebsiteSection HOC

--- packages/nextjs-web/src/hoc.js
+++ packages/nextjs-web/src/hoc.js
@@ -1,9 +1,11 @@
 import withDynamicPage from './hoc/withDynamicPage';
 import withLayout from './hoc/withLayout';
 import withRequestOrigin from './hoc/withRequestOrigin';
+import withWebsiteSection from './hoc/withWebsiteSection';
 
 export {
   withDynamicPage,
   withLayout,
   withRequestOrigin,
+  withWebsiteSection,
 };
