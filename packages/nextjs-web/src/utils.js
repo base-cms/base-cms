ee49b5a9ae860e601fef4e7991d22b8f898d6837
Jacob Bare <jacob@limit0.io>
2018-12-19 13:26:36 -0600

    Add fragment extraction utils

--- packages/nextjs-web/src/utils.js
+++ packages/nextjs-web/src/utils.js
@@ -3,6 +3,8 @@ import asObject from './utils/as-object';
 import cleanPath from './utils/clean-path';
 import componentDisplayName from './utils/component-display-name';
 import createMarkup from './utils/create-markup';
+import extractFragmentData from './utils/extract-fragment-data';
+import extractFragmentName from './utils/extract-fragment-name';
 import get from './utils/get';
 import getAsArray from './utils/get-as-array';
 import getAsObject from './utils/get-as-object';
@@ -15,6 +17,8 @@ export {
   cleanPath,
   componentDisplayName,
   createMarkup,
+  extractFragmentData,
+  extractFragmentName,
   get,
   getAsArray,
   getAsObject,
