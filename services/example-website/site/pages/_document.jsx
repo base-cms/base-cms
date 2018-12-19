2cbb1f9558fbc0487167437da4e4625e2d2f9b4d
Jacob Bare <jacob@limit0.io>
2018-12-19 15:33:20 -0600

    Create _document

---
+++ services/example-website/site/pages/_document.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  /**
+   * This is overriden so we can add custom, global head elements.
+   */
+  render() {
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
