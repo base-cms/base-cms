05c31d77c74f895253d3a305ab1326a7d767644b
Jacob Bare <jacob@limit0.io>
2018-12-21 10:58:06 -0600

    Remove old nextjs-bootstrap package

--- packages/website-nextjs-bootstrap/src/layouts.js
+++
@@ -1,7 +0,0 @@
-import DefaultLayout from './layouts/Default';
-import withLayout from './layouts/withLayout';
-
-export {
-  DefaultLayout,
-  withLayout,
-};
--- packages/website-nextjs-bootstrap/test/app/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import withLayout from '../../../src/layouts/withLayout';
-import DefaultLayout from '../layouts/Default';
-
-const IndexPage = () => (
-  <h1>Home Page</h1>
-);
-export default withLayout(DefaultLayout)(IndexPage);
