1119885107f8182256fbf48cc7fc2ee8812fbabb
Jacob Bare <jacob@limit0.io>
2018-12-21 10:57:10 -0600

    Remove old starter kit

--- packages/website-nextjs-starter/next.config.js
+++
@@ -1,9 +0,0 @@
-const withSass = require('@base-cms/website-nextjs-bootstrap/next-config');
-const withBaseWebsite = require('@base-cms/website-nextjs/next-config');
-
-module.exports = withSass(withBaseWebsite({
-  distDir: '../.next/build',
-  publicRuntimeConfig: {
-    sectionRoutePrefix: '',
-  },
-}));
--- packages/website-nextjs-starter/site/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { withLayout } from '@base-cms/website-nextjs-bootstrap/layouts';
-import DefaultLayout from '../layouts/Default';
-
-const IndexPage = () => (
-  <h1>Default Home Page</h1>
-);
-
-export default withLayout(DefaultLayout)(IndexPage);
