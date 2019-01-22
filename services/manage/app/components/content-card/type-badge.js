275f70413ed31c9001277d3be4f6587ed13fd985
Jacob Bare <jacob@limit0.io>
2019-01-22 07:26:43 -0600

    Remove redudant badge class

--- services/manage/app/components/content-card/type-badge.js
+++ services/manage/app/components/content-card/type-badge.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'span',
-  classNames: ['badge', 'badge', 'badge-white', 'bg-default', 'z-depth-half'],
+  classNames: ['badge', 'badge-white', 'bg-default', 'z-depth-half'],
 });
