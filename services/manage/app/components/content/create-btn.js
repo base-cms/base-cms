5c449be7bae25b204453fb050db7aa8d3d8708d2
Jacob Bare <jacob@limit0.io>
2019-01-22 14:37:27 -0600

    Remove commented code

--- services/manage/app/components/content/create-btn.js
+++ services/manage/app/components/content/create-btn.js
@@ -1,9 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  // tagName: 'button',
-  // classNames: ['btn', 'btn-success', 'btn-circle', 'btn-xl', 'btn-content-create', 'z-depth-2'],
-  // attributeBindings: ['type'],
   tagName: '',
 
   type: 'button',
