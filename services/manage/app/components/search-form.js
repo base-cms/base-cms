60e93d08b2ccbb9dd773509360331d618b4b427b
Jacob Bare <jacob@limit0.io>
2019-01-28 13:16:41 -0600

    Slowly consolidate browse page styles and elements

---
+++ services/manage/app/components/search-form.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: 'form',
+  classNames: ['navbar-search', 'navbar-search-dark', 'form-inline', 'mr-3', 'd-none', 'd-md-flex', 'ml-lg-auto'],
+});
