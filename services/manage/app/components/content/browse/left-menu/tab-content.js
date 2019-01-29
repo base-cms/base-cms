a152e75f8ccdeed4ee0f026ece769899b35176be
Jacob Bare <jacob@limit0.io>
2019-01-29 08:35:38 -0600

    Add tab content active/transition support

--- services/manage/app/components/content/browse/left-menu/tab-content.js
+++ services/manage/app/components/content/browse/left-menu/tab-content.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['tab-content', 'bg-secondary'],
+  classNames: ['tab-content'],
   classNameBindings: ['active'],
 
   active: computed('tab.key', 'activeTabKey', function() {
@@ -12,7 +12,7 @@ export default Component.extend({
   activeTabKey: '',
   tab: null,
 
-  componentName: computed('tab.key', function() {
+  contentComponentName: computed('tab.key', function() {
     const key = this.get('tab.key');
     return `content/browse/left-menu/tab-content/${key}`;
   }),
