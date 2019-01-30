a00d73e6d1cbfcb30e7f96377ab18570a16d15c9
Jacob Bare <jacob@limit0.io>
2019-01-30 17:11:40 -0600

    Rework search form

--- services/manage-core/app/components/search-form.js
+++ services/manage-core/app/components/search-form.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'form',
-  classNames: ['navbar-search', 'navbar-search-dark', 'form-inline', 'mr-3', 'd-none', 'd-md-flex', 'ml-lg-auto'],
+  classNames: ['search', 'form-inline', 'my-2', 'ml-auto', 'mr-3'],
 });
