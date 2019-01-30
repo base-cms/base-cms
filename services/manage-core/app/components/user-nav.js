67aab0cacb89c622f14a61dad26feff0b5c8d2ab
Jacob Bare <jacob@limit0.io>
2019-01-30 16:33:56 -0600

    Rework user nav

--- services/manage-core/app/components/user-nav.js
+++ services/manage-core/app/components/user-nav.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: 'ul',
-  classNames: ['navbar-nav', 'align-items-center', 'd-none', 'd-md-flex'],
+  tagName: 'li',
+  classNames: ['nav-item dropdown user-dropdown'],
 });
