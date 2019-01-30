cb408c16841cfc6ec5fbd53000e77e5c0f8a4936
Jacob Bare <jacob@limit0.io>
2019-01-30 12:22:16 -0600

    Set new badge classes

--- services/manage-core/app/components/content/type-badge.js
+++ services/manage-core/app/components/content/type-badge.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'span',
-  classNames: ['badge', 'badge-white', 'bg-primary', 'z-depth-1'],
+  classNames: ['badge', 'badge-primary', 'text-uppercase', 'z-depth-1'],
 });
