539a342461804881916e693d962cf962992f29fb
Jacob Bare <jacob@limit0.io>
2019-02-11 11:33:49 -0600

    Ensure rootURL is properly inject from the config

--- services/manage-core/app/components/content/-header/navbar.js
+++ services/manage-core/app/components/content/-header/navbar.js
@@ -1,6 +1,13 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import config from '@base-cms/manage-core/config/environment';
 
 export default Component.extend({
   tagName: 'nav',
   classNames: ['content-header__navbar', 'navbar', 'navbar-expand', 'navbar-dark'],
+
+  rootURL: computed(function() {
+    return config.rootURL;
+  }),
+
 });
