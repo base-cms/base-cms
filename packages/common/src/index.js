90bf7cb68a0c670b7064d21e8dc4b7861f9c3cb1
Jacob Bare <jacob@limit0.io>
2019-02-26 08:43:40 -0600

    Create asyncRoute and cleanPath utils

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,11 +1,15 @@
 const async = require('./async');
+const asyncRoute = require('./async-route');
 const callOnce = require('./call-once');
+const cleanPath = require('./clean-path');
 const isObject = require('./is-object');
 const stripHtml = require('./strip-html');
 
 module.exports = {
   async,
+  asyncRoute,
   callOnce,
+  cleanPath,
   isObject,
   stripHtml,
 };
