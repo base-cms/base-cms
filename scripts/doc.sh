7ab161f501b9a137be5bec1928fb89e917dd9fa8
Josh Worden <solocommand@gmail.com>
2019-02-26 13:35:19 -0600

    Update build script

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Install deps
-./scripts/install.sh
-
 # Update GraphQL documentation
-docker-compose up -d graphql-server
-
-curl -IX GET http://localhost:10002/grapqhl?query=%7Bping%7D \
-    --retry 100 \
-    --retry-connrefused \
-    --retry-delay 2
-
-docker-compose run --rm --no-deps --entrypoint ./node_modules/.bin/graphdoc commands \
-    --force \
-    -o doc/graphql/$1 \
-    -e http://graphql-server/graphql
-
-docker-compose down
+node services/graphql-server/src/index.js &
+./node_modules/.bin/graphdoc --force -o doc/graphql/$1 -e http://localhost:10002/graphql
+killall node
 
 # Commit the new docs!
 git config --global user.email "travis@travis-ci.org"
@@ -28,5 +15,5 @@ mv doc/graphql graphql
 git add graphql
 git commit --message "Documentation update: $1"
 
-git remote add origin-pages https://${GH_DOC_TOKEN}@github.com/base-cms/base-cms.git > /dev/null 2>&1
+git remote add origin-pages https://${GITHUB_TOKEN}@github.com/base-cms/base-cms.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-pages gh-pages
