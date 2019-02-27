d5375578c75bbe5ba359a40349af6fd63e8aa680
Josh Worden <solocommand@gmail.com>
2019-02-27 09:37:16 -0600

    Revert "Don’t generate .bak file"
    This reverts commit 6b8452197f170e42bd5d3c521a7ec383cbe0f4ca.

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -27,7 +27,7 @@ if [ "master" != "$1" ]; then
 fi
 
 # Ensure there is always a change to prevent the git commit from erroring
-sed -i '' "s/\"Last Updated.*\"/\"Last Updated $(date +"%Y-%m-%d %H:%M")\"/" _doc/_config.yml
+sed -i.bak "s/\"Last Updated.*\"/\"Last Updated $(date +"%Y-%m-%d %H:%M")\"/" _doc/_config.yml
 
 cd _doc
 git add .
