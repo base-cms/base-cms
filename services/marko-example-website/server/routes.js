a6972c221588c0b516083767104770812c1718da
Jacob Bare <jacob@limit0.io>
2019-02-27 16:10:57 -0600

    Create contrived page examples

--- services/marko-example-website/server/routes.js
+++ services/marko-example-website/server/routes.js
@@ -1,5 +1,32 @@
+const { withWebsiteSection, withDynamicPage } = require('@base-cms/marko-web/middleware');
+const index = require('../app/templates/index');
+const section = require('../app/templates/section');
+const page = require('../app/templates/dynamic-page');
+const content = require('../app/templates/content');
+
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.json({ ping: 'pong' });
+    res.marko(index, {
+      name: 'Test',
+    });
   });
+
+  app.get('/page/:alias', withDynamicPage({
+    template: page,
+  }));
+
+  // All content
+  app.get('/:prefix(*):id(\\d{8}):suffix(*)', (req, res) => {
+    res.marko(content, {
+      name: 'Test',
+    });
+  });
+
+  app.get('/:alias(tactical)', withWebsiteSection({
+    template: section,
+  }));
+
+  app.get('/:alias(*)', withWebsiteSection({
+    template: section,
+  }));
 };
