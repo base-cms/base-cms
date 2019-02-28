7f9f667f92df654ce4058a3ddd326c5a58074ea2
Jacob Bare <jacob@limit0.io>
2019-02-28 14:25:15 -0600

    Pass apollo link config properly

--- packages/express-web/src/express/apollo.js
+++ packages/express-web/src/express/apollo.js
@@ -1,5 +1,5 @@
 const { apolloClient } = require('@base-cms/express-apollo');
 
 module.exports = (app, uri, config = {}) => {
-  app.use(apolloClient(uri, config));
+  app.use(apolloClient(uri, config, config.link));
 };
