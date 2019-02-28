35f1290a70015c395c5753694d9a022c1694f4fb
Jacob Bare <jacob@limit0.io>
2019-02-28 14:11:11 -0600

    Add underscore

--- packages/inflector/src/index.js
+++ packages/inflector/src/index.js
@@ -1,9 +1,11 @@
 const camelize = require('./camelize');
 const dasherize = require('./dasherize');
 const titleize = require('./titleize');
+const underscore = require('./underscore');
 
 module.exports = {
   camelize,
   dasherize,
   titleize,
+  underscore,
 };
