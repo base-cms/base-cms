a8e72622c273f5581ce7990feabd38224f2cd4ed
Jacob Bare <jacob@limit0.io>
2019-02-28 15:38:11 -0600

    Use web-common loaders

--- packages/marko-web/src/middleware/index.js
+++ packages/marko-web/src/middleware/index.js
@@ -1,7 +1,9 @@
-const withWebsiteSection = require('./with-website-section');
+const withContent = require('./with-content');
 const withDynamicPage = require('./with-dynamic-page');
+const withWebsiteSection = require('./with-website-section');
 
 module.exports = {
+  withContent,
   withDynamicPage,
   withWebsiteSection,
 };
--- packages/marko-web/src/middleware/with-content.js
+++ packages/marko-web/src/middleware/with-content.js
@@ -1,22 +1,22 @@
 const { asyncRoute, isFunction: isFn } = require('@base-cms/utils');
-const { websiteSection: loader } = require('@base-cms/web-common/page-loaders');
+const { content: loader } = require('@base-cms/web-common/page-loaders');
 
 module.exports = ({
   template,
   queryFragment,
-  aliasResolver,
+  idResolver,
   redirectOnPathMismatch = true,
 } = {}) => asyncRoute(async (req, res) => {
-  const alias = isFn(aliasResolver) ? await aliasResolver(req, res) : req.params.alias;
+  const id = isFn(idResolver) ? await idResolver(req, res) : req.params.id;
   const { apollo } = req;
 
-  const section = await loader(apollo, { alias, queryFragment });
-  const { redirectTo, canonicalPath } = section;
+  const content = await loader(apollo, { id, queryFragment });
+  const { redirectTo, canonicalPath } = content;
   if (redirectTo) {
     return res.redirect(301, redirectTo);
   }
   if (redirectOnPathMismatch && canonicalPath !== req.path) {
     return res.redirect(301, canonicalPath);
   }
-  return res.marko(template, { section });
+  return res.marko(template, { content });
 });
