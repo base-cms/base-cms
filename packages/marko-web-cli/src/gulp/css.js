3d4549cc4b54aa11744895c00c8050944dcbbb1d
Jacob Bare <jacob@limit0.io>
2019-03-06 14:03:48 -0600

    Use pump and log errors

--- packages/marko-web-cli/src/gulp/css.js
+++ packages/marko-web-cli/src/gulp/css.js
@@ -1,34 +1,37 @@
-const {
-  dest,
-  src,
-} = require('gulp');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
+const pump = require('pump');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const { dest, src } = require('gulp');
+const logError = require('../utils/log-error');
 
 sass.compiler = require('node-sass');
 
-module.exports = cwd => () => src('server/styles/index.scss', { cwd })
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(postcss([
-    autoprefixer({
-      browsers: [
-        '>= 1%',
-        'last 1 major version',
-        'Chrome >= 45',
-        'Firefox >= 38',
-        'Edge >= 12',
-        'Explorer >= 10',
-        'iOS >= 9',
-        'Safari >= 9',
-        'Android >= 4.4',
-        'Opera >= 30',
-      ],
-    }),
-    cssnano(),
-  ]))
-  .pipe(sourcemaps.write('.'))
-  .pipe(dest('dist', { cwd }));
+module.exports = cwd => () => {
+  pump([
+    src('server/styles/app.scss', { cwd }),
+    sourcemaps.init(),
+    sass(),
+    postcss([
+      autoprefixer({
+        browsers: [
+          '>= 1%',
+          'last 1 major version',
+          'Chrome >= 45',
+          'Firefox >= 38',
+          'Edge >= 12',
+          'Explorer >= 10',
+          'iOS >= 9',
+          'Safari >= 9',
+          'Android >= 4.4',
+          'Opera >= 30',
+        ],
+      }),
+      cssnano(),
+    ]),
+    sourcemaps.write('.'),
+    dest('dist', { cwd }),
+  ], logError);
+};
