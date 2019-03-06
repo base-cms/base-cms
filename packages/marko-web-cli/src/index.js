a107764cb02142abb58ca260dcb42e4d48c57f0f
Jacob Bare <jacob@limit0.io>
2019-03-06 12:18:47 -0600

    Add program usage

--- packages/marko-web-cli/src/index.js
+++ packages/marko-web-cli/src/index.js
@@ -1,6 +1,8 @@
 const program = require('yargs');
 const commands = require('./commands');
 
+program.usage('Usage: $0 <command> [options]');
+
 commands(program);
 
 module.exports = () => program.argv;
