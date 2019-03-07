7ca12f9f19bdda1eb4575937c68572676137384d
Jacob Bare <jacob@limit0.io>
2019-03-07 10:55:51 -0600

    Add note regarding global vs project cli

--- packages/marko-web-cli/bin/cli.js
+++ packages/marko-web-cli/bin/cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
+// @todo Determine when to require the global CLI vs. project CLI.
+// @see https://github.com/ember-cli/ember-cli/blob/master/bin/ember
+
 require('../src')();
