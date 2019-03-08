b80cc59f718b917cb46bc06dc932e35a16ede7a5
Jacob Bare <jacob@limit0.io>
2019-03-08 10:18:34 -0600

    Do not copy files present in gitignore

--- packages/web-cli/src/generator/build-files.js
+++ packages/web-cli/src/generator/build-files.js
@@ -1,7 +1,8 @@
 const handlebars = require('handlebars');
-const fs = require('fs');
+const ignore = require('ignore');
 const readdir = require('recursive-readdir');
-const path = require('path');
+const { join, parse } = require('path');
+const { readFileSync } = require('fs');
 const ifrm = require('./if-rm');
 
 handlebars.registerHelper('if-rm', ifrm);
@@ -9,25 +10,35 @@ handlebars.registerHelper('if-rm', ifrm);
 const cleanLines = contents => contents.replace(/\s^\s*__REMOVELINE__\s*$/gm, '').replace(/__REMOVELINE__/g, '');
 
 module.exports = async (templatesDir, answers) => {
-  // const templates = path.join(__dirname, 'templates');
   const files = await readdir(templatesDir);
+
+  // Create "relative" versions of each file.
+  const relative = files.map(f => f.replace(`${templatesDir}/`, ''));
+
+  // Load any .gitignore files and filter out files that should be ignored.
+  const toIgnore = ignore();
+  files.filter(f => /\.gitignore/.test(f)).map(f => readFileSync(f).toString()).forEach(patterns => toIgnore.add(patterns));
+  const filteredFiles = toIgnore.filter(relative);
+
   const targets = [];
 
-  const toWrite = files.map((file) => {
+  const toWrite = filteredFiles.map((f) => {
+    const file = join(templatesDir, f);
     const {
       dir,
       ext,
       base,
       name,
-    } = path.parse(file);
+    } = parse(file);
 
     const target = dir.replace(templatesDir, '');
     targets.push(target);
     const info = { target };
 
     if (ext === '.hbs') {
+      // Render the template contents with command line answers.
       info.file = name;
-      info.contents = cleanLines(handlebars.compile(fs.readFileSync(file).toString(), {
+      info.contents = cleanLines(handlebars.compile(readFileSync(file).toString(), {
         noEscape: true,
       })(answers));
     } else {
