2c267bc0b79e4203c1cb5f681ac3492ef4602a17
Jacob Bare <jacob@limit0.io>
2019-03-08 07:27:53 -0600

    Only load sections with assigned keywords

--- services/keyword-analysis/src/import/load-keywords.js
+++ services/keyword-analysis/src/import/load-keywords.js
@@ -10,13 +10,11 @@ module.exports = async () => {
   const map = {};
   log(chalk`{dim Loading keywords from website sections.}`);
 
-  const query = { status: 1 };
-  const sections = await base4.find('website.Section', query, { keywords: 1, alias: 1 }, true);
+  const query = { status: 1, keywords: { $exists: true } };
+  const sections = await base4.find('website.Section', query, { alias: 1, keywords: 1 }, true);
+
   sections.forEach((section) => {
-    const { alias } = section;
-    const keywords = Object.prototype.hasOwnProperty.call(section, 'keywords') && section.keywords
-      ? section.keywords
-      : alias.replace(/-/g, ' ').split('/');
+    const { alias, keywords } = section;
     map[alias] = keywords;
   });
 
