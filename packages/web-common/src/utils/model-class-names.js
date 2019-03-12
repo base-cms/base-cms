1aaf8e29dcf49145b88e83487f3fc7d71ebc6dfe
Jacob Bare <jacob@limit0.io>
2019-03-12 17:00:18 -0500

    Concatenate long deep class paths together

--- packages/web-common/src/utils/model-class-names.js
+++ packages/web-common/src/utils/model-class-names.js
@@ -3,6 +3,6 @@ const { dasherize } = require('@base-cms/inflector');
 module.exports = (modelName, path) => {
   const parts = String(path).split('.');
   const name = dasherize(modelName);
-  const classes = [`${name}-field`].concat(parts.filter(p => p).map(p => `${name}-field--${dasherize(p)}`));
+  const classes = [`${name}-field`, `${name}-field--${parts.map(p => dasherize(p)).join('-')}`];
   return classes;
 };
