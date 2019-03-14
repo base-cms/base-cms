a93730bb7734475e403b52de2b7764820b955602
Jacob Bare <jacob@limit0.io>
2019-03-14 09:19:27 -0500

    Add additional required schedule query fields

--- services/marko-example-website/server/api/fragments/website-scheduled-content.js
+++ services/marko-example-website/server/api/fragments/website-scheduled-content.js
@@ -3,12 +3,17 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 fragment StandardWebsiteScheduleQueryFragment on Content {
+  id
+  type
+  name
   shortName
   teaser
   canonicalPath
+  published
   primarySection {
     id
     name
+    fullName
     canonicalPath
   }
   primaryImage {
