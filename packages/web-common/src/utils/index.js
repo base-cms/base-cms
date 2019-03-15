87ec2b104248dc8b939811f593fd2e66b96d03ce
Jacob Bare <jacob@limit0.io>
2019-03-15 09:40:25 -0500

    Export new utilities

--- packages/web-common/src/utils/index.js
+++ packages/web-common/src/utils/index.js
@@ -1,15 +1,19 @@
+const buildImgixQuery = require('./build-imgix-query');
 const elementClassNames = require('./element-class-names');
 const extractFragmentData = require('./extract-fragment-data');
 const extractFragmentName = require('./extract-fragment-name');
 const linkClassNames = require('./link-class-names');
 const modelClassNames = require('./model-class-names');
+const objectTypeName = require('./object-type-name');
 const sectionPath = require('./section-path');
 
 module.exports = {
+  buildImgixQuery,
   elementClassNames,
   extractFragmentData,
   extractFragmentName,
   linkClassNames,
   modelClassNames,
+  objectTypeName,
   sectionPath,
 };
