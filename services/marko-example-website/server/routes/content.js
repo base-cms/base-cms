d860ad2700c2ee1ab515aa9bf96a7d94751238b4
Jacob Bare <jacob@limit0.io>
2019-03-15 14:18:38 -0500

    Add content images and faux load more

--- services/marko-example-website/server/routes/content.js
+++ services/marko-example-website/server/routes/content.js
@@ -1,8 +1,18 @@
+const gql = require('graphql-tag');
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
 
 module.exports = (app) => {
   app.get('/:prefix(*):id(\\d{8}):suffix(*)', withContent({
     template: content,
+    queryFragment: gql`
+      fragment ContentPageFragment on Content {
+        primaryImage {
+          id
+          src
+          alt
+        }
+      }
+    `,
   }));
 };
