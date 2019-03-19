06cbfec6599874942a9343cdb70b2797ecc0352b
Jacob Bare <jacob@limit0.io>
2019-03-19 10:07:50 -0500

    Add embed constants

---
+++ packages/embedded-media/src/constants.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /**
+   * The ending database tag.
+   */
+  DB_TAG_END: ']}%',
+
+  /**
+   * The starting database tag.
+   */
+  DB_TAG_START: '%{[',
+
+  /**
+   * The default HTML element to use for HTML tags.
+   */
+  DEFAULT_HTML_ELEMENT: 'aside',
+
+  /**
+   * The attribute name that siginfies the html element the tag should use.
+   */
+  TAG_ELEMENT_ATTR_NAME: 'data-embed-element',
+
+  /**
+   * The attribute name that siginfies the tag id.
+   */
+  TAG_ID_ATTR_NAME: 'data-embed-id',
+
+  /**
+   * The attribute name that siginfies the tag type.
+   */
+  TAG_TYPE_ATTR_NAME: 'data-embed-type',
+
+  /**
+   * The attribute name that siginfies a tag is in an error state.
+   */
+  TAG_INVALID_ATTR_NAME: 'data-embed-invalid',
+
+  /**
+   * The attribute name that siginfies a tag is in a disabled HTML display state.
+   */
+  TAG_DISABLED_ATTR_NAME: 'data-embed-disabled',
+};
