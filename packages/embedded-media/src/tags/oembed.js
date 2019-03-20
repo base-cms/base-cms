4a0547b2c8d7d5cb63474854ab9965a365dc541e
Jacob Bare <jacob@limit0.io>
2019-03-20 08:14:09 -0500

    Remove console

--- packages/embedded-media/src/tags/oembed.js
+++ packages/embedded-media/src/tags/oembed.js
@@ -5,9 +5,7 @@ class OEmbedTag extends AbstractTag {
   async buildHtmlTagContents() {
     const maxwidth = this.getAttribute('maxwidth');
     const maxheight = this.getAttribute('maxheight');
-
     const data = await oembed.extract(this.identifier, { maxwidth, maxheight });
-    console.info(data);
     return data && data.html ? data.html : '<!-- invalid oembed -->';
   }
 }
