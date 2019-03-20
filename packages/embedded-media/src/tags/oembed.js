b78098a8f0396f6fc45c99175662e548bdc55b21
Jacob Bare <jacob@limit0.io>
2019-03-20 08:13:43 -0500

    Add initial oembed tags

--- packages/embedded-media/src/tags/oembed.js
+++ packages/embedded-media/src/tags/oembed.js
@@ -1,9 +1,14 @@
+const oembed = require('oembed-parser');
 const AbstractTag = require('./abstract-tag');
 
 class OEmbedTag extends AbstractTag {
   async buildHtmlTagContents() {
-    console.log('buildHtmlTag', this.identifier, this.attributes);
-    return '<!-- Embedded HTML tag here -->';
+    const maxwidth = this.getAttribute('maxwidth');
+    const maxheight = this.getAttribute('maxheight');
+
+    const data = await oembed.extract(this.identifier, { maxwidth, maxheight });
+    console.info(data);
+    return data && data.html ? data.html : '<!-- invalid oembed -->';
   }
 }
 
