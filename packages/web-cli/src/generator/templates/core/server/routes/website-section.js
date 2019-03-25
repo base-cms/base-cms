ec0b35fe1a96785645a6e91d472d61a2a0c62d38
Jacob Bare <jacob@limit0.io>
2019-03-25 16:10:33 -0500

    Make website section routing less aggresive

--- packages/web-cli/src/generator/templates/core/server/routes/website-section.js
+++ packages/web-cli/src/generator/templates/core/server/routes/website-section.js
@@ -2,7 +2,7 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
-  app.get('/:alias(*)', withWebsiteSection({
+  app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
   }));
 };
