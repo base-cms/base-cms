2124304778e1b4177d2f92cfd27363268ebec012
Jacob Bare <jacob@limit0.io>
2019-03-25 07:42:09 -0500

    Adjust load more button param names; add classes

--- services/marko-example-website/client/load-more-button.vue
+++ services/marko-example-website/client/load-more-button.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-if="error">
+  <div v-bind:class="wrappingClassObj" v-if="error">
     <pre>An unexpected error occurred: {{ error.message }}</pre>
     <pre>{{ error.stack }}</pre>
   </div>
-  <div v-else>
-    <button v-on:click="load()" v-bind:disabled="loading">
+  <div v-bind:class="wrappingClassObj" v-else>
+    <button v-on:click="load()" v-bind:disabled="loading" v-bind:class="buttonClassObj">
       <template v-if="loading">
         Loading...
       </template>
       <template v-else>
-        Load More
+        {{ label }}
       </template>
     </button>
   </div>
@@ -19,13 +19,25 @@
 import setInnerHTML from './utils/set-inner-html';
 
 export default {
-  props: ['tag', 'params'],
+  props: {
+    blockName: {
+      type: String,
+      required: true,
+    },
+    params: Object,
+    buttonClassObj: Object,
+    wrappingClassObj: Object,
+    label: {
+      type: String,
+      default: 'Load More',
+    },
+  },
   data: () => ({ loading: false, error: null }),
   methods: {
     async load() {
       this.error = null;
       this.loading = true;
-      const href = `/load-more/${this.tag}?q=${encodeURIComponent(JSON.stringify(this.params))}`
+      const href = `/load-more/${this.blockName}?q=${encodeURIComponent(JSON.stringify(this.params))}`
       try {
         const r = await fetch(href);
         const html = await r.text();
--- services/marko-example-website/server/routes/load-more.js
+++ services/marko-example-website/server/routes/load-more.js
@@ -1,16 +1,16 @@
 const loadMore = require('../templates/load-more');
 const contentBlockSubPageA = require('../components/blocks/content/sub-page-a');
 
-const tags = {
+const blocks = {
   'content-block-sub-page-a': contentBlockSubPageA,
 };
 
 module.exports = (app) => {
-  app.get('/load-more/:tag', (req, res) => {
+  app.get('/load-more/:blockName', (req, res) => {
     const { query, params } = req;
     const input = JSON.parse(query.q || null) || {};
     res.marko(loadMore, {
-      tag: tags[params.tag],
+      block: blocks[params.blockName],
       input,
     });
   });
