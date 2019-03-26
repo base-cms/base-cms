786ec1fd01bcf58d0700a2788b9bfba53143f845
Josh Worden <solocommand@gmail.com>
2019-03-26 16:39:17 -0500

    Merge file tree before copying

--- packages/web-cli/src/generator/index.js
+++ packages/web-cli/src/generator/index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const log = require('fancy-log');
-const { join } = require('path');
+const { join, resolve } = require('path');
 const buildFiles = require('./build-files');
 const { mkdir, copy, write } = require('../utils/filesystem');
 const createPkg = require('./create-pkg');
@@ -25,23 +25,31 @@ const writeTemplates = async (dir, answers, templateDir) => {
 };
 
 module.exports = async (dir, answers) => {
-  const { projectName } = answers;
+  const { projectName, withBootstrap, templateDir } = answers;
   log(chalk`Creating {cyan ${projectName}} in {green ${dir}}...`);
 
   const pkg = createPkg(answers);
 
-  log(chalk`Creating {green core} templates`);
-  await writeTemplates(dir, answers, join(__dirname, 'templates/core'));
+  const dirs = [join(__dirname, 'templates/core')];
+  if (withBootstrap) dirs.push(join(__dirname, 'templates/bootstrap'));
+  if (templateDir) dirs.push(resolve(templateDir));
 
-  if (answers.withBootstrap) {
-    log(chalk`Creating {green bootstrap} templates`);
-    await writeTemplates(dir, answers, join(__dirname, 'templates/bootstrap'));
-  }
+  const { targets, files } = await buildFiles(dirs, answers);
 
-  if (answers.templateDir) {
-    log(chalk`Creating {green custom} templates`);
-    await writeTemplates(dir, answers, answers.templateDir);
-  }
+  targets.forEach(target => mkdir(dir, target));
+  files.forEach((file) => {
+    const { src } = file;
+    const dest = join(dir, file.target);
+    if (file.contents) {
+      write(dest, file.file, file.contents);
+    } else {
+      let destination = join(dest, file.file);
+      if (/gitignore$/.test(file.file)) {
+        destination = join(dest, file.file.replace('gitignore', '.gitignore'));
+      }
+      copy(src, destination);
+    }
+  });
 
   write(dir, 'package.json', `${JSON.stringify(pkg, null, 2)}\n`);
 };
