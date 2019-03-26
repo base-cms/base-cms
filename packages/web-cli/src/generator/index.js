e7d7ad7f01c1ab684a9837609f3f65d47f7ea421
Josh Worden <solocommand@gmail.com>
2019-03-26 14:03:37 -0500

    Support writing bootstrap templates

--- packages/web-cli/src/generator/index.js
+++ packages/web-cli/src/generator/index.js
@@ -5,13 +5,7 @@ const buildFiles = require('./build-files');
 const { mkdir, copy, write } = require('../utils/filesystem');
 const createPkg = require('./create-pkg');
 
-module.exports = async (dir, answers) => {
-  const { projectName } = answers;
-  log(chalk`Creating {cyan ${projectName}} ...`);
-
-  const pkg = createPkg(answers);
-
-  const templateDir = join(__dirname, 'templates/core');
+const writeTemplates = async (dir, answers, templateDir) => {
   const toWrite = await buildFiles(templateDir, answers);
 
   toWrite.targets.forEach(target => mkdir(dir, target));
@@ -28,5 +22,21 @@ module.exports = async (dir, answers) => {
       copy(join(src, file), destination);
     }
   });
+};
+
+module.exports = async (dir, answers) => {
+  const { projectName } = answers;
+  log(chalk`Creating {cyan ${projectName}} in {green ${dir}}...`);
+
+  const pkg = createPkg(answers);
+
+  log(chalk`Creating {green core} templates`);
+  await writeTemplates(dir, answers, join(__dirname, 'templates/core'));
+
+  if (answers.withBootstrap) {
+    log(chalk`Creating {green bootstrap} templates`);
+    await writeTemplates(dir, answers, join(__dirname, 'templates/bootstrap'));
+  }
+
   write(dir, 'package.json', `${JSON.stringify(pkg, null, 2)}\n`);
 };
