3b5b80d9577b846208d3429131e82e0c0f779156
Jacob Bare <jacob@limit0.io>
2019-03-27 11:22:44 -0500

    Use site config for ads and create wrapper

--- services/marko-example-website/config/site.js
+++ services/marko-example-website/config/site.js
@@ -16,6 +16,13 @@ module.exports = {
     { to: '/page/advertise', label: 'Advertise' },
     { to: '/contact-us', label: 'Contact Us' },
   ],
+  ad: {
+    units: {
+      BS: { path: '/21687441225/Officer/Officer_BS', size: [[970, 250], [970, 90]] },
+      MR: { path: '/21687441225/Officer/Officer_MR', size: [300, 250] },
+      HP: { path: '/21687441225/Officer/Officer_HP', size: [300, 600] },
+    },
+  },
   // logo: 'https://cdn.evaluationengineering.com/files/base/ebm/ee/image/static/logo/site_logo_bare.png',
   // primaryNavItems: [
   //   { to: '/applications', label: 'Applications' },
