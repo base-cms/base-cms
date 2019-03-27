e786c5781552973bedadd9841c209a0699a895b3
Jacob Bare <jacob@limit0.io>
2019-03-27 12:55:11 -0500

    Clean custom key/values per GAM specs

--- packages/marko-web/src/components/ads/gam/utils/build-targeting.js
+++ packages/marko-web/src/components/ads/gam/utils/build-targeting.js
@@ -2,12 +2,17 @@ const { isObject } = require('@base-cms/utils');
 
 const { isArray } = Array;
 
+const invalid = /["'=!+#*~;^()[\]<>,.&]/g;
+
+const clean = value => `${value || ''}`.replace(invalid, '').trim();
+
 module.exports = (targeting) => {
   if (!isObject(targeting)) return '';
   return Object.keys(targeting).map((key) => {
     const value = targeting[key];
-    const v = isArray(value) ? value : [value];
-    // @todo targeting values must be cleaned!
-    return `setTargeting('${key}', ${JSON.stringify(v)})`;
+    const values = isArray(value) ? value : [value];
+    // Clean the key and the values.
+    // Per https://support.google.com/admanager/answer/177381
+    return `setTargeting('${clean(key)}', ${JSON.stringify(values.map(clean))})`;
   }).join('.');
 };
