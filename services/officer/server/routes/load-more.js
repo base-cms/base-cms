02cdab6de45429cd0193114f1828256cf6e72507
Jacob Bare <jacob@limit0.io>
2019-03-27 15:40:03 -0500

    Commit officer (will be deleted)

--- services/officer/server/routes/load-more.js
+++ services/officer/server/routes/load-more.js
@@ -1,7 +1,10 @@
 const { withLoadMore } = require('@base-cms/marko-web/middleware');
+const contentSubPageA = require('../components/content-sub-page-a');
 
 // Register blocks that support load more...
-const blocks = {};
+const blocks = {
+  'content-sub-page-a': contentSubPageA,
+};
 
 module.exports = (app) => {
   app.get('/load-more/:blockName', withLoadMore({
