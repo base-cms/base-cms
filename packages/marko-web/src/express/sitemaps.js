d7f46371a2bda30efc594b9ad793d5ed0fa3212e
Josh Worden <solocommand@gmail.com>
2019-04-04 13:15:44 -0500

    Point marko web routes to proxy to sitemap-handler

--- packages/marko-web/src/express/sitemaps.js
+++ packages/marko-web/src/express/sitemaps.js
@@ -1,17 +1,3 @@
-module.exports = (app) => {
-  /**
-   * @todo Implement this using the remote sitemap service.
-   */
-  app.get('/sitemap.xml', (req, res) => {
-    res.append('Content-Type', 'text/xml; charset=UTF-8');
-    res.send('<?xml version="1.0" encoding="utf-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></sitemapindex>');
-  });
+const proxy = require('express-http-proxy');
 
-  /**
-   * @todo Implement this using the remote sitemap service.
-   */
-  app.get('/sitemap-google-news.xml', (req, res) => {
-    res.append('Content-Type', 'text/xml; charset=UTF-8');
-    res.send('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"></urlset>');
-  });
-};
+module.exports = app => app.use('/sitemap', proxy('sitemap-handler'));
