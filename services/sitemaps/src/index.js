5af8def7cc33add043e6a09550a9be838331e469
Josh Worden <solocommand@gmail.com>
2019-04-08 12:12:17 -0500

    Use incoming host and req parser

--- services/sitemaps/src/index.js
+++ services/sitemaps/src/index.js
@@ -3,7 +3,7 @@ const micro = require('micro');
 const { retrieve, generate, googleNews } = require('./routes');
 
 const server = micro(async (req, res) => {
-  if (req.url === '/sitemap-google-news.xml') return googleNews(res);
+  if (req.url === '/sitemap-google-news.xml') return googleNews(req, res);
   if (/^\/sitemap(?:.*).xml$/.test(req.url)) return retrieve(res, req.url);
   if (/^\/generate$/.test(req.url)) return generate(req, res);
   throw micro.createError(404, 'Not found');
--- services/sitemaps/src/routes/google-news.js
+++ services/sitemaps/src/routes/google-news.js
@@ -1,4 +1,11 @@
+const moment = require('moment');
+const {
+  content: canonicalPathFor,
+  requestParser,
+} = require('@base-cms/canonical-path');
 const { getLatestNews } = require('../db/base');
+const { GOOGLE_NEWS_PUBLICATION } = require('../env');
+
 const formatter = (docs = []) => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
 ${docs.reduce((str, doc) => `${str}  <url>
@@ -19,9 +26,18 @@ ${docs.reduce((str, doc) => `${str}  <url>
  * Returns up to 1000 News, PressRelease, or Blog content published in the last 5 days.
  * https://support.google.com/news/publisher-center/answer/74288?hl=en
  */
-module.exports = async (res) => {
+module.exports = async (req, res) => {
+  const { host } = req.headers;
+  const baseUri = `http://${host}`;
+  const canonicalRules = requestParser(req);
   const news = await getLatestNews();
   res.setHeader('X-Robots-Tag', 'noindex');
   res.setHeader('Content-Type', 'text/xml');
-  res.end(formatter(news));
+  const toFormat = await Promise.all(news.map(async (content) => {
+    const path = await canonicalPathFor(content, { canonicalRules });
+    const canonicalPath = `${baseUri}${path}`;
+    return { ...content, canonicalPath };
+  }));
+  console.log(toFormat);
+  res.end(formatter(toFormat));
 };
