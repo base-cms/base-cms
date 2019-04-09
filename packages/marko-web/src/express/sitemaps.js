5e2dc32c6a863adca9e4a58491b5dc0ccd5d6e1a
Josh Worden <solocommand@gmail.com>
2019-04-09 08:18:39 -0500

    Error if service uri is missing

--- packages/marko-web/src/express/sitemaps.js
+++ packages/marko-web/src/express/sitemaps.js
@@ -1,5 +1,6 @@
 const proxy = require('express-http-proxy');
 
-const SITEMAP_SERVICE_URI = process.env.SITEMAP_SERVICE_URI || 'http://sitemaps';
+const SERVICE_URI = process.env.SITEMAP_SERVICE_URI;
+if (!SERVICE_URI) throw new Error('Missing required environment variable: SITEMAP_SERVICE_URI.');
 
-module.exports = app => app.use('/sitemap', proxy(SITEMAP_SERVICE_URI));
+module.exports = app => app.use('/sitemap', proxy(SERVICE_URI));
