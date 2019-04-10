64e73dab811a916501b96c5598df159aeb6cabcd
Josh Worden <solocommand@gmail.com>
2019-04-10 13:30:41 -0500

    adjust prefixing

--- services/sitemaps/src/routes/index.js
+++ services/sitemaps/src/routes/index.js
@@ -4,10 +4,12 @@ const sitemapContent = require('./sitemap-content');
 const sitemapGoogleNews = require('./sitemap-google-news');
 const health = require('./_health');
 
+const prefix = '([a-z0-9-/]*)?';
+
 module.exports = (app) => {
   health(app);
-  app.get('([a-z0-9-/]*)?/sitemap.xml', sitemap);
-  app.get('([a-z0-9-/]*)?/sitemap-google-news.xml', sitemapGoogleNews);
-  app.get('([a-z0-9-/]*)?/sitemap/sections.xml', sitemapSections);
-  app.get('([a-z0-9-/]*)?/sitemap/*.xml', sitemapContent);
+  app.get(`${prefix}/sitemap.xml`, sitemap);
+  app.get(`${prefix}/sitemap-google-news.xml`, sitemapGoogleNews);
+  app.get(`${prefix}/sitemap/sections.xml`, sitemapSections);
+  app.get(`${prefix}/sitemap/*.xml`, sitemapContent);
 };
