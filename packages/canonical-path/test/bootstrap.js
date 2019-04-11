668ef24ae08ddb9acbb7684f25248a18b294f447
Josh Worden <solocommand@gmail.com>
2019-04-11 10:01:45 -0500

    Remove extra test deps

--- packages/canonical-path/test/bootstrap.js
+++
@@ -1,7 +0,0 @@
-global.Promise = require('bluebird');
-global.chai = require('chai');
-global.request = require('supertest');
-global.sinon = require('sinon');
-
-global.expect = global.chai.expect;
-global.chai.use(require('chai-as-promised'));
