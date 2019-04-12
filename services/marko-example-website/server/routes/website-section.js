c52930253c473b3cbf81c8c2c3734aeefd2fe50d
Jacob Bare <jacob@limit0.io>
2019-04-11 19:12:26 -0500

    Add page contexts to home, section, and content

--- services/marko-example-website/server/routes/website-section.js
+++ services/marko-example-website/server/routes/website-section.js
@@ -1,13 +1,26 @@
+const gql = require('graphql-tag');
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('../templates/website-section');
 const applications = require('../templates/website-section/applications');
 
+const queryFragment = gql`
+  fragment WebsiteSectionPageFragment on WebsiteSection {
+    fullName
+    hierarchy {
+      id
+      name
+    }
+  }
+`;
+
 module.exports = (app) => {
   app.get('/:alias(tactical)', withWebsiteSection({
+    queryFragment,
     template: applications,
   }));
 
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
+    queryFragment,
     template: section,
   }));
 };
