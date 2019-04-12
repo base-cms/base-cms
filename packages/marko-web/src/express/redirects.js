399ca3800779914f75b220609361c428d58eff7e
Josh Worden <solocommand@gmail.com>
2019-04-12 11:39:50 -0500

    Remove logs

--- packages/marko-web/src/express/redirects.js
+++ packages/marko-web/src/express/redirects.js
@@ -1,8 +1,6 @@
 const { asyncRoute } = require('@base-cms/utils');
 const gql = require('graphql-tag');
 
-const { log } = console;
-
 const query = gql`
   query WebsiteRedirect($input: WebsiteRedirectQueryInput!) {
     websiteRedirect(input: $input) {
@@ -14,14 +12,11 @@ const query = gql`
 
 module.exports = asyncRoute(async (req, res) => {
   const { apollo, path: from } = req;
-  log('checking redirect', { from });
   const variables = { input: { from } };
   const { data } = await apollo.query({ query, variables });
   const { websiteRedirect } = data;
-  log('redirect result', { websiteRedirect });
   if (websiteRedirect) {
     const { to, code } = websiteRedirect;
-    log('redirecting', { to, code });
     res.redirect(code, to);
   }
 });
