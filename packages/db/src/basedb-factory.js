aaf5caa2f7d8bb420c0f0f1288ad9b2af5cc0a2e
Josh Worden <solocommand@gmail.com>
2019-04-26 11:11:33 -0500

    create client and db factories

---
+++ packages/db/src/basedb-factory.js
@@ -0,0 +1,22 @@
+const { inspect } = require('util');
+const BaseDB = require('./basedb');
+
+const { NODE_ENV, ENABLE_BASEDB_LOGGING } = process.env;
+const { log } = console;
+
+const logger = (obj) => {
+  log('');
+  Object.keys(obj).forEach(key => log(`${key}:`, inspect(obj[key], { colors: true, depth: 5 })));
+  log('');
+};
+
+const shouldLog = () => {
+  if (!NODE_ENV !== 'development') return false;
+  return ENABLE_BASEDB_LOGGING;
+};
+
+module.exports = ({ tenant, client }) => new BaseDB({
+  tenant,
+  client,
+  logger: shouldLog() ? logger : undefined,
+});
--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,7 +1,11 @@
 const BaseDB = require('./basedb');
 const MongoDB = require('./mongodb');
+const mongodbFactory = require('./mongodb-factory');
+const basedbFactory = require('./basedb-factory');
 
 module.exports = {
   BaseDB,
   MongoDB,
+  mongodbFactory,
+  basedbFactory,
 };
