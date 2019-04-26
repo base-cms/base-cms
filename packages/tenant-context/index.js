d9adb5d94c661f53b8fccd7ae56f372195508304
Josh Worden <solocommand@gmail.com>
2019-04-26 09:58:03 -0500

    create tenant-context package

---
+++ packages/tenant-context/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  getFromRequest: (req) => {
+    const tenant = req.get('x-tenant-key');
+    const imageHost = req.get('x-cdn-image-hostname');
+    const assetHost = req.get('x-cdn-asset-hostname');
+    if (!tenant) throw new Error('A required header `x-tenant-key` was not sent!');
+    if (!imageHost) throw new Error('A required header `x-cdn-image-hostname` was not sent!');
+    if (!assetHost) throw new Error('A required header `x-cdn-asset-hostname` was not sent!');
+    return { tenant, imageHost, assetHost };
+  },
+  buildRequestHeaders: (config = {}) => {
+    const { tenantKey, cdnImageHostname, cdnAssetHostname } = config;
+    if (!tenantKey) throw new Error('A required configuration value `tenantKey` was not sent!');
+    if (!cdnImageHostname) throw new Error('A required configuration value `cdnImageHostname` was not sent!');
+    if (!cdnAssetHostname) throw new Error('A required configuration value `cdnAssetHostname` was not sent!');
+    return {
+      'x-tenant-key': tenantKey,
+      'x-cdn-image-hostname': cdnImageHostname,
+      'x-cdn-asset-hostname': cdnAssetHostname,
+    };
+  },
+};
