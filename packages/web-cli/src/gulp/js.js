1b3e5e6f50ce1c56260dbc3ab53463f3b6dd87dc
Josh Worden <solocommand@gmail.com>
2019-04-30 10:58:31 -0500

    Support for customizing babel build from rcfile

--- packages/web-cli/src/gulp/js.js
+++ packages/web-cli/src/gulp/js.js
@@ -7,14 +7,52 @@ const webpack = require('webpack-stream');
 const wp = require('webpack');
 const { dest, src } = require('gulp');
 const { getIfUtils } = require('webpack-config-utils');
+const { existsSync } = require('fs');
+const { join } = require('path');
 const completeTask = require('../utils/task-callback');
 
 const absoluteRuntime = path.dirname(require.resolve('@babel/runtime/package.json'));
+
+/**
+ * Loads an RC file from the CWD, if present, to provide gulp configuration
+ * @param {String} cwd The current directory
+ */
+const readRcFile = (cwd) => {
+  const excludeFn = file => (
+    /node_modules/.test(file)
+    && !/\.vue\.js/.test(file)
+  );
+
+  const defaultTargets = {
+    browsers: [
+      'Chrome >= 49',
+      'Firefox >= 45',
+      'Safari >= 10',
+      'Edge >= 12',
+      'Explorer >= 11',
+      'iOS >= 10',
+    ],
+  };
+
+  // eslint-disable-next-line import/no-dynamic-require
+  const rc = existsSync(join(cwd, '.basecmsrc.js')) ? require(join(cwd, '.basecmsrc.js')) : {};
+  const exclude = rc && rc.babelLoader && typeof rc.babelLoader.exclude === 'function'
+    ? rc.babelLoader.exclude : excludeFn;
+
+  const targets = rc && rc.babelLoader && rc.babelLoader.targets
+    ? rc.babelLoader.targets : defaultTargets;
+
+  const debug = rc && rc.babelLoader && typeof rc.babelLoader.debug === 'boolean'
+    ? rc.babelLoader.debug : false;
+
+  return { exclude, targets, debug };
+};
 
 // @todo Determine how to combine CSS with the main CSS build
 // @todo Add sass to Vue components
 // @todo Add default polyfills to entry point
 module.exports = cwd => (cb) => {
+  const { exclude, targets, debug } = readRcFile(cwd);
   const { ifProduction, ifNotProduction } = getIfUtils(process.env.NODE_ENV);
   pump([
     src('browser/index.js', { cwd }),
@@ -37,10 +75,7 @@ module.exports = cwd => (cb) => {
           {
             test: /\.js$/,
             loader: require.resolve('babel-loader'),
-            exclude: file => (
-              /node_modules/.test(file)
-              && !/\.vue\.js/.test(file)
-            ),
+            exclude,
             options: {
               presets: [
                 [
@@ -49,17 +84,9 @@ module.exports = cwd => (cb) => {
                     modules: false,
                     useBuiltIns: 'usage',
                     corejs: 2,
-                    targets: {
-                      browsers: [
-                        'Chrome >= 49',
-                        'Firefox >= 45',
-                        'Safari >= 10',
-                        'Edge >= 12',
-                        'Explorer >= 11',
-                        'iOS >= 10',
-                      ],
-                    },
+                    targets,
                     loose: false,
+                    debug,
                   },
                 ],
               ],
