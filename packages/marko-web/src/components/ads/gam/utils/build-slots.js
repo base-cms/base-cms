b24541766987768b50269e2b9d183bb57e625685
Josh Worden <solocommand@gmail.com>
2019-05-10 13:53:53 -0500

    Support both OOP and regular ad slots

--- packages/marko-web/src/components/ads/gam/utils/build-slots.js
+++ packages/marko-web/src/components/ads/gam/utils/build-slots.js
@@ -3,18 +3,26 @@ const buildSizeMapping = require('./build-size-mapping');
 
 const { isArray } = Array;
 
-module.exports = (slots) => {
-  if (!isObject(slots)) return [];
-  return Object.keys(slots)
-    .map(id => ({ ...slots[id], id }))
-    .filter(slot => slot.path && isArray(slot.size))
-    .map(({
+module.exports = (adUnits) => {
+  const definitions = [];
+  if (!isObject(adUnits)) return definitions;
+
+  const slots = Object.keys(adUnits).map(id => ({ ...adUnits[id], id }));
+
+  slots.filter(slot => slot.path && slot.outOfPage)
+    .forEach(({ path, id }) => {
+      definitions.push(`googletag.defineOutOfPageSlot('${path}', '${id}').addService(googletag.pubads());`);
+    });
+
+  slots.filter(slot => slot.path && isArray(slot.size))
+    .forEach(({
       id,
       path,
       size,
       sizeMapping,
     }) => {
       const mappings = buildSizeMapping(sizeMapping);
-      return `googletag.defineSlot('${path}', ${JSON.stringify(size)}, '${id}')${mappings}.addService(googletag.pubads());`;
+      definitions.push(`googletag.defineSlot('${path}', ${JSON.stringify(size)}, '${id}')${mappings}.addService(googletag.pubads());`);
     });
+  return definitions;
 };
