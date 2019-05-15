a851a5b69f4aef899d471ab62b159e82f059aed0
Josh Worden <solocommand@gmail.com>
2019-05-15 10:08:51 -0500

    Move NR notify to service deploy

--- scripts/deploy-notify.sh
+++ scripts/deploy-notify.sh
@@ -10,14 +10,3 @@ payload="{
 curl -f -X POST --data "$payload" \
   -H 'Content-type: application/json' \
   https://hooks.slack.com/services/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
-
-payload="{
-  \"deployment\": {
-    \"revision\": \"\`$TRAVIS_TAG\`\",
-    \"user\": \"TravisCD\"
-  }
-}"
-curl -f -X POST --data "$payload" \
-  -H 'Content-type: application/json' \
-  -H "X-Api-Key:$NR_APIKEY" \
-  https://api.newrelic.com/v2/applications/222815958/deployments.json
