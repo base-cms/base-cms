1f3a3df1f20420f41b85f6591a29a308387cc3be
Jacob Bare <jacob@limit0.io>
2019-05-16 14:26:22 -0500

    Throw error if tenant cannot be found

--- services/hooks/src/app.js
+++ services/hooks/src/app.js
@@ -14,6 +14,9 @@ app.post('/platform-history', asyncRoute(async (req, res) => {
   const db = basedb(tenant)[stack];
   if (!db) throw new Error(`No database found for '${stack}'`);
 
+  const exists = await db.tenantExists();
+  if (!exists) throw new Error(`The tenant '${tenant}' was not found on stack '${stack}'`);
+
   const result = await handle(db, history);
   res.json(result);
 }));
