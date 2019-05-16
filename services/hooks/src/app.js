85b5e167184f22ffc5eb0aefa98844d4ad133f7a
Jacob Bare <jacob@limit0.io>
2019-05-16 13:57:32 -0500

    Add platform-history route handling

--- services/hooks/src/app.js
+++ services/hooks/src/app.js
@@ -1,14 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-
-const { log } = console;
+const { asyncRoute } = require('@base-cms/utils');
+const basedb = require('./basedb');
+const handle = require('./platform-history');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/platform-history', (req, res) => {
-  log(req.body);
-  res.json(req.body);
-});
+app.post('/platform-history', asyncRoute(async (req, res) => {
+  const { body } = req;
+  const { stack, context_key: tenant, history } = body;
+  if (!tenant) throw new Error('No context_key (tenant) was provided.');
+  const db = basedb(tenant)[stack];
+  if (!db) throw new Error(`No database found for '${stack}'`);
+
+  const result = await handle(db, history);
+  res.json(result);
+}));
 
 module.exports = app;
---
+++ services/hooks/src/platform-history/index.js
@@ -0,0 +1,5 @@
+module.exports = async (db, history) => {
+  console.log(db);
+  console.log(history);
+  return {};
+};
