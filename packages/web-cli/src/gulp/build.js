db9789bea01ade6f2e6934ca8672053c0186b9c3
Josh Worden <solocommand@gmail.com>
2019-05-16 15:40:30 -0500

    clenup temp dir

--- packages/web-cli/src/gulp/build.js
+++ packages/web-cli/src/gulp/build.js
@@ -2,8 +2,12 @@ const {
   parallel,
   series,
 } = require('gulp');
-const clean = require('./clean');
+const del = require('del');
 const css = require('./css');
 const js = require('./js');
 
-module.exports = cwd => series(clean(cwd), parallel(css(cwd), js(cwd)));
+module.exports = cwd => series(
+  () => del('dist/**/*', { cwd }),
+  parallel(css(cwd), js(cwd)),
+  () => del('dist/tmp', { cwd }),
+);
