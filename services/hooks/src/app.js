412b944e6c374d4871c054c22ceb65f9457127a7
Jacob Bare <jacob@limit0.io>
2019-05-17 09:56:52 -0500

    Create get-db util and use in route

--- services/hooks/src/app.js
+++ services/hooks/src/app.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const { asyncRoute } = require('@base-cms/utils');
-const basedb = require('./basedb');
+const getDB = require('./platform-history/utils/get-db');
 const handle = require('./platform-history');
 
 const app = express();
@@ -10,13 +10,7 @@ app.use(bodyParser.json());
 app.post('/platform-history', asyncRoute(async (req, res) => {
   const { body } = req;
   const { stack, context_key: tenant, history } = body;
-  if (!tenant) throw new Error('No context_key (tenant) was provided.');
-  const db = basedb(tenant)[stack];
-  if (!db) throw new Error(`No database found for '${stack}'`);
-
-  const exists = await db.tenantExists();
-  if (!exists) throw new Error(`The tenant '${tenant}' was not found on stack '${stack}'`);
-
+  const db = await getDB(stack, tenant);
   const result = await handle(db, history);
   res.json(result);
 }));
