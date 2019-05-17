ae280e8f82b785f0ac6008d29933cc331188777f
Josh Worden <solocommand@gmail.com>
2019-05-17 09:55:07 -0500

    Use gulp-rev-all to rewrite sourcemap refs

--- packages/web-cli/src/gulp/manifest.js
+++ packages/web-cli/src/gulp/manifest.js
@@ -1,16 +1,16 @@
 const pump = require('pump');
-const rev = require('gulp-rev');
+const revall = require('gulp-rev-all');
 const del = require('gulp-rev-delete-original');
 const { dest, src } = require('gulp');
 const completeTask = require('../utils/task-callback');
 
 module.exports = cwd => (cb) => {
   pump([
-    rev(),
     src('dist/**/*', { cwd }),
+    revall.revision({ includeFilesInManifest: ['.css', '.js'] }),
     del(),
     dest('dist', { cwd }),
-    rev.manifest(),
+    revall.manifestFile(),
     dest('dist', { cwd }),
   ], e => completeTask(e, cb));
 };
