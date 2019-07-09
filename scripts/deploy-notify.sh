81cb4dce5569dba5d0112824acc522b0a3b697c7
Josh Worden <solocommand@gmail.com>
2019-07-09 14:22:56 -0500

    Update slack notifications to use environment vars

--- scripts/deploy-notify.sh
+++ scripts/deploy-notify.sh
@@ -8,6 +8,4 @@ payload="{
     \"text\": \"Deployment of \`$TRAVIS_REPO_SLUG\` @ \`$TRAVIS_TAG\` to \`$ENVIRONMENT\` has finished.\"
   }]
 }"
-curl -f -X POST --data "$payload" \
-  -H 'Content-type: application/json' \
-  https://hooks.slack.com/services/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
+curl -f -X POST --data "$payload" -H 'Content-type: application/json' $SLACK_WEBHOOK_URL
