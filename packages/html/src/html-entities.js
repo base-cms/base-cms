f088932d40ee25d4389372284ea53360d5001748
Jacob Bare <jacob@limit0.io>
2019-07-18 08:53:51 -0500

    Create html-entities utility

---
+++ packages/html/src/html-entities.js
@@ -0,0 +1,3 @@
+const { Html5Entities } = require('html-entities');
+
+module.exports = new Html5Entities();
--- packages/html/src/index.js
+++ packages/html/src/index.js
@@ -1,7 +1,9 @@
+const htmlEntities = require('./html-entities');
 const stripHtml = require('./strip-html');
 const stripTags = require('./strip-tags');
 
 module.exports = {
+  htmlEntities,
   stripHtml,
   stripTags,
 };
