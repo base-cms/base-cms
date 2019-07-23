1aeca2c454a51c2b840d7363d2ecc3641e2182a2
Jacob Bare <jacob@limit0.io>
2019-07-23 13:28:22 -0500

    Add additional imgix URL parameters

--- packages/image/src/build-imgix-url.js
+++ packages/image/src/build-imgix-url.js
@@ -2,26 +2,145 @@ const { camelize } = require('@base-cms/inflector');
 const { URL, URLSearchParams } = require('url');
 
 const params = [
-  'auto',
-  'blur',
-  'border',
-  'border-radius',
-  'border-radius-inner',
-  'crop',
-  'dpr',
-  'fit',
-  'fill-color',
-  'fp-x',
-  'fp-y',
-  'h',
-  'max-h',
-  'max-w',
-  'min-h',
-  'min-w',
-  'pad',
-  'px',
-  'trim',
-  'w',
+  // Adjustment
+  'bri', // Brightness
+  'con', // Contrast
+  'exp', // Exposure
+  'gam', // Gamma
+  'high', // Highlight
+  'hue', // Hue
+  'invert', // Invert
+  'sat', // Saturation
+  'shad', // Shadow
+  'sharp', // Sharpen
+  'usm', // Unsharp Mask
+  'usmrad', // Unsharp Mask Radius
+  'vib', // Vibrance
+
+  // Automatic
+  'auto', // Automatic
+
+  // Blending
+  'ba', // Blend Align
+  'balph', // Blend Alpha
+  'bc', // Blend Crop
+  'bf', // Blend Fit
+  'bh', // Blend Height
+  'blend', // Blend
+  'bm', // Blend Mode
+  'bp', // Blend Padding
+  'bs', // Blend Size
+  'bw', // Blend Width
+  'bx', // Blend X Position
+  'by', // Blend Y Position
+
+  // Border & Padding
+  'border-radius-inner', // Inner Border Radius
+  'border-radius', // Outher Border Radius
+  'border', // Border Size Color
+  'pad', // Padding
+
+  // Color Palette
+  'colors', // Palette Color Count
+  'palette', // Color Palette Extraction
+  'prefix', // CSS Prefix
+
+  // Face Detection
+  'facebindex', // Face Index
+  'facepad', // Face Padding
+  'faces', // JSON Face Data
+
+  // Fill
+  'bg', // Background Color
+  'fill-color', // Fill Color
+  'fill-mode', // Fill Mode
+
+  // Focal Point Crop
+  'ch', // Client Hints
+  'chromasub', // Chroma Subsampling
+  'colorquant', // Color Quantization
+  'cs', // Color Space
+  'dl', // Download
+  'dpi', // Dots Per Inch
+  'fm', // Output Format
+  'lossless', // Lossless Compression
+  'q', // Output Quality
+
+  // Mask Image
+  'corner-radius', // Mask Corner Radius
+  'mask', // Mask Type
+  'maskbg', // Mask Background Color
+
+  // Noise Reduction
+  'nr', // Noise Reduction Bound
+  'nrs', // Noise Reduction Sharpen
+
+  // PDF
+  'page', // PDF Page Number
+
+  // Pixel Density
+  'dpr', // Device Pixel Ration
+
+  // Rotation
+  'flip', // Flip Axis
+  'or', // Orientation
+  'rot', // Rotation
+
+  // Size
+  'ar', // Aspect Ratio
+  'crop', // Crop Mode
+  'fit', // Resize Fit Mode
+  'h', // Image Height
+  'max-h', // Maximum Height
+  'max-w', // Maximum Width
+  'min-h', // Minimum Height
+  'min-w', // Minimum Width
+  'rect', // Source Rectangle Region
+  'w', // Image Width
+
+  // Stylize
+  'blur', // Gaussian Blur
+  'duotone-alpha', // Duotone Alpha
+  'duotone', // Duotone
+  'htn', // Halftone
+  'mono', // Monochrome
+  'px', // Pixellate
+  'sepia', // Sepia Tone
+
+  // Text
+  'txt', // Text String
+  'txtalign', // Text Align
+  'txtclip', // Text Clipping Mode
+  'txtclr', // Text Color
+  'txtfit', // Text Fit Mode
+  'txtfont', // Text Font
+  'txtlig', // Text Ligatures
+  'txtline', // Text Outline
+  'txtlineclr', // Text Outline Color
+  'txtpad', // Text Padding
+  'txtshad', // Text Shadow
+  'txtsize', // Text Font Size
+  'txtwidth', // Text Width
+
+  // Trim
+  'trim', // Trim Image
+  'trimcolor', // Trim Color
+  'trimmd', // Trim Mean Difference
+  'trimsd', // Trim Standard Deviation
+  'trimtol', // Trim Tolerance
+
+  // Watermark
+  'mark', // Watermark Image URL
+  'markalign', // Watermark Alignment Mode
+  'markalpha', // Watermark Alpha
+  'markbase', // Watermark Base URL
+  'markfit', // Watermark Fit Mode
+  'markh', // Watermark Height
+  'markpad', // Watermark Padding
+  'markscale', // Watermark Scale
+  'markw', // Watermark Width
+  'markx', // Watermark X Position
+  'marky', // Watermark Y Position
 ];
 
 const coreDefaults = {
