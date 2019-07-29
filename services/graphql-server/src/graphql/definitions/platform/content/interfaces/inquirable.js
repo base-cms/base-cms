de1000fada8655ee8ece2f31ebff1f4e8fe031ae
Josh Worden <solocommand@gmail.com>
2019-07-29 12:51:18 -0500

    Ensure enableRmi is requested

--- services/graphql-server/src/graphql/definitions/platform/content/interfaces/inquirable.js
+++ services/graphql-server/src/graphql/definitions/platform/content/interfaces/inquirable.js
@@ -2,12 +2,11 @@ const gql = require('graphql-tag');
 
 module.exports = gql`
 
-# Renamed from 'Contacts' interface in Base Platform
 interface Inquirable {
-  # fields from platform.model::Content mutations
+  # fields from platform.interface::Contacts
   enableRmi: Boolean @projection(localField: "mutations.Website.enableRmi") @value(localField: "mutations.Website.enableRmi")
   # GraphQL-only fields
-  inquiryContacts: [ContentContact!]! @projection(localField: "salesContacts", needs: ["company", "parentCompany", "venue", "parentVenue", "supplier", "parentSupplier"])
+  inquiryContacts: [ContentContact!]! @projection(localField: "salesContacts", needs: ["mutations.Website.enableRmi", "company", "parentCompany", "venue", "parentVenue", "supplier", "parentSupplier"])
 
   # @deprecated: This field was deprecated in Base3 and should no longer be used.
   leadsDelivery: Boolean @projection(localField: "mutations.Website.leadsDelivery") @value(localField: "mutations.Website.leadsDelivery")
