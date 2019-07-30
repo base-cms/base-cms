d2df7543161f230e253427d0ed1df4e25644701d
Jacob Bare <jacob@limit0.io>
2019-07-30 14:32:34 -0500

    Add redirect handler support

--- packages/marko-web/src/express/get-redirect.js
+++ packages/marko-web/src/express/get-redirect.js
@@ -9,10 +9,22 @@ const query = gql`
   }
 `;
 
-module.exports = async (req) => {
-  const { apollo, path: from, query: params } = req;
+/**
+ * @param {object} req The Express request object.
+ * @param {function} [handler] An optional redirect handler to execute if no redirect was found.
+ *                             Must return an object of `{ to, code }` or `null`
+ */
+module.exports = async (req, handler) => {
+  const { apollo, path, query: params } = req;
+  const from = path.replace(/\/$/, '');
   const variables = { input: { from, params } };
   const { data } = await apollo.query({ query, variables });
   const { websiteRedirect } = data;
-  return websiteRedirect;
+  const { to } = websiteRedirect || {};
+  if (to) return websiteRedirect;
+  // Attempt to find a redirect using the handler.
+  if (typeof handler !== 'function') return null;
+  const result = await handler({ from, params });
+  if (!typeof result === 'object' || !result.to) return null;
+  return { ...result, code: result.code || 301 };
 };
