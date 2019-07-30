c921247d447b9650c8425eac999fa4f1c4031215
Jacob Bare <jacob@limit0.io>
2019-07-30 13:02:13 -0500

    Add inquiryEmails field to Inquirable interface

--- services/graphql-server/src/graphql/definitions/platform/content/interfaces/inquirable.js
+++ services/graphql-server/src/graphql/definitions/platform/content/interfaces/inquirable.js
@@ -6,10 +6,11 @@ interface Inquirable {
   # fields from platform.interface::Contacts
   enableRmi: Boolean @projection(localField: "mutations.Website.enableRmi") @value(localField: "mutations.Website.enableRmi")
   # GraphQL-only fields
-  inquiryContacts: [ContentContact!]! @projection(localField: "salesContacts", needs: ["mutations.Website.enableRmi", "company", "parentCompany", "venue", "parentVenue", "supplier", "parentSupplier"])
+  inquiryEmails: [String!]! @projection(localField: "salesContacts", needs: ["mutations.Website.enableRmi", "company", "parentCompany", "venue", "parentVenue", "supplier", "parentSupplier"])
 
   # @deprecated: This field was deprecated in Base3 and should no longer be used.
   leadsDelivery: Boolean @projection(localField: "mutations.Website.leadsDelivery") @value(localField: "mutations.Website.leadsDelivery")
+  inquiryContacts: [ContentContact!]! @projection(localField: "salesContacts", needs: ["mutations.Website.enableRmi", "company", "parentCompany", "venue", "parentVenue", "supplier", "parentSupplier"])
 }
 
 `;
