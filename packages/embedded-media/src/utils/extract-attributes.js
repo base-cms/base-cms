a2e85ae8c434801e50cb0c6387ebc5e3e224defc
Jacob Bare <jacob@limit0.io>
2019-08-01 11:00:52 -0500

    Encode/decode html entities within tag attrs

--- packages/embedded-media/src/utils/extract-attributes.js
+++ packages/embedded-media/src/utils/extract-attributes.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio');
+const { htmlEntities } = require('@base-cms/html');
 const { camelize } = require('@base-cms/inflector');
 const {
   DB_TAG_END,
@@ -10,10 +11,10 @@ const {
  */
 module.exports = (match) => {
   const html = match.replace(DB_TAG_START, '<span').replace(DB_TAG_END, '></span>');
-  const $ = cheerio.load(html);
+  const $ = cheerio.load(html, { decodeEntities: false });
   const obj = $('span').data() || {};
   return Object.keys(obj).reduce((o, k) => {
     const key = camelize(k.replace('embed', ''));
-    return ({ ...o, [key]: obj[k] });
+    return ({ ...o, [key]: htmlEntities.decode(obj[k]) });
   }, {});
 };
