16d2c48662368702bc773293a5849304bcc39a61
Jacob Bare <jacob@limit0.io>
2019-08-01 09:21:55 -0500

    Create extraction/match/attribute utilities

---
+++ packages/embedded-media/src/utils/extract-attributes.js
@@ -0,0 +1,19 @@
+const cheerio = require('cheerio');
+const { camelize } = require('@base-cms/inflector');
+const {
+  DB_TAG_END,
+  DB_TAG_START,
+} = require('../constants');
+
+/**
+ * @param {string} match The database tag match, e.g. `%{[ ... ]}%`.
+ */
+module.exports = (match) => {
+  const html = match.replace(DB_TAG_START, '<span').replace(DB_TAG_END, '></span>');
+  const $ = cheerio.load(html);
+  const obj = $('span').data() || {};
+  return Object.keys(obj).reduce((o, k) => {
+    const key = camelize(k.replace('embed', ''));
+    return ({ ...o, [key]: obj[k] });
+  }, {});
+};
