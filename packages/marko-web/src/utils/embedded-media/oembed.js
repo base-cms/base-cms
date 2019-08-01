4694909c9822790f6400d0d23dacfbfafd48af5a
Jacob Bare <jacob@limit0.io>
2019-08-01 12:45:30 -0500

    Generate default oembed markup

--- packages/marko-web/src/utils/embedded-media/oembed.js
+++ packages/marko-web/src/utils/embedded-media/oembed.js
@@ -1,1 +1,7 @@
-module.exports = tag => `<p><strong>EMBED ${tag.type}: ${tag.id}</strong></p>`;
+const BrowserComponent = require('../../components/browser/component');
+
+module.exports = (tag, $global) => {
+  const url = tag.id;
+  const props = { url, attrs: tag.attrs };
+  return BrowserComponent.renderToString({ $global, name: 'OEmbed', props });
+};
