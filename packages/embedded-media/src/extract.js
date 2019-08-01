19d98c9310f079c346dcddfb150fc65515adbec5
Jacob Bare <jacob@limit0.io>
2019-08-01 10:09:06 -0500

    Fix docblock typo

--- packages/embedded-media/src/extract.js
+++ packages/embedded-media/src/extract.js
@@ -2,7 +2,7 @@ const getTagMatches = require('./utils/get-tag-matches');
 const Tag = require('./tag');
 
 /**
- * @param {string} html The HTML/text to extraxt embedded items from.
+ * @param {string} html The HTML/text to extract embedded items from.
  * @returns {Tag[]} An array of Tag instances.
  */
 module.exports = (html) => {
