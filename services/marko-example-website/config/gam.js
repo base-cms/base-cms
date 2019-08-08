affe0babf11f74b5268605442f9bc50329218578
Jacob Bare <jacob@limit0.io>
2019-08-08 11:10:53 -0500

    Create GAM config and use throughout site

---
+++ services/marko-example-website/config/gam.js
@@ -0,0 +1,23 @@
+const GAMConfiguration = require('@base-cms/marko-web-gam/config');
+
+const config = new GAMConfiguration('21687441225', { basePath: 'Officer' });
+
+config
+  .setTemplate('BS', {
+    size: [[970, 250], [970, 90], [728, 90], [320, 50], [300, 50], [300, 100]],
+    sizeMapping: [
+      { viewport: [980, 0], size: [[970, 250], [970, 90], [728, 90]] },
+      { viewport: [750, 0], size: [728, 90] },
+      { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
+    ],
+  })
+  .setTemplate('MR', { size: [300, 250] })
+  .setTemplate('HP', { size: [300, 600] });
+
+config.setAliasAdUnits('default', [
+  { name: 'BS', templateName: 'BS', path: 'Officer_BS' },
+  { name: 'MR', templateName: 'MR', path: 'Officer_MR' },
+  { name: 'HP', templateName: 'HP', path: 'Officer_HP' },
+]);
+
+module.exports = config;
