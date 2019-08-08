98202fec8bbe6bf7a566564f5da09c3364ae805a
Jacob Bare <jacob@limit0.io>
2019-08-08 12:38:07 -0500

    Remove old gtm components from core

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -1,9 +1,7 @@
-import GTMTrackInViewEvent from './gtm-track-in-view-event.vue';
 import LoadMoreButton from './load-more-button.vue';
 import OEmbed from './oembed.vue';
 
 const components = {
-  GTMTrackInViewEvent,
   LoadMoreButton,
   OEmbed,
 };
