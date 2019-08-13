851c1ca0250164dab2429098dea97fe1d5b6dda6
Jacob Bare <jacob@limit0.io>
2019-08-13 13:41:58 -0500

    Use null/undefined check

--- packages/marko-web/src/utils/should-collapse.js
+++ packages/marko-web/src/utils/should-collapse.js
@@ -1,5 +1,5 @@
 module.exports = (collapse) => {
-  if (collapse === 'false' || collapse === false) return false;
-  if (!collapse) return true;
+  if (collapse === 'false') return false;
+  if (collapse == null) return true;
   return Boolean(collapse);
 };
