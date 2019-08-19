a68cdcd2bc4ddb599305fb252b728b899d9415d3
Jacob Bare <jacob@limit0.io>
2019-08-19 11:29:30 -0500

    Install and use fixed bottom gam ad

--- services/marko-example-website/browser/index.js
+++ services/marko-example-website/browser/index.js
@@ -1,7 +1,9 @@
 import Browser from '@base-cms/marko-web/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
+import GAM from '@base-cms/marko-web-gam/browser';
 
 GTM(Browser);
+GAM(Browser);
 
 // Register custom Vue components here...
 /*
