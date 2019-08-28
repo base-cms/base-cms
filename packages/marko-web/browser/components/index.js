e33fa3b801ad4e3b7f21b2a6d001682d4c7bae18
Jacob Bare <jacob@limit0.io>
2019-08-28 14:53:50 -0500

    Create generic in-view event trigger

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -1,9 +1,11 @@
 import LoadMoreButton from './load-more-button.vue';
 import OEmbed from './oembed.vue';
+import TriggerInViewEvent from './trigger-in-view-event.vue';
 
 const components = {
   LoadMoreButton,
   OEmbed,
+  TriggerInViewEvent,
 };
 
 export default components;
---
+++ packages/marko-web/browser/components/trigger-in-view-event.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :id="elementId" class="lazyload" :data-expand="expand" />
+</template>
+
+<script>
+import EventBus from '../event-bus';
+
+export default {
+  props: {
+    eventName: {
+      type: String,
+      default: 'in-view',
+    },
+    expand: {
+      type: Number,
+      default: -100,
+    },
+    data: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    elementId() {
+      return `${this.eventName}-event-${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+    },
+  },
+  created() {
+    document.addEventListener('lazybeforeunveil', this.trigger.bind(this));
+  },
+  methods: {
+    trigger(event) {
+      const { target } = event;
+      if (target.id === this.elementId) {
+        EventBus.$emit(this.eventName, { event, data: this.data });
+      }
+    },
+  },
+};
+</script>
