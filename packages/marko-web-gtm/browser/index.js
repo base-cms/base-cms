9d9e44b3663400f201cf8a1df1aed07cd396404d
Jacob Bare <jacob@limit0.io>
2019-08-28 14:54:45 -0500

    Create load-more tracker

--- packages/marko-web-gtm/browser/index.js
+++ packages/marko-web-gtm/browser/index.js
@@ -1,5 +1,8 @@
 import GTMTrackInViewEvent from './track-in-view-event.vue';
+import GTMTrackLoadMore from './track-load-more.vue';
 
 export default (Browser) => {
+  const { EventBus } = Browser;
   Browser.registerComponent('GTMTrackInViewEvent', GTMTrackInViewEvent);
+  Browser.registerComponent('GTMTrackLoadMore', GTMTrackLoadMore, { EventBus });
 };
---
+++ packages/marko-web-gtm/browser/track-load-more.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gtm-track-load-more" />
+</template>
+
+<script>
+export default {
+  inject: ['EventBus'],
+  props: {
+    name: {
+      type: String,
+      default: 'dataLayer',
+    },
+    event: {
+      type: String,
+      default: 'page_load',
+    },
+  },
+  created() {
+    this.EventBus.$on('load-more-in-view', ({ data }) => {
+      const dataLayer = window[this.name];
+      if (dataLayer) {
+        dataLayer.push({ ...data, event: this.event });
+      }
+    });
+  },
+};
+</script>
