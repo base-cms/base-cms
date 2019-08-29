0f01681c180cc98095c25ec66b2b8465c292fddb
Jacob Bare <jacob@limit0.io>
2019-08-29 15:26:02 -0500

    Create load more trigger vue component

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -1,9 +1,11 @@
 import LoadMoreButton from './load-more-button.vue';
+import LoadMoreTrigger from './load-more-trigger.vue';
 import OEmbed from './oembed.vue';
 import TriggerInViewEvent from './trigger-in-view-event.vue';
 
 const components = {
   LoadMoreButton,
+  LoadMoreTrigger,
   OEmbed,
   TriggerInViewEvent,
 };
