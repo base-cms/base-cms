4bc43b53155ca5bba80c40c08fc25f7fde804c89
Jacob Bare <jacob@limit0.io>
2019-08-29 13:17:19 -0500

    Remove undeeded site load more route

--- services/marko-example-website/server/routes/index.js
+++ services/marko-example-website/server/routes/index.js
@@ -1,7 +1,6 @@
 const contentTypes = require('./content');
 const dynamicPages = require('./dynamic-page');
 const home = require('./home');
-const loadMore = require('./load-more');
 const websiteSections = require('./website-section');
 const magazine = require('./magazine');
 const subscribe = require('./subscribe');
@@ -12,9 +11,6 @@ module.exports = (app) => {
 
   // Homepage
   home(app);
-
-  // Load More / Infinite Scroll
-  loadMore(app);
 
   // Dynamic Pages
   dynamicPages(app);
