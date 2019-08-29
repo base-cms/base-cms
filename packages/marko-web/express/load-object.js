32ba7adb2149dfba96a3ba74c3df8b3d9e6db60c
Jacob Bare <jacob@limit0.io>
2019-08-29 11:40:53 -0500

    Load object with a getter

--- packages/marko-web/express/load-object.js
+++ packages/marko-web/express/load-object.js
@@ -1,9 +1,10 @@
-const { getAsObject } = require('@base-cms/object-path');
+const { getAsObject, get } = require('@base-cms/object-path');
 const { dasherize } = require('@base-cms/inflector');
 
 const { keys } = Object;
 
 module.exports = (value) => {
   const v = getAsObject(value);
-  return keys(v).reduce((o, k) => ({ ...o, [dasherize(k)]: v[k] }), {});
+  const obj = keys(v).reduce((o, k) => ({ ...o, [dasherize(k)]: v[k] }), {});
+  return { get: name => get(obj, dasherize(name)) };
 };
