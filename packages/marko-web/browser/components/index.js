abf1df6118207c4707335abe3aa9fb84efe87ecc
Jacob Bare <jacob@limit0.io>
2019-08-30 08:35:58 -0500

    Delete load more button

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -1,10 +1,8 @@
-import LoadMoreButton from './load-more-button.vue';
 import LoadMoreTrigger from './load-more-trigger.vue';
 import OEmbed from './oembed.vue';
 import TriggerInViewEvent from './trigger-in-view-event.vue';
 
 const components = {
-  LoadMoreButton,
   LoadMoreTrigger,
   OEmbed,
   TriggerInViewEvent,
