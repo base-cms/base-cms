38a8ed923653d1b106fc95dae430b13001b12eea
Jacob Bare <jacob@limit0.io>
2019-09-04 10:00:13 -0500

    Register global document and use default section layout

--- services/example-bizbash/index.js
+++ services/example-bizbash/index.js
@@ -2,6 +2,7 @@ const { startServer } = require('@base-cms/marko-web');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
+const document = require('./server/document');
 
 const { log } = console;
 
@@ -10,4 +11,5 @@ module.exports = startServer({
   coreConfig,
   siteConfig,
   routes,
+  document,
 }).then(() => log('Website started!')).catch(e => setImmediate(() => { throw e; }));
