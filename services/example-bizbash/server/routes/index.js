a52e23f842000fdce67c60b68df94df674f8011d
Jacob Bare <jacob@limit0.io>
2019-09-04 13:13:10 -0500

    Create initial dynamic page route and template

--- services/example-bizbash/server/routes/index.js
+++ services/example-bizbash/server/routes/index.js
@@ -1,10 +1,14 @@
 const home = require('./home');
 const content = require('./content');
+const dynamicPages = require('./dynamic-page');
 const websiteSections = require('./website-section');
 
 module.exports = (app) => {
   // Homepage
   home(app);
+
+  // Dynamic Pages
+  dynamicPages(app);
 
   // Content Pages
   content(app);
