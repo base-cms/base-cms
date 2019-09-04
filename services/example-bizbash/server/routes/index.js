ac377aeee4bec13d1d2854af447ce2220fb4c148
Jacob Bare <jacob@limit0.io>
2019-09-04 10:46:57 -0500

    Create initial website section routing and template

--- services/example-bizbash/server/routes/index.js
+++ services/example-bizbash/server/routes/index.js
@@ -1,6 +1,10 @@
 const home = require('./home');
+const websiteSections = require('./website-section');
 
 module.exports = (app) => {
   // Homepage
   home(app);
+
+  // Website Sections
+  websiteSections(app);
 };
