e125687e08bf6860cb2c816eb074bdb9e2f51659
Jacob Bare <jacob@limit0.io>
2019-09-06 10:19:18 -0500

    Add default site header

--- services/example-laserfocusworld/config/site.js
+++ services/example-laserfocusworld/config/site.js
@@ -1,4 +1,18 @@
 module.exports = {
+  logos: {
+    navbar: {
+      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=60',
+      srcset: [
+        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=120 2x',
+      ],
+    },
+    footer: {
+      src: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=60',
+      srcset: [
+        'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=120 2x',
+      ],
+    },
+  },
   gtm: {
     containerId: 'GTM-M7H8VJG',
   },
