a0dbf5de5e6cbdf0cd0f5743fad85312dae3909b
Jacob Bare <jacob@limit0.io>
2019-09-06 12:15:16 -0500

    Import default theme browser components

--- services/example-laserfocusworld/browser/index.js
+++ services/example-laserfocusworld/browser/index.js
@@ -1,7 +1,9 @@
 import Browser from '@base-cms/marko-web/browser';
+import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 
+DefaultTheme(Browser);
 GTM(Browser);
 GAM(Browser);
 
