99a023bc148cc9ea76d23482889e57526bddb81c
Jacob Bare <jacob@limit0.io>
2019-09-09 11:41:28 -0500

    Create example GAM config

---
+++ services/example-laserfocusworld/config/gam.js
@@ -0,0 +1,26 @@
+const GAMConfiguration = require('@base-cms/marko-web-gam/config');
+
+const config = new GAMConfiguration('21687441225', { basePath: 'LFW' });
+
+config
+  .setTemplate('LB', {
+    size: [[970, 250], [970, 90], [970, 66], [728, 90], [320, 50], [300, 50], [300, 100]],
+    sizeMapping: [
+      { viewport: [980, 0], size: [[970, 250], [970, 90], [970, 66], [728, 90]] },
+      { viewport: [750, 0], size: [728, 90] },
+      { viewport: [320, 0], size: [[300, 50], [320, 50], [300, 100]] },
+    ],
+  })
+  .setTemplate('CONTENT', { size: [[300, 250], [300, 600]] });
+
+config.setAliasAdUnits('default', [
+  { name: 'lb1', templateName: 'LB', path: 'default/lb1' },
+  { name: 'lb2', templateName: 'LB', path: 'default/lb2' },
+  { name: 'rail1', templateName: 'CONTENT', path: 'default/rail1' },
+  { name: 'rail2', templateName: 'CONTENT', path: 'default/rail2' },
+  { name: 'load-more', templateName: 'CONTENT', path: 'default/load-more' },
+  { name: 'reskin', path: 'default/reskin' },
+  { name: 'wa', path: 'default/wa' },
+]);
+
+module.exports = config;
