d2e687376039f57d6520d78349e99e59450bc428
Jacob Bare <jacob@limit0.io>
2019-09-12 14:59:54 -0500

    Split card deck out of content load more

--- services/example-laserfocusworld/server/components.js
+++ services/example-laserfocusworld/server/components.js
@@ -1,5 +1,7 @@
+const ContentCardDeckBlock = require('./components/blocks/content-card-deck');
 const ContentLoadMoreBlock = require('./components/blocks/content-load-more');
 
 module.exports = {
+  ContentCardDeckBlock,
   ContentLoadMoreBlock,
 };
