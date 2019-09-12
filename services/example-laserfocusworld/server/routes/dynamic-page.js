b0f3f47cbbb127f48f73d8c9154007b226ee620d
Jacob Bare <jacob@limit0.io>
2019-09-12 13:26:13 -0500

    Add dynamic page query fragment

--- services/example-laserfocusworld/server/routes/dynamic-page.js
+++ services/example-laserfocusworld/server/routes/dynamic-page.js
@@ -1,8 +1,10 @@
 const { withDynamicPage } = require('@base-cms/marko-web/middleware');
 const page = require('../templates/dynamic-page');
+const queryFragment = require('../api/fragments/dynamic-page');
 
 module.exports = (app) => {
   app.get('/page/:alias', withDynamicPage({
     template: page,
+    queryFragment,
   }));
 };
