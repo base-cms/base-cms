f77d305f9f2472fcceb9a32b1378c1b51cc34f7e
Jacob Bare <jacob@limit0.io>
2019-09-12 12:28:11 -0500

    Temporarily remove old bizbash routes/templates

--- services/example-bizbash/server/routes/index.js
+++ services/example-bizbash/server/routes/index.js
@@ -1,18 +1,2 @@
-const home = require('./home');
-const content = require('./content');
-const dynamicPages = require('./dynamic-page');
-const websiteSections = require('./website-section');
-
-module.exports = (app) => {
-  // Homepage
-  home(app);
-
-  // Dynamic Pages
-  dynamicPages(app);
-
-  // Content Pages
-  content(app);
-
-  // Website Sections
-  websiteSections(app);
+module.exports = () => {
 };
