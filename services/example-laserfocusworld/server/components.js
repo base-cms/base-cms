3282f4c541b80b502716693842cbc2150034c3e8
Jacob Bare <jacob@limit0.io>
2019-09-12 15:28:41 -0500

    Register content list block

--- services/example-laserfocusworld/server/components.js
+++ services/example-laserfocusworld/server/components.js
@@ -1,7 +1,9 @@
 const ContentCardDeckBlock = require('./components/blocks/content-card-deck');
 const ContentLoadMoreBlock = require('./components/blocks/content-load-more');
+const ContentListBlock = require('./components/blocks/content-list');
 
 module.exports = {
   ContentCardDeckBlock,
   ContentLoadMoreBlock,
+  ContentListBlock,
 };
