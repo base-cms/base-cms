cfc4793e86b5a2ee3ba224f1a8b3304edb40b20c
Jacob Bare <jacob@limit0.io>
2019-09-13 12:52:05 -0500

    Create initial magazine page route/template

--- services/example-laserfocusworld/server/routes/magazine.js
+++ services/example-laserfocusworld/server/routes/magazine.js
@@ -1,7 +1,15 @@
+const { withMagazineIssue } = require('@base-cms/marko-web/middleware');
 const magazine = require('../templates/magazine');
+const magazineIssue = require('../templates/magazine/issue');
+const magazineIssueFragment = require('../api/fragments/magazine-issue-page');
 
 module.exports = (app) => {
   app.get('/magazine', (req, res) => {
     res.marko(magazine);
   });
+
+  app.get('/magazine/:id(\\d+)', withMagazineIssue({
+    template: magazineIssue,
+    queryFragment: magazineIssueFragment,
+  }));
 };
