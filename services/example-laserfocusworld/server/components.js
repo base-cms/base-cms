cd30b7aece86dd34f5b9fc32d66986b9d5032eeb
Jacob Bare <jacob@limit0.io>
2019-09-13 08:08:36 -0500

    Rename blocks to flows

--- services/example-laserfocusworld/server/components.js
+++ services/example-laserfocusworld/server/components.js
@@ -1,9 +1,9 @@
-const ContentCardDeckBlock = require('./components/blocks/content-card-deck');
-const ContentLoadMoreBlock = require('./components/blocks/content-load-more');
-const ContentListBlock = require('./components/blocks/content-list');
+const ContentCardDeckFlow = require('./components/flows/content-card-deck');
+const ContentLoadMoreFlow = require('./components/flows/content-load-more');
+const ContentListFlow = require('./components/flows/content-list');
 
 module.exports = {
-  ContentCardDeckBlock,
-  ContentLoadMoreBlock,
-  ContentListBlock,
+  ContentCardDeckFlow,
+  ContentLoadMoreFlow,
+  ContentListFlow,
 };
