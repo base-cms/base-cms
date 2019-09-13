76b738d59328e3fee1792c56fa762b21b4151b95
Jacob Bare <jacob@limit0.io>
2019-09-13 12:52:14 -0500

    Use standard website section fragment

--- services/example-laserfocusworld/server/routes/home.js
+++ services/example-laserfocusworld/server/routes/home.js
@@ -1,9 +1,11 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const home = require('../templates/index');
+const queryFragment = require('../api/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/', withWebsiteSection({
     aliasResolver: () => 'home',
     template: home,
+    queryFragment,
   }));
 };
