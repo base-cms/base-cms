00e582c443ebf9f86be6bc0b09dd0a2688044cdf
Jacob Bare <jacob@limit0.io>
2019-09-13 13:35:36 -0500

    Create magazine issue context

--- packages/marko-web-gtm/context/index.js
+++ packages/marko-web-gtm/context/index.js
@@ -2,10 +2,12 @@ const defaultBuilder = require('./default');
 const contentBuilder = require('./content');
 const websiteSectionBuilder = require('./website-section');
 const dynamicPageBuilder = require('./dynamic-page');
+const magazineIssueBuilder = require('./magazine-issue');
 
 module.exports = {
   defaultBuilder,
   contentBuilder,
   websiteSectionBuilder,
   dynamicPageBuilder,
+  magazineIssueBuilder,
 };
