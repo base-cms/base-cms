e6e3cfed53de6871756bcaa606968e9200232dfc
Jacob Bare <jacob@limit0.io>
2019-09-17 09:14:36 -0500

    Add reveal ad listener

--- services/example-laserfocusworld/browser/index.js
+++ services/example-laserfocusworld/browser/index.js
@@ -3,10 +3,12 @@ import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import GCSE from '@base-cms/marko-web-gcse/browser';
+import RevealAd from '@base-cms/marko-web-reveal-ad/browser';
 
 DefaultTheme(Browser);
 GTM(Browser);
 GAM(Browser);
 GCSE(Browser);
+RevealAd(Browser);
 
 export default Browser;
