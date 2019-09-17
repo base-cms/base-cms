bf391898f97bca3e7d9f4de4d0ba5e1a700e5059
Jacob Bare <jacob@limit0.io>
2019-09-17 15:01:09 -0500

    Add wufoo gated download form

--- packages/marko-web/browser/components/index.js
+++ packages/marko-web/browser/components/index.js
@@ -2,12 +2,14 @@ import LoadMoreTrigger from './load-more-trigger.vue';
 import OEmbed from './oembed.vue';
 import TriggerInViewEvent from './trigger-in-view-event.vue';
 import FormDotComGatedDownload from './gated-download/form-dot-com.vue';
+import WufooGatedDownload from './gated-download/wufoo.vue';
 
 const components = {
   LoadMoreTrigger,
   OEmbed,
   TriggerInViewEvent,
   FormDotComGatedDownload,
+  WufooGatedDownload,
 };
 
 export default components;
