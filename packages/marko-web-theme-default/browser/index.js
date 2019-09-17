3b5a0568d0b8f79e47ab34baa0042b63c89e6be7
Jacob Bare <jacob@limit0.io>
2019-09-17 11:58:04 -0500

    Remove contact us form

--- packages/marko-web-theme-default/browser/index.js
+++ packages/marko-web-theme-default/browser/index.js
@@ -1,7 +1,5 @@
 import MenuToggleButton from './menu-toggle-button.vue';
-import ContactUsForm from './contact-us-form.vue';
 
 export default (Browser) => {
   Browser.registerComponent('DefaultThemeMenuToggleButton', MenuToggleButton);
-  Browser.registerComponent('DefaultThemeContactUsForm', ContactUsForm);
 };
