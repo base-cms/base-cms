7854f0b39da2155ff48b5361816d3754a35a4cce
Jacob Bare <jacob@limit0.io>
2019-09-17 08:46:11 -0500

    Add gcse to lfw example

--- services/example-laserfocusworld/browser/index.js
+++ services/example-laserfocusworld/browser/index.js
@@ -2,9 +2,11 @@ import Browser from '@base-cms/marko-web/browser';
 import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
+import GCSE from '@base-cms/marko-web-gcse/browser';
 
 DefaultTheme(Browser);
 GTM(Browser);
 GAM(Browser);
+GCSE(Browser);
 
 export default Browser;
