d242d231b08e35d9c2c7acf3d5aac3f9b7d8c7f5
Jacob Bare <jacob@limit0.io>
2019-09-17 13:01:45 -0500

    Create initial whitepaper support

--- services/example-laserfocusworld/server/routes/content.js
+++ services/example-laserfocusworld/server/routes/content.js
@@ -1,8 +1,13 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
+const whitepaper = require('../templates/content/whitepaper');
 const queryFragment = require('../api/fragments/content-page');
 
 module.exports = (app) => {
+  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
+    template: whitepaper,
+    queryFragment,
+  }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
