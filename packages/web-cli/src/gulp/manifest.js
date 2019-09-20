4d76386f867cc4b1ae757e2a8473a46462a8023e
Jacob Bare <jacob@limit0.io>
2019-09-20 12:46:59 -0500

    Only replace sourcemap when creating revision

--- packages/web-cli/src/gulp/manifest.js
+++ packages/web-cli/src/gulp/manifest.js
@@ -2,7 +2,7 @@ const pump = require('pump');
 const revall = require('gulp-rev-all');
 const del = require('gulp-rev-delete-original');
 const { dest, src } = require('gulp');
-const path = require('path');
+const { basename, extname } = require('path');
 const completeTask = require('../utils/task-callback');
 
 module.exports = cwd => (cb) => {
@@ -11,13 +11,21 @@ module.exports = cwd => (cb) => {
     revall.revision({
       includeFilesInManifest: ['.css', '.js'],
       transformFilename: (file, hash) => {
-        const basename = path.basename(file.path);
-        const mapname = path.basename(file.path, '.map');
-        const prefix = basename === mapname
-          ? path.basename(mapname, path.extname(file.path))
-          : path.basename(mapname, path.extname(mapname));
-        const suffix = basename.replace(prefix, '');
+        const base = basename(file.path);
+        const mapname = basename(file.path, '.map');
+        const prefix = base === mapname
+          ? basename(mapname, extname(file.path))
+          : basename(mapname, extname(mapname));
+        const suffix = base.replace(prefix, '');
         return `${prefix}.${hash.substr(0, 8)}${suffix}`;
+      },
+      annotator: (contents, path) => ([{ contents, path }]), // provide file path.
+      replacer: (fragment, replaceRegExp, newReference) => {
+        // Prevent replacing generic "index" values.
+        if (`${replaceRegExp}` !== '/(\'|")(index)()(\'|"|$)/g') {
+          // eslint-disable-next-line no-param-reassign
+          fragment.contents = fragment.contents.replace(replaceRegExp, `$1${newReference}$3$4`);
+        }
       },
     }),
     del(),
