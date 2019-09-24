13aab28d224d32a778b9f234da0877a08de0611a
Jacob Bare <jacob@limit0.io>
2019-09-24 12:24:36 -0500

    Remove BASEDB_LOGGING

--- services/rss/src/env.js
+++ services/rss/src/env.js
@@ -10,7 +10,6 @@ const { nonemptystr } = custom;
 
 module.exports = cleanEnv(process.env, {
   MONGO_DSN: nonemptystr({ desc: 'The Base MongoDB connection URL.' }),
-  ENABLE_BASEDB_LOGGING: bool({ desc: 'Whether the BaseDB instance should log to the console.', default: false }),
   PAGE_SIZE: num({ desc: 'The number of urls per page', default: 25 }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
