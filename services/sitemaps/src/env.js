2aa527459c7823b0a2c86390bdc4ff1bee4f48bd
Jacob Bare <jacob@limit0.io>
2019-09-25 09:30:22 -0500

    Add port and exposed port env vals

--- services/sitemaps/src/env.js
+++ services/sitemaps/src/env.js
@@ -5,11 +5,14 @@ const {
   cleanEnv,
   bool,
   num,
+  port,
 } = envalid;
 const { nonemptystr } = custom;
 
 module.exports = cleanEnv(process.env, {
   GRAPHQL_URI: nonemptystr({ desc: 'The BaseCMS GraphQL API URL.' }),
+  PORT: port({ desc: 'The internal port to run on.', default: 80 }),
+  EXPOSED_PORT: port({ desc: 'The external port to run on.', default: 80 }),
   PAGE_SIZE: num({ desc: 'The number of urls per page', default: 5000 }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
