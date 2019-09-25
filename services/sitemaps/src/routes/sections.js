d7be3a170dc498605431dbad59f6f2f3b58d3c16
Jacob Bare <jacob@limit0.io>
2019-09-25 14:15:45 -0500

    Remove unused namespaces

--- services/sitemaps/src/routes/sections.js
+++ services/sitemaps/src/routes/sections.js
@@ -36,7 +36,7 @@ module.exports = asyncRoute(async (req, res) => {
 
   const urlset = urls.map(url => createUrl(url)).filter(v => v);
   const lines = ['<?xml version="1.0" encoding="utf-8"?>'];
-  lines.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">');
+  lines.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
   lines.push(...urlset);
   lines.push('</urlset>');
 
