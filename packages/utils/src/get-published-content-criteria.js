7a11ca32db75a67874f8511085d662ff7b28a5e8
Jacob Bare <jacob@limit0.io>
2019-09-25 10:35:13 -0500

    Add support for excluding content types

--- packages/utils/src/get-published-content-criteria.js
+++ packages/utils/src/get-published-content-criteria.js
@@ -5,15 +5,21 @@ const { isArray } = Array;
 module.exports = ({
   excludeContentIds = [],
   contentTypes = [],
+  excludeContentTypes = [],
   since,
 } = {}) => {
   const date = since || new Date();
   const types = isArray(contentTypes) && contentTypes.length
     ? contentTypes : getDefaultContentTypes();
 
+  const type = { $in: types };
+  if (isArray(excludeContentTypes) && excludeContentTypes.length) {
+    type.$nin = excludeContentTypes;
+  }
+
   const query = {
     status: 1,
-    type: { $in: types },
+    type,
     published: { $lte: date },
     $and: [
       {
