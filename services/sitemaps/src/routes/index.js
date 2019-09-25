06e450b26b4c8b17e2a6d097c27acb5bf3034e1c
Jacob Bare <jacob@limit0.io>
2019-09-25 09:40:46 -0500

    Remove old middleware

--- services/sitemaps/src/routes/index.js
+++ services/sitemaps/src/routes/index.js
@@ -2,11 +2,10 @@ const sitemap = require('./sitemap');
 const sitemapSections = require('./sitemap-sections');
 const sitemapContent = require('./sitemap-content');
 const sitemapGoogleNews = require('./sitemap-google-news');
-const sitemapMiddleware = require('./middleware');
 
 module.exports = (app) => {
-  app.get('/sitemap.xml', sitemapMiddleware, sitemap);
-  app.get('/sitemap-google-news.xml', sitemapMiddleware, sitemapGoogleNews);
-  app.get('/sitemap/sections.xml', sitemapMiddleware, sitemapSections);
-  app.get('/sitemap/*.xml', sitemapMiddleware, sitemapContent);
+  app.get('/sitemap.xml', sitemap);
+  app.get('/sitemap-google-news.xml', sitemapGoogleNews);
+  app.get('/sitemap/sections.xml', sitemapSections);
+  app.get('/sitemap/*.xml', sitemapContent);
 };
