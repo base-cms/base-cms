204d16120c8a44024524d0cca3818813aa231a3c
Jacob Bare <jacob@limit0.io>
2019-09-26 09:14:20 -0500

    Create “bootable” RSS app with site context

--- services/rss/src/app.js
+++ services/rss/src/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
-const routes = require('./routes');
+const { apollo, websiteContext } = require('./middleware');
+// const routes = require('./routes');
 
 const app = express();
 
 app.use(helmet());
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
 
-routes(app);
+app.use(apollo());
+app.use(websiteContext());
+app.use((req, res, next) => {
+  res.setHeader('X-Robots-Tag', 'noindex');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json(res.locals.websiteContext);
+});
+
+// routes(app);
 
 module.exports = app;
---
+++ services/rss/src/middleware/website-context.js
@@ -0,0 +1,22 @@
+const { asyncRoute } = require('@base-cms/utils');
+const gql = require('graphql-tag');
+
+const query = gql`
+
+query WebsiteContext {
+  websiteContext {
+    id
+    name
+    url
+    origin
+  }
+}
+
+`;
+
+module.exports = () => asyncRoute(async (req, res, next) => {
+  const { apollo } = res.locals;
+  const { data } = await apollo.query({ query });
+  res.locals.websiteContext = data.websiteContext;
+  next();
+});
