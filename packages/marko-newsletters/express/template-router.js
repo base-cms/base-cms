1d8fa10ab1d13d25b252c2c05a49a8175fc0d827
Jacob Bare <jacob@limit0.io>
2019-10-01 14:47:33 -0500

    Query for newsletter directly

--- packages/marko-newsletters/express/template-router.js
+++ packages/marko-newsletters/express/template-router.js
@@ -1,26 +1,37 @@
 const { Router } = require('express');
 const { asyncRoute } = require('@base-cms/utils');
-const mapTemplates = require('../utils/map-templates');
+const gql = require('graphql-tag');
+const createError = require('http-errors');
 
-/**
- * @todo Might want adjust how this is queried.
- */
+const query = gql`
+
+query WithMarkoNewsletter($input: EmailNewsletterAliasQueryInput!) {
+  emailNewsletterAlias(input: $input) {
+    id
+    name
+    alias
+    description
+    status
+  }
+}
+
+`;
+
+
 module.exports = ({ templates }) => {
   const router = Router();
 
-  router.use(asyncRoute(async (req, res, next) => {
-    const { apollo } = res.locals;
-    const { newsletters } = await mapTemplates(apollo, { templates });
-    res.locals.newsletters = newsletters;
-    next();
-  }));
-
   templates.forEach(({ route, template, alias }) => {
-    router.get(route, (req, res) => {
-      const { newsletters } = res.locals;
-      const newsletter = newsletters.find(n => n.alias === alias);
+    router.get(route, asyncRoute(async (req, res) => {
+      const { apollo } = res.locals;
+      const input = { alias, status: 'any' };
+
+      const { data } = await apollo.query({ query, variables: { input } });
+      const { emailNewsletterAlias: newsletter } = data;
+
+      if (newsletter && newsletter.status !== 1) throw createError(404, 'No newsletter found for the provided alias.');
       res.marko(template, { newsletter: newsletter || {}, isStatic: !newsletter });
-    });
+    }));
   });
   return router;
 };
