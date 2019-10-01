a2204b5d11a96729214735a0719a8c083728ee37
Jacob Bare <jacob@limit0.io>
2019-10-01 13:20:55 -0500

    Add bootstrap and create basic admin shell

--- packages/marko-newsletters/admin/routes/home.js
+++ packages/marko-newsletters/admin/routes/home.js
@@ -1,7 +1,8 @@
 const { asyncRoute } = require('@base-cms/utils');
+const template = require('../templates/index');
 
 module.exports = (router) => {
   router.get('/', asyncRoute(async (req, res) => {
-    res.send('Admin Home');
+    res.marko(template);
   }));
 };
