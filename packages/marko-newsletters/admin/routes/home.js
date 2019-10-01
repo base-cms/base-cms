bbee8a036626ba1758226a0dc439e11e4cbda7d9
Jacob Bare <jacob@limit0.io>
2019-10-01 13:37:57 -0500

    Load newsletters and static templates

--- packages/marko-newsletters/admin/routes/home.js
+++ packages/marko-newsletters/admin/routes/home.js
@@ -1,8 +1,38 @@
 const { asyncRoute } = require('@base-cms/utils');
+const { getAsArray } = require('@base-cms/object-path');
+const gql = require('graphql-tag');
 const template = require('../templates/index');
 
-module.exports = (router) => {
+const query = gql`
+
+query MarkoNewsletterList {
+  emailNewsletters(input: { sort: { field: name, order: asc }, pagination: { limit: 200 } }) {
+    edges {
+      node {
+        id
+        name
+        description
+        alias
+      }
+    }
+  }
+}
+
+`;
+
+module.exports = (router, { templates }) => {
   router.get('/', asyncRoute(async (req, res) => {
-    res.marko(template);
+    const { apollo } = res.locals;
+
+    const { data } = await apollo.query({ query });
+    const newsletters = getAsArray(data, 'emailNewsletters.edges').map((edge) => {
+      const node = { ...edge.node };
+      node.templates = templates.filter(t => t.alias === node.alias).map(t => t.key);
+      return node;
+    });
+
+    const aliases = newsletters.map(n => n.alias);
+    const staticTemplates = templates.filter(t => !aliases.includes(t.alias)).map(t => t.key);
+    res.marko(template, { newsletters, staticTemplates });
   }));
 };
