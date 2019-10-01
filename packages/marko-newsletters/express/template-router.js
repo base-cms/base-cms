3a3d25431ddd5d78ea277db2c287c2e4ef718332
Jacob Bare <jacob@limit0.io>
2019-10-01 15:02:24 -0500

    Add date handling

--- packages/marko-newsletters/express/template-router.js
+++ packages/marko-newsletters/express/template-router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { asyncRoute } = require('@base-cms/utils');
 const gql = require('graphql-tag');
 const createError = require('http-errors');
+const moment = require('moment');
 
 const query = gql`
 
@@ -17,12 +18,20 @@ query WithMarkoNewsletter($input: EmailNewsletterAliasQueryInput!) {
 
 `;
 
-
 module.exports = ({ templates }) => {
   const router = Router();
 
   templates.forEach(({ route, template, alias }) => {
     router.get(route, asyncRoute(async (req, res) => {
+      const ts = req.query.date ? parseInt(req.query.date, 10) : null;
+      let date = moment();
+      if (ts) {
+        date = moment(ts);
+      } else if (req.query.date) {
+        date = moment(req.query.date);
+      }
+      if (!date.isValid()) throw createError(400, 'The provided date parameter is invalid.');
+
       const { apollo } = res.locals;
       const input = { alias, status: 'any' };
 
@@ -30,7 +39,11 @@ module.exports = ({ templates }) => {
       const { emailNewsletterAlias: newsletter } = data;
 
       if (newsletter && newsletter.status !== 1) throw createError(404, 'No newsletter found for the provided alias.');
-      res.marko(template, { newsletter: newsletter || {}, isStatic: !newsletter });
+      res.marko(template, {
+        day: date.startOf('day'),
+        newsletter: newsletter || {},
+        isStatic: !newsletter,
+      });
     }));
   });
   return router;
