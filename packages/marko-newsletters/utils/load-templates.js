c74a70d6141f8e5444939208f40fb4fc67d86a87
Jacob Bare <jacob@limit0.io>
2019-10-01 12:52:47 -0500

    Create template loader/resolver

---
+++ packages/marko-newsletters/utils/load-templates.js
@@ -0,0 +1,31 @@
+const readdir = require('recursive-readdir');
+const fs = require('fs');
+const path = require('path');
+
+let promise;
+
+const load = async ({ rootDir, templatePath }) => {
+  const templateDir = path.resolve(rootDir, templatePath);
+  if (!fs.existsSync(templateDir)) throw new Error(`The template directory ${templateDir} does not exist.`);
+
+  const templates = [];
+  const files = await readdir(templateDir, ['!*.marko']);
+  files.forEach((file) => {
+    const route = file.replace(templateDir, '').replace(/\.marko$/i, '');
+    const alias = route.split('/')[1];
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const template = require(file);
+    templates.push({
+      route,
+      alias,
+      file,
+      template,
+    });
+  });
+  return templates;
+};
+
+module.exports = ({ rootDir, templatePath }) => {
+  if (!promise) promise = load({ rootDir, templatePath });
+  return promise;
+};
