a31f217188c49845d90b704ed76d38a7a8adce5b
Jacob Bare <jacob@limit0.io>
2019-10-02 15:19:02 -0500

    Remove canonicalPath and do not truncate teaser

--- services/newsletter-example-ien/graphql/fragments/content-list.js
+++ services/newsletter-example-ien/graphql/fragments/content-list.js
@@ -6,8 +6,7 @@ fragment NewsletterContentListFragment on Content {
   id
   type
   name(input: { mutation: Email })
-  teaser(input: { mutation: Email, maxLength: 500, useFallback: false, truncatedSuffix: "..." })
-  canonicalPath
+  teaser(input: { mutation: Email, useFallback: false, maxLength: null })
   primaryImage {
     id
     src
