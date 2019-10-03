5f6809e02d2d5c1a79a50595bb4fe0bcd03481b4
Jacob Bare <jacob@limit0.io>
2019-10-03 07:28:12 -0500

    Add value formatter support

--- packages/marko-core/utils/default-value.js
+++ packages/marko-core/utils/default-value.js
@@ -1,1 +1,6 @@
-module.exports = (value, def) => (value != null ? value : def);
+const { isFunction } = require('@base-cms/utils');
+
+module.exports = (value, def, formatter) => {
+  const fn = isFunction(formatter) ? formatter : v => v;
+  return value != null ? fn(value) : def;
+};
