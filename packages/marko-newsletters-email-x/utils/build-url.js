cb4f34fe41efc4a0630e0a68ef989f84cbcf7edd
Jacob Bare <jacob@limit0.io>
2019-10-04 09:06:55 -0500

    Allow unencoded url params; format date

--- packages/marko-newsletters-email-x/utils/build-url.js
+++ packages/marko-newsletters-email-x/utils/build-url.js
@@ -1,11 +1,20 @@
-const { URLSearchParams } = require('url');
+const { asArray, asObject } = require('@base-cms/utils');
 
 module.exports = ({
   uri,
   action,
   id,
   query,
+  decodedParams,
 } = {}) => {
-  const search = new URLSearchParams(query);
+  const q = asObject(query);
+  const noEncode = asArray(decodedParams);
+  const search = Object.keys(q).reduce((pairs, key) => {
+    const v = q[key];
+    if (!v) return pairs;
+    const value = noEncode.includes(key) ? v : encodeURIComponent(v);
+    pairs.push(`${encodeURIComponent(key)}=${value}`);
+    return pairs;
+  }, []).join('&');
   return `${uri}/${action}/${id}?${search}`;
 };
