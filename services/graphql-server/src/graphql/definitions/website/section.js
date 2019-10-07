7faef6b9d36bac1885dbc09a0ef7691bf655d418
Jacob Bare <jacob@limit0.io>
2019-10-07 16:05:19 -0500

    Break query directive args onto own lines

--- services/graphql-server/src/graphql/definitions/website/section.js
+++ services/graphql-server/src/graphql/definitions/website/section.js
@@ -3,12 +3,35 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 extend type Query {
-  websiteSection(input: WebsiteSectionQueryInput!): WebsiteSection @findOne(model: "website.Section", withSite: true, using: { id: "_id" })
-  websiteSectionAlias(input: WebsiteSectionAliasQueryInput!): WebsiteSection @findOne(model: "website.Section", withSite: true, using: { alias: "alias" })
-  websiteSectionRedirect(input: WebsiteSectionRedirectQueryInput!): WebsiteSection @findOne(model: "website.Section", withSite: true, using: { alias: "redirects" })
-  websiteSections(input: WebsiteSectionsQueryInput = {}): WebsiteSectionConnection! @findMany(model: "website.Section", withSite: true)
-  rootWebsiteSections(input: RootWebsiteSectionsQueryInput = {}): WebsiteSectionConnection! @findMany(model: "website.Section", withSite: true, criteria: "rootWebsiteSection")
-  websiteSectionsFromIds(input: WebsiteSectionsFromIdsQueryInput!): WebsiteSectionConnection! @findMany(model: "website.Section", withSite: true, using: { ids: "_id" })
+  websiteSection(input: WebsiteSectionQueryInput!): WebsiteSection @findOne(
+    model: "website.Section",
+    withSite: true,
+    using: { id: "_id" }
+  )
+  websiteSectionAlias(input: WebsiteSectionAliasQueryInput!): WebsiteSection @findOne(
+    model: "website.Section",
+    withSite: true,
+    using: { alias: "alias" }
+  )
+  websiteSectionRedirect(input: WebsiteSectionRedirectQueryInput!): WebsiteSection @findOne(
+    model: "website.Section",
+    withSite: true,
+    using: { alias: "redirects" }
+  )
+  websiteSections(input: WebsiteSectionsQueryInput = {}): WebsiteSectionConnection! @findMany(
+    model: "website.Section",
+    withSite: true
+  )
+  rootWebsiteSections(input: RootWebsiteSectionsQueryInput = {}): WebsiteSectionConnection! @findMany(
+    model: "website.Section",
+    withSite: true,
+    criteria: "rootWebsiteSection"
+  )
+  websiteSectionsFromIds(input: WebsiteSectionsFromIdsQueryInput!): WebsiteSectionConnection! @findMany(
+    model: "website.Section",
+    withSite: true,
+    using: { ids: "_id" }
+  )
   websiteSectionSitemapUrls(input: WebsiteSectionSitemapUrlsInput = {}): [WebsiteSectionSitemapUrl!]!
 }
 
