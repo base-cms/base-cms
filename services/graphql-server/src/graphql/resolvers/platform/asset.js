e8ad053e74e8aeac165811b079047a12216c3130
Jacob Bare <jacob@limit0.io>
2019-10-08 07:28:16 -0500

    Use site then global default for image src

--- services/graphql-server/src/graphql/resolvers/platform/asset.js
+++ services/graphql-server/src/graphql/resolvers/platform/asset.js
@@ -1,11 +1,16 @@
 const { createAltFor, createSrcFor, createCaptionFor } = require('@base-cms/image');
+const defaults = require('../../defaults');
 
 module.exports = {
   /**
    *
    */
   AssetImage: {
-    src: (image, { input = {} }, { site }) => createSrcFor(site.imageHost, image, input.options, { w: 320, auto: 'format' }),
+    src: (image, { input = {} }, { site }) => {
+      // Use site image host otherwise fallback to global default.
+      const imageHost = site.get('imageHost', defaults.imageHost);
+      return createSrcFor(imageHost, image, input.options, { w: 320, auto: 'format' });
+    },
     alt: image => createAltFor(image),
     caption: image => createCaptionFor(image.caption),
   },
