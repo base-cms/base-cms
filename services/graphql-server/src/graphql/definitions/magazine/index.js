921dd0c44fc8802387ce708f623b2771dbb4fc5a
Jacob Bare <jacob@limit0.io>
2019-10-14 18:05:35 -0500

    Add magazine schedule query and create mutation

--- services/graphql-server/src/graphql/definitions/magazine/index.js
+++ services/graphql-server/src/graphql/definitions/magazine/index.js
@@ -1,12 +1,14 @@
 const gql = require('graphql-tag');
 const issue = require('./issue');
 const publication = require('./publication');
+const schedule = require('./schedule');
 const section = require('./section');
 
 module.exports = gql`
 
 ${issue}
 ${publication}
+${schedule}
 ${section}
 
 `;
--- services/graphql-server/src/graphql/resolvers/magazine/index.js
+++ services/graphql-server/src/graphql/resolvers/magazine/index.js
@@ -2,10 +2,12 @@ const deepAssign = require('deep-assign');
 
 const issue = require('./issue');
 const publication = require('./publication');
+const schedule = require('./schedule');
 const section = require('./section');
 
 module.exports = deepAssign(
   issue,
   publication,
+  schedule,
   section,
 );
