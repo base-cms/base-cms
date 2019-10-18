8b3700f8eb93068cfaf1ce3b08bd6ea54fcb0c27
Jacob Bare <jacob@limit0.io>
2019-10-18 07:20:00 -0500

    Apply taxonomyCategory criteria to primary category

--- services/graphql-server/src/graphql/definitions/platform/content/interfaces/primary-category.js
+++ services/graphql-server/src/graphql/definitions/platform/content/interfaces/primary-category.js
@@ -3,7 +3,13 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 interface PrimaryCategory {
-  primaryCategory(input: PrimaryCategoryInput = {}): Taxonomy @projection(localField: "mutations.Website.primaryCategory") @refOne(loader: "platformTaxonomy", localField: "mutations.Website.primaryCategory")
+  primaryCategory(input: PrimaryCategoryInput = {}): Taxonomy
+    @projection(localField: "mutations.Website.primaryCategory")
+    @refOne(
+      loader: "platformTaxonomy",
+      localField: "mutations.Website.primaryCategory",
+      criteria: "taxonomyCategory",
+    )
 }
 
 input PrimaryCategoryInput {
