2759cc02477a7abba93eb4e374c6e57d97d34d94
Jacob Bare <jacob@limit0.io>
2019-10-24 13:26:38 -0500

    Use vue/recommended rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
