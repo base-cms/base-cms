147da4089151de270449defe17b7047778361845
Jacob Bare <jacob@limit0.io>
2019-10-24 13:37:28 -0500

    Create simple list-group component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="leaders">
     <h1>Hello World!</h1>
+    <list-group :items="items">
+      <template slot-scope="leader">
+        {{ leader.label }}
+      </template>
+    </list-group>
   </div>
 </template>
 
+<script>
+import ListGroup from './components/list-group.vue';
+
+export default {
+  components: { ListGroup },
+
+  data: () => ({
+    items: [
+      { label: 'Foo' },
+      { label: 'Bar' },
+      { label: 'Baz' },
+    ],
+  }),
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Muli:300,400,600&display=swap");
