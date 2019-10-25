1c15e9bc1d84b03dcc0382a1e54d0bb1bdebe825
Josh Worden <solocommand@gmail.com>
2019-10-25 09:46:31 -0500

    Update actions to support deep access

--- services/google-data-api/src/index.js
+++ services/google-data-api/src/index.js
@@ -25,59 +25,61 @@ module.exports = service.json({
   },
   ping,
   actions: {
-    /**
-     * The Youtube Channel List API
-     * @see https://developers.google.com/youtube/v3/docs/channels/list
-     */
-    'youtube.channelList': async (params) => {
-      const uri = 'https://www.googleapis.com/youtube/v3/channels';
-      const {
-        maxResults = 1,
-        part = 'snippet',
-        forUsername,
-        id,
-        ttl = 365 * 24 * 60 * 60,
-        force = false,
-      } = params;
-      if (!forUsername && !id) throw new Error('A channel id or username is required.');
+    youtube: {
+      /**
+       * The Youtube Channel List API
+       * @see https://developers.google.com/youtube/v3/docs/channels/list
+       */
+      channelList: async (params) => {
+        const uri = 'https://www.googleapis.com/youtube/v3/channels';
+        const {
+          maxResults = 1,
+          part = 'snippet',
+          forUsername,
+          id,
+          ttl = 365 * 24 * 60 * 60,
+          force = false,
+        } = params;
+        if (!forUsername && !id) throw new Error('A channel id or username is required.');
 
-      const payload = {
-        maxResults,
-        part,
-        ...(id && { id }),
-        ...(forUsername && { forUsername }),
-      };
-      const url = `${uri}?${stringify(sortObject(payload))}`;
-      const record = await retrieve(url);
-      if (record && !force) return record.response;
+        const payload = {
+          maxResults,
+          part,
+          ...(id && { id }),
+          ...(forUsername && { forUsername }),
+        };
+        const url = `${uri}?${stringify(sortObject(payload))}`;
+        const record = await retrieve(url);
+        if (record && !force) return record.response;
 
-      const response = await fetch(uri, payload);
-      write(url, response, ttl);
-      return response;
-    },
-    /**
-     * The Youtube Playlist Items API
-     * @see https://developers.google.com/youtube/v3/docs/playlistItems/list
-     */
-    'youtube.playlistItems': async (params) => {
-      const uri = 'https://www.googleapis.com/youtube/v3/playlistItems';
-      const {
-        maxResults = 10,
-        part = 'snippet',
-        playlistId,
-        ttl = 24 * 60 * 60,
-        force = false,
-      } = params;
-      if (!playlistId) throw new Error('A playlist id is required.');
+        const response = await fetch(uri, payload);
+        write(url, response, ttl);
+        return response;
+      },
+      /**
+       * The Youtube Playlist Items API
+       * @see https://developers.google.com/youtube/v3/docs/playlistItems/list
+       */
+      playlistItems: async (params) => {
+        const uri = 'https://www.googleapis.com/youtube/v3/playlistItems';
+        const {
+          maxResults = 10,
+          part = 'snippet',
+          playlistId,
+          ttl = 24 * 60 * 60,
+          force = false,
+        } = params;
+        if (!playlistId) throw new Error('A playlist id is required.');
 
-      const payload = { maxResults, part, playlistId };
-      const url = `${uri}?${stringify(sortObject(payload))}`;
-      const record = await retrieve(url);
-      if (record && !force) return record.response;
+        const payload = { maxResults, part, playlistId };
+        const url = `${uri}?${stringify(sortObject(payload))}`;
+        const record = await retrieve(url);
+        if (record && !force) return record.response;
 
-      const response = await fetch(uri, payload);
-      write(url, response, ttl);
-      return response;
+        const response = await fetch(uri, payload);
+        write(url, response, ttl);
+        return response;
+      },
     },
   },
 });
