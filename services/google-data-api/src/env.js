41980c72c8a63a24fe3a89fc9ae897e5a883d73c
Josh Worden <solocommand@gmail.com>
2019-10-25 11:42:39 -0500

    Use different env for google data connection

--- services/google-data-api/src/env.js
+++ services/google-data-api/src/env.js
@@ -4,6 +4,7 @@ const { bool, cleanEnv, validators } = envalid;
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
+  GOOGLEDATA_MONGO_DSN: nonemptystr({ desc: 'The Base MongoDB connection URL.' }),
   GOOGLE_API_KEY: nonemptystr({ desc: 'The Google Data API key.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
