a6cb195af851da7bf8efae5bd01fa05274c43d93
Josh Worden <solocommand@gmail.com>
2019-10-25 20:13:56 -0500

    Support pageToken for pagination

--- services/google-data-api/src/actions/youtube/playlist-items.js
+++ services/google-data-api/src/actions/youtube/playlist-items.js
@@ -11,13 +11,19 @@ module.exports = async ({
   maxResults = 10,
   part = 'snippet',
   playlistId,
+  pageToken,
   ttl = 24 * 60 * 60,
   force = false,
 } = {}) => {
   const uri = buildYoutubeUrl('playlistItems');
   if (!playlistId) throw new Error('A playlist id is required.');
 
-  const payload = { maxResults, part, playlistId };
+  const payload = {
+    maxResults,
+    part,
+    playlistId,
+    ...(pageToken && { pageToken }),
+  };
   const url = `${uri}?${stringify(sortObject(payload))}`;
   const record = await retrieve(url);
   if (record && !force) return record.response;
