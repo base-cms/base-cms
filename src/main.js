1b65b984b37570fb4a30a02aa8600616e5559047
Jacob Bare <jacob@limit0.io>
2019-10-29 17:57:21 -0500

    Create and apply apollo client to vue instance

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
+import createProvider from './apollo/create-provider';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueApollo);
+
 new Vue({
+  apolloProvider: createProvider({
+    graphqlUri: 'https://caprica.graphql-staging.base-cms.io',
+    tenantKey: 'pmmi_all',
+  }),
   render: h => h(App),
 }).$mount('#app');
