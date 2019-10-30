db664bc0256a1472c2409d625193028ff4739ba3
Jacob Bare <jacob@limit0.io>
2019-10-29 21:29:05 -0500

    Fix arrow positioning for portal dropdowns

--- src/components/leaders/positions/arrow-position.js
+++ src/components/leaders/positions/arrow-position.js
@@ -3,16 +3,18 @@ import AbstractPosition from './abstract-position';
 class ArrowPosition extends AbstractPosition {
   get x() {
     const { calculus: calcs } = this;
-    if (this.opensAbove || this.opensBelow) return calcs.link('left') + calcs.link('halfW');
-    if (this.opensLeft) return calcs.link('left');
-    return calcs.link('right');
+    const { pageXOffset } = window;
+    if (this.opensAbove || this.opensBelow) return pageXOffset + calcs.link('left') + calcs.link('halfW');
+    if (this.opensLeft) return pageXOffset + calcs.link('left');
+    return pageXOffset + calcs.link('right');
   }
 
   get y() {
     const { calculus: calcs } = this;
-    if (this.opensLeft || this.opensRight) return calcs.link('topNavTop') + calcs.link('halfH');
-    if (this.opensAbove) return calcs.link('topNavTop');
-    return calcs.link('bottomNavTop');
+    const { pageYOffset } = window;
+    if (this.opensLeft || this.opensRight) return pageYOffset + calcs.link('top') + calcs.link('halfH');
+    if (this.opensAbove) return pageYOffset + calcs.link('top');
+    return pageYOffset + calcs.link('bottom');
   }
 }
 
