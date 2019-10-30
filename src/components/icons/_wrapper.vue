35c69dd63e65594b60b3496f525131a7067f7ada
Jacob Bare <jacob@limit0.io>
2019-10-30 07:41:54 -0500

    Add icons

---
+++ src/components/icons/_wrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <tag
+    :is="tag"
+    v-if="name"
+    :class="classNames"
+  >
+    <slot />
+  </tag>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    className: {
+      type: String,
+      default: null,
+    },
+    tag: {
+      type: String,
+      default: 'span',
+    },
+  },
+  computed: {
+    classNames() {
+      return ['leaders-icon', `leaders-icon--${this.name}`, this.className];
+    },
+  },
+};
+</script>
