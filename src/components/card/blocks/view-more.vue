b73f169abf9e0089df97bb712fb700322ffd1568
Jacob Bare <jacob@limit0.io>
2019-11-01 12:30:58 -0500

    Create view-more block component

---
+++ src/components/card/blocks/view-more.vue
@@ -0,0 +1,44 @@
+<template>
+  <common-link :href="href" @click="emitClick">
+    {{ value }} &raquo;
+  </common-link>
+</template>
+
+<script>
+import CommonLink from '../../common/link.vue';
+
+export default {
+  components: { CommonLink },
+
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    prefix: {
+      type: String,
+      default: 'View more',
+    },
+  },
+
+  computed: {
+    value() {
+      const { prefix, label } = this;
+      return [prefix, label].filter(v => v).join(' ');
+    },
+  },
+
+  methods: {
+    emitClick(data, event) {
+      this.$emit('click', {
+        sourceLabel: this.value,
+        ...data,
+      }, event);
+    },
+  },
+};
+</script>
