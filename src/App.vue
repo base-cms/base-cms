9c237829c98be83f835c9fa91690d47b36703f09
Jacob Bare <jacob@limit0.io>
2019-11-02 20:29:29 -0500

    Cleanup example “App” component

--- src/App.vue
+++ src/App.vue
@@ -11,43 +11,10 @@
               <div style="height: 250px; margin-bottom: 1rem;">
                 Maybe an ad here?
               </div>
-              <leaders>
-                <div v-if="error">
-                  Error! {{ error.message }}
-                </div>
-                <div v-else-if="isLoading">
-                  Loading....
-                </div>
-                <div v-else-if="noResults">
-                  No results
-                </div>
-                <section-content-loader
-                  v-for="section in sections"
-                  v-else
-                  :key="section.id"
-                  v-slot:default="{ items }"
-                  :section-id="section.id"
-                  :title="section.name"
-                >
-                  <list
-                    :items="items"
-                    :identifier="section.id"
-                    nav-direction="vertical"
-                    open="left"
-                  >
-                    <template #nav-link="{ item, isActive }">
-                      <nav-link-contents
-                        :title="item.name"
-                        :is-active="isActive"
-                        :youtube="item.youtube"
-                      />
-                    </template>
-                    <template #dropdown="{ item, isActive }">
-                      <card :company="item" :is-active="isActive" @action="emitCardAction" />
-                    </template>
-                  </list>
-                </section-content-loader>
-              </leaders>
+              <content-sections
+                :content-id="13354061"
+                leaders-section-alias="leaders"
+              />
             </aside>
           </div>
         </div>
@@ -57,123 +24,11 @@
 </template>
 
 <script>
-import { get } from '@base-cms/object-path';
-import gql from 'graphql-tag';
-import SectionContentLoader from './components/section-content-loader.vue';
-import List from './components/list/index.vue';
-import Card from './components/card/index.vue';
-import Leaders from './components/root.vue';
-import NavLinkContents from './components/nav-link-contents.vue';
-
-const getNodes = (obj, field) => {
-  if (!obj || !obj[field]) return [];
-  return obj[field].edges.map(edge => edge.node);
-};
+import ContentSections from './components/content-sections.vue';
 
 export default {
   components: {
-    Card,
-    Leaders,
-    List,
-    NavLinkContents,
-    SectionContentLoader,
-  },
-
-
-  data: () => ({
-    contentId: 13320089,
-    sectionAlias: 'leaders',
-    sections: [],
-
-    isLoading: false,
-    noResults: false,
-    error: null,
-  }),
-
-  mounted() {
-    this.load();
-  },
-
-  methods: {
-    get(obj, path) {
-      return get(obj, path);
-    },
-
-    emitCardAction(...args) {
-      console.log('emitCardAction', ...args);
-    },
-
-    /**
-     *
-     */
-    async loadTaxonomyIds() {
-      const query = gql`
-        query Content($input: ContentQueryInput!) {
-          content(input: $input) {
-            id
-            taxonomy {
-              edges {
-                node {
-                  id
-                  type
-                }
-              }
-            }
-          }
-        }
-      `;
-      const input = { id: this.contentId };
-      const { data } = await this.$apollo.query({ query, variables: { input } });
-      return getNodes(data.content, 'taxonomy').map(node => node.id);
-    },
-
-    /**
-     *
-     */
-    async loadLeadersSections({ taxonomyIds }) {
-      const query = gql`
-        query WebsiteSections($input: WebsiteSectionsQueryInput = {}) {
-          websiteSections(input: $input) {
-            edges {
-              node {
-                id
-                name
-                hierarchy {
-                  id
-                  alias
-                }
-              }
-            }
-          }
-        }
-      `;
-      const input = { taxonomyIds };
-      const { data } = await this.$apollo.query({ query, variables: { input } });
-      const sections = getNodes(data, 'websiteSections');
-      if (!sections.length) return sections;
-      return sections
-        .filter(section => section.hierarchy.some(({ alias }) => alias === this.sectionAlias));
-    },
-
-    async load() {
-      this.isLoading = true;
-      try {
-        const taxonomyIds = await this.loadTaxonomyIds();
-        if (!taxonomyIds.length) {
-          this.noResults = true;
-          return;
-        }
-        this.sections = await this.loadLeadersSections({ taxonomyIds });
-        if (!this.sections.length) {
-          this.noResults = true;
-          return;
-        }
-      } catch (e) {
-        this.error = e;
-      } finally {
-        this.isLoading = false;
-      }
-    },
+    ContentSections,
   },
 };
 </script>
