ae5b8baa11892e4a0604f8e1eb9db8002757f1bb
Jacob Bare <jacob@limit0.io>
2019-11-02 19:30:47 -0500

    Match fonts to design

--- src/App.vue
+++ src/App.vue
@@ -179,7 +179,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Muli:300,400,600,700,800&display=swap");
+@import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,400,500,600,700&display=swap");
 @import "../node_modules/bootstrap/scss/bootstrap";
 @import "./scss/document";
 </style>
