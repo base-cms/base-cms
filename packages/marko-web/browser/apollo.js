4a37cec5a23d30c037a806be6c3f52c85b4b9d58
Jacob Bare <jacob@limit0.io>
2019-11-06 11:28:58 -0600

    Export provider as an instance, not a function

--- packages/marko-web/browser/apollo.js
+++ packages/marko-web/browser/apollo.js
@@ -4,7 +4,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import fragmentMatcher from '@base-cms/graphql-fragment-types/fragment-matcher';
 
-export default () => new VueApollo({
+export default new VueApollo({
   defaultClient: new ApolloClient({
     link: createHttpLink({ uri: '/__graphql' }),
     cache: new InMemoryCache({ fragmentMatcher }),
