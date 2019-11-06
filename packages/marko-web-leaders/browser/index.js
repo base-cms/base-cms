95cffa6b381c76c9a5b206d6227576af8e34825a
Jacob Bare <jacob@limit0.io>
2019-11-06 14:29:44 -0600

    Import and export leaders program  component

--- packages/marko-web-leaders/browser/index.js
+++ packages/marko-web-leaders/browser/index.js
@@ -1,1 +1,5 @@
-export default () => {};
+const LeadersProgram = () => import(/* webpackChunkName: "leaders-program" */ '@base-cms/leaders-program');
+
+export default (Browser) => {
+  Browser.regiser('LeadersProgram', LeadersProgram, { withApollo: true });
+};
