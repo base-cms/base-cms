53a4332d383062edc9c1f488f8fc91869b42ca38
Jacob Bare <jacob@limit0.io>
2019-11-06 12:39:27 -0600

    Delete apollo client and provider files

--- packages/leaders-program/src/apollo/fragment-matcher.js
+++
@@ -1,4 +0,0 @@
-import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
-import introspectionQueryResultData from '@base-cms/graphql-fragment-types';
-
-export default new IntrospectionFragmentMatcher({ introspectionQueryResultData });
