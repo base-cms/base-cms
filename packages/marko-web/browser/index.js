57f09757e75c423444aa97479dadad20c2116a77
Jacob Bare <jacob@limit0.io>
2019-11-06 10:30:16 -0600

    Create load and register; deprecate old versions

--- packages/marko-web/browser/index.js
+++ packages/marko-web/browser/index.js
@@ -6,23 +6,46 @@ import './lazysizes';
 
 const providers = {};
 
-const loadComponent = (el, name, props) => {
+const load = async ({
+  el,
+  name,
+  props,
+  on,
+} = {}) => {
+  if (!el || !name) throw new Error('A Vue component name and element must be provided.');
   const Component = components[name];
   if (!Component) throw new Error(`No Vue component found for '${name}'`);
   new Vue({
     provide: providers[name],
     el,
-    render: h => h(Component, { props }),
+    render: h => h(Component, { props, on }),
   });
 };
 
-const registerComponent = (name, Component, provide) => {
+const register = async (name, Component, { provide } = {}) => {
+  if (!name) throw new Error('A Vue component name must be provided.');
   if (components[name]) throw new Error(`A Vue component already exists for '${name}'`);
   components[name] = Component;
   providers[name] = provide;
 };
 
+/**
+ * @deprecated Use `load` instead.
+ */
+const loadComponent = (el, name, props) => {
+  load({ el, name, props });
+};
+
+/**
+ * @deprecated Use `register` instead.
+ */
+const registerComponent = (name, Component, provide) => {
+  register(name, Component, { provide });
+};
+
 export default {
+  load,
+  register,
   loadComponent,
   registerComponent,
   EventBus,
