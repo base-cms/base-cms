567887873c128253901b3d9e157e595c150fa201
Jacob Bare <jacob@limit0.io>
2019-11-07 12:22:25 -0600

    Set resources and user tools

--- services/example-website-iw/config/navigation.js
+++ services/example-website-iw/config/navigation.js
@@ -27,30 +27,22 @@ module.exports = {
         { href: '/the-economy', label: 'The Economy' },
       ],
     },
-    // {
-    //   label: 'Resources',
-    //   items: [
-    //     { href: '/magazine', label: 'In Print' },
-    //     { href: '/as-the-expert', label: 'Ask The Expert & Tech Tips' },
-    //     { href: '/blogs', label: 'Blogs' },
-    //     { href: '/videos', label: 'Videos' },
-    //     { href: '/shop-operations/training-resources/webinars', label: 'Webinars' },
-    //     { href: '/shop-operations/training-resources/case-studies-whitepapers',
-    // label: 'Whitepapers' },
-    //     { href: '/reviews', label: 'Tool Reviews' },
-    //     { href: 'https://vehicleservicepros.jobboard.io/', label: 'Job Board', target: '_blank' },
-    //   ],
-    // },
-    // {
-    //   label: 'User Tools',
-    //   items: [
-    //     { href: '/subscribe', label: 'Subscribe' },
-    //     { href: '/advertise', label: 'Advertise' },
-    //     { href: '/about-us', label: 'About Us' },
-    //     { href: '/contact-us', label: 'Contact Us' },
-    //     { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Policy', target: '_blank' },
-    //     { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms & Conditions', target: '_blank' },
-    //   ],
-    // },
+    {
+      label: 'Resources',
+      items: [
+        { href: '/webinars', label: 'Webinars' },
+        { href: '/white-papers', label: 'White Papers' },
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+        { href: '/subscribe', label: 'Subscribe' },
+        { href: '/contact-us', label: 'Contact Us' },
+        { href: '/advertise', label: 'Advertise' },
+        { href: 'https://www.endeavorbusinessmedia.com/privacy-policy', label: 'Privacy Statement', target: '_blank' },
+        { href: 'https://www.endeavorbusinessmedia.com/endeavor-terms', label: 'Terms of Service', target: '_blank' },
+      ],
+    },
   ],
 };
--- services/example-website-iw/server/routes/published-content.js
+++ services/example-website-iw/server/routes/published-content.js
@@ -5,7 +5,7 @@ const videos = require('../templates/published-content/videos');
 
 module.exports = (app) => {
   app.get('/events', (_, res) => { res.marko(events); });
-  app.get('/webcasts', (_, res) => { res.marko(webinars); });
+  app.get('/webinars', (_, res) => { res.marko(webinars); });
   app.get('/white-papers', (_, res) => { res.marko(whitePapers); });
   app.get('/videos', (_, res) => { res.marko(videos); });
 };
