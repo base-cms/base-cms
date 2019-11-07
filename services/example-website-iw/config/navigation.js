2010f38ff82e9030e6c585ed4e454891252c90e5
Jacob Bare <jacob@limit0.io>
2019-11-07 14:09:53 -0600

    Style the site mnu

--- services/example-website-iw/config/navigation.js
+++ services/example-website-iw/config/navigation.js
@@ -17,26 +17,20 @@ module.exports = {
   },
   menu: [
     {
-      label: 'Topics',
       items: [
         { href: '/talent', label: 'Talent' },
-        { href: '/technology-and-iiot', label: 'Technology and IIoT' },
-        { href: '/operations', label: 'Operation' },
+        { href: '/technology-and-iiot', label: 'Technology & IIoT' },
+        { href: '/operations', label: 'Operations' },
         { href: '/leadership', label: 'Leadership' },
         { href: '/supply-chain', label: 'Supply Chain' },
         { href: '/the-economy', label: 'The Economy' },
       ],
     },
     {
-      label: 'Resources',
+      modifiers: ['secondary'],
       items: [
         { href: '/webinars', label: 'Webinars' },
         { href: '/white-papers', label: 'White Papers' },
-      ],
-    },
-    {
-      label: 'User Tools',
-      items: [
         { href: '/subscribe', label: 'Subscribe' },
         { href: '/contact-us', label: 'Contact Us' },
         { href: '/advertise', label: 'Advertise' },
