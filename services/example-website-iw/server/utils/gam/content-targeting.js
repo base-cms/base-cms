2ef659ad9773f1961520d060eebe13b666338927
Brandon Krigbaum <brandonbk@gmail.com>
2019-11-11 09:54:53 -0600

    Use get for deep object properties

--- services/example-website-iw/server/utils/gam/content-targeting.js
+++ services/example-website-iw/server/utils/gam/content-targeting.js
@@ -1,4 +1,5 @@
 const { isObject } = require('@base-cms/utils');
+const { get } = require('@base-cms/object-path');
 
 module.exports = (content) => {
   if (!isObject(content)) return '';
@@ -6,8 +7,8 @@ module.exports = (content) => {
     cont_id: content.id,
     cont_type: content.type,
     ptype: content.type,
-    pterm: content.primarySection.name,
-    sterm: content.primarySection.name,
+    pterm: get(content, 'primarySection.name'),
+    sterm: get(content, 'primarySection.name'),
     // author: '', // @todo Determine if this is needed, how to handle multiple/zero authors
     reg: 'anonymous', // @todo Get this from identity-x
   };
