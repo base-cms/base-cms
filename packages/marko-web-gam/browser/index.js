a8f645a73acc603cd138485b40bdda2251e5b85d
Jacob Bare <jacob@limit0.io>
2019-11-11 12:22:17 -0600

    Create refreshable ad support

--- packages/marko-web-gam/browser/index.js
+++ packages/marko-web-gam/browser/index.js
@@ -1,5 +1,11 @@
+import GAMRefreshAd from './refresh-ad.vue';
+
 const GAMFixedAdBottom = () => import(/* webpackChunkName: "gam-fixed-ad-bottom" */ './fixed-ad-bottom.vue');
 
 export default (Browser) => {
+  const { EventBus } = Browser;
+  Browser.register('GAMRefreshAd', GAMRefreshAd, {
+    provide: { EventBus },
+  });
   Browser.register('GAMFixedAdBottom', GAMFixedAdBottom);
 };
