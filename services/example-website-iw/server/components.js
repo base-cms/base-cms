d4c52b5bae9e20af4f29d7ac77943d1beb8ac8b4
Jacob Bare <jacob@limit0.io>
2019-11-12 08:44:41 -0600

    Add content-to-content scroll

--- services/example-website-iw/server/components.js
+++ services/example-website-iw/server/components.js
@@ -1,11 +1,13 @@
 const ContentCardDeckFlow = require('./components/flows/content-card-deck');
 const ContentLoadMoreFlow = require('./components/flows/content-load-more');
+const ContentPageLoadMoreFlow = require('./components/flows/content-page-load-more');
 const ContentListFlow = require('./components/flows/content-list');
 const MagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   ContentCardDeckFlow,
   ContentLoadMoreFlow,
+  ContentPageLoadMoreFlow,
   ContentListFlow,
   MagazineIssueArchiveFlow,
 };
--- services/example-website-iw/server/fragments.js
+++ services/example-website-iw/server/fragments.js
@@ -1,7 +1,9 @@
 const contentList = require('./graphql/fragments/content-list');
+const contentPage = require('./graphql/fragments/content-page');
 const magazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
 
 module.exports = {
   contentList,
+  contentPage,
   magazineIssueArchive,
 };
