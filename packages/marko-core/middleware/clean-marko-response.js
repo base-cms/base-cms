2f4a30754b9553c2e9edd466b9f95acecdde27af
Jacob Bare <jacob@limit0.io>
2019-11-13 12:28:01 -0600

    Strip appended component JS

--- packages/marko-core/middleware/clean-marko-response.js
+++ packages/marko-core/middleware/clean-marko-response.js
@@ -1,4 +1,5 @@
 const stripComponentJS = chunk => chunk.replace(/<script>\(function\(\){var w=window;w\.\$components=.*w\.\$components}\)\(\)<\/script>/, '');
+const stripAppendedJS = chunk => chunk.replace(/<script>\$components=.*\$components<\/script>/, '');
 const stripComments = chunk => chunk.replace(/<!--[A-Z][#/].*?-->/g, '');
 
 module.exports = ({ enabled = true } = {}) => (req, res, next) => {
@@ -9,7 +10,7 @@ module.exports = ({ enabled = true } = {}) => (req, res, next) => {
     } else {
       const [chunk] = args;
       const cleanedArgs = [...args];
-      cleanedArgs[0] = stripComponentJS(stripComments(chunk));
+      cleanedArgs[0] = stripAppendedJS(stripComponentJS(stripComments(chunk)));
       write.apply(this, cleanedArgs);
     }
   };
