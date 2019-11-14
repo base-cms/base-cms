6e4088430d567dc781e2ed76e170f3127156f5dd
Josh Worden <solocommand@gmail.com>
2019-11-14 16:41:01 -0600

    Update config, add lint

--- packages/marko-web-identity-x/config.js
+++ packages/marko-web-identity-x/config.js
@@ -2,14 +2,29 @@ class IdentityXConfiguration {
   /**
    *
    * @param {string} appId
+   * @param {string} sentryDsn
    */
-  constructor(appId) {
+  constructor(appId, sentryDsn, mountPoint = '/__idx') {
     if (!appId) throw new Error('Unable to configure IdentityX: no Application ID was provided.');
     this.appId = appId;
+    this.sentryDsn = sentryDsn;
+    this.mountPoint = mountPoint;
   }
 
   getAppId() {
     return this.appId;
+  }
+
+  getSentryDsn() {
+    return this.sentryDsn;
+  }
+
+  getMountPoint() {
+    return this.mountPoint;
+  }
+
+  isSentryEnabled() {
+    return Boolean(this.sentryDsn);
   }
 }
 
