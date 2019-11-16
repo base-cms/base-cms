23e1ff8e0f6797164134082c67311a2011acf167
Jacob Bare <jacob@limit0.io>
2019-11-16 15:43:25 -0600

    Remove unused fields from list fragment

--- services/example-website-iw/server/graphql/fragments/content-list.js
+++ services/example-website-iw/server/graphql/fragments/content-list.js
@@ -5,21 +5,12 @@ module.exports = gql`
 fragment WebsiteContentListFragment on Content {
   id
   type
-  typeTitled: type(input: { format: titleize })
   shortName
   teaser(input: { useFallback: false, maxLength: null })
   siteContext {
     path
   }
   published
-  company {
-    id
-    type
-    name
-    siteContext {
-      path
-    }
-  }
   primarySection {
     id
     name
@@ -32,31 +23,9 @@ fragment WebsiteContentListFragment on Content {
     alt
     isLogo
   }
-  userRegistration {
-    isRequired
-  }
-  ... on Authorable {
-    authors {
-      edges {
-        node {
-          id
-          name
-          type
-          siteContext {
-            path
-          }
-        }
-      }
-    }
-  }
   ... on ContentPromotion {
     linkText
     linkUrl
-  }
-  ... on ContentContact {
-    title
-    phone
-    publicEmail
   }
   ... on ContentWebinar {
     startDate
