2d1947748e6e66ae4a7674935fbf2c54d10dde36
Jacob Bare <jacob@limit0.io>
2019-11-16 15:57:55 -0600

    Remove magazine routes and templates

--- services/example-website-iw/server/routes/index.js
+++ services/example-website-iw/server/routes/index.js
@@ -1,7 +1,6 @@
 const home = require('./home');
 const content = require('./content');
 const dynamicPages = require('./dynamic-page');
-const magazine = require('./magazine');
 const search = require('./search');
 const subscribe = require('./subscribe');
 const websiteSections = require('./website-section');
@@ -9,9 +8,6 @@ const websiteSections = require('./website-section');
 module.exports = (app) => {
   // Homepage
   home(app);
-
-  // Magazine Pages
-  magazine(app);
 
   // Dynamic Pages
   dynamicPages(app);
