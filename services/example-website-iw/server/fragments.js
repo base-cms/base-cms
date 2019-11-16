f21fab0d1dedd62056016af36b69ac8f0d56a828
Jacob Bare <jacob@limit0.io>
2019-11-16 15:59:58 -0600

    Remove magazine fragments

--- services/example-website-iw/server/fragments.js
+++ services/example-website-iw/server/fragments.js
@@ -1,9 +1,7 @@
 const contentList = require('./graphql/fragments/content-list');
 const contentPage = require('./graphql/fragments/content-page');
-const magazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
 
 module.exports = {
   contentList,
   contentPage,
-  magazineIssueArchive,
 };
