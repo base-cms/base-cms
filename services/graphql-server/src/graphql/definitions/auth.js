c16e7ab66692265f7c30c526d1915de662f5b950
Josh Worden <solocommand@gmail.com>
2019-11-19 16:27:16 -0600

    Add user auth definitions

--- services/graphql-server/src/graphql/definitions/auth.js
+++ services/graphql-server/src/graphql/definitions/auth.js
@@ -6,10 +6,30 @@ extend type Query {
   activeUser: User @requiresAuth
 }
 
+extend type Mutation {
+  userTokenCreate(input: UserLoginMutationInput!): UserAuthentication!
+  userTokenDestroy: String! @requiresAuth
+}
+
 enum AuthRole {
   admin
   member
   restricted
+}
+
+type UserAuthentication {
+  user: User!
+  token: UserAuthToken!
+}
+
+type UserAuthToken {
+  id: String!
+  value: String!
+}
+
+input UserLoginMutationInput {
+  username: String!
+  password: String!
 }
 
 `;
