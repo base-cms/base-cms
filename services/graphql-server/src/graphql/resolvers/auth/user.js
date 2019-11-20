e9c34ff0614ce7914ac6ab5fabdd5238ff7da37b
Josh Worden <solocommand@gmail.com>
2019-11-19 19:08:07 -0600

    Use user service

--- services/graphql-server/src/graphql/resolvers/auth/user.js
+++ services/graphql-server/src/graphql/resolvers/auth/user.js
@@ -1,17 +1,14 @@
-const service = require('../../../user-context/service');
+const service = require('../../../auth-context/user-service');
 
 module.exports = {
   Query: {
     activeUser: (_, __, { user }) => user.getUser(),
   },
   Mutation: {
-    userLogin: async (_, { input }) => {
+    userLogin: (_, { input }) => {
       const { username, password } = input;
       return service.login(username, password);
     },
-    userLogout: async () => {
-      await service.logout();
-      return 'ok';
-    },
+    userLogout: () => service.logout(),
   },
 };
