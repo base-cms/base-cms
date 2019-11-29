f9f8afce8d57475a523f8877bb8415910f43fba5
Jacob Bare <jacob@limit0.io>
2019-11-29 12:27:17 -0600

    Add collapse support to individual ad slots

--- packages/marko-web-gam/utils/build-slots.js
+++ packages/marko-web-gam/utils/build-slots.js
@@ -1,8 +1,16 @@
 const { isObject } = require('@base-cms/utils');
+const defaultValue = require('@base-cms/marko-core/utils/default-value');
 const buildTargeting = require('./build-targeting');
 const buildSizeMapping = require('./build-size-mapping');
 
 const { isArray } = Array;
+
+const buildCollapse = (collapse, collapseBeforeAdFetch) => {
+  const args = [defaultValue(collapse, true) ? 'true' : 'false'];
+  if (collapseBeforeAdFetch === true) args.push('true');
+  if (collapseBeforeAdFetch === false) args.push('false');
+  return `setCollapseEmptyDiv(${args.join(', ')})`;
+};
 
 module.exports = (slots) => {
   if (!isObject(slots)) return [];
@@ -15,10 +23,13 @@ module.exports = (slots) => {
       size,
       sizeMapping,
       targeting,
+      collapse,
+      collapseBeforeAdFetch,
     }) => [
       `googletag.defineSlot('${path}', ${JSON.stringify(size)}, '${id}')`,
       buildTargeting(targeting),
       buildSizeMapping(sizeMapping),
+      buildCollapse(collapse, collapseBeforeAdFetch),
       'addService(googletag.pubads());',
     ].filter(v => v).join('.'));
 };
