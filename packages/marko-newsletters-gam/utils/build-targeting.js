5453412c255ca3b6abba37e5ebfc1a2cec00cac7
Jacob Bare <jacob@limit0.io>
2019-12-02 15:03:27 -0600

    Allow non-null falsey values to be set

--- packages/marko-newsletters-gam/utils/build-targeting.js
+++ packages/marko-newsletters-gam/utils/build-targeting.js
@@ -2,6 +2,6 @@ const { URLSearchParams } = require('url');
 
 module.exports = (kvs = {}) => Object.keys(kvs).reduce((params, key) => {
   const value = kvs[key];
-  if (value) params.set(key, value);
+  if (value != null) params.set(key, value);
   return params;
 }, new URLSearchParams()).toString();
