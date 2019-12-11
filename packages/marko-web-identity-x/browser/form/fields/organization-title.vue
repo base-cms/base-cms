590adf50d0b273a9a701877e4ea48c6bf81b6c26
Jacob Bare <jacob@limit0.io>
2019-12-11 16:37:56 -0600

    Create organization fields

---
+++ packages/marko-web-identity-x/browser/form/fields/organization-title.vue
@@ -0,0 +1,55 @@
+<template>
+  <form-group>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      v-model="organizationTitle"
+      class="form-control"
+      type="text"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      autocomplete="organization-title"
+    >
+  </form-group>
+</template>
+
+<script>
+import FormGroup from '../common/form-group.vue';
+
+export default {
+  components: {
+    FormGroup,
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: 'Job Title',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    id: 'sign-on-organization-title',
+  }),
+  computed: {
+    organizationTitle: {
+      get() {
+        return this.value;
+      },
+      set(organizationTitle) {
+        this.$emit('input', organizationTitle);
+      },
+    },
+  },
+};
+</script>
---
+++ packages/marko-web-identity-x/browser/form/fields/organization.vue
@@ -0,0 +1,55 @@
+<template>
+  <form-group>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      v-model="organization"
+      class="form-control"
+      type="text"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      autocomplete="organization"
+    >
+  </form-group>
+</template>
+
+<script>
+import FormGroup from '../common/form-group.vue';
+
+export default {
+  components: {
+    FormGroup,
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: 'Company Name',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    id: 'sign-on-organization',
+  }),
+  computed: {
+    organization: {
+      get() {
+        return this.value;
+      },
+      set(organization) {
+        this.$emit('input', organization);
+      },
+    },
+  },
+};
+</script>
