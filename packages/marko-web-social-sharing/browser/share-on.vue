8ffc4b62e681919c2d8117acec58f38affce9b56
Jacob Bare <jacob@limit0.io>
2019-12-13 08:35:09 -0600

    Build sharer url

--- packages/marko-web-social-sharing/browser/share-on.vue
+++ packages/marko-web-social-sharing/browser/share-on.vue
@@ -38,8 +38,26 @@ export default {
     this.type = provider.type;
   },
   methods: {
+    encode(v) {
+      return encodeURIComponent(v);
+    },
+    buildSharerUrl() {
+      const pattern = /^@/;
+      const kvs = Object.keys(this.params).map((key) => {
+        const value = this.params[key];
+        if (pattern.test(value)) {
+          // Needs to be replaced with a component value.
+          const prop = value.replace(pattern, '');
+          return { key, value: this[prop] };
+        }
+        // Return value as is
+        return { key, value };
+      }).filter(({ value }) => value).map(({ key, value }) => `${this.encode(key)}=${this.encode(value)}`);
+      return `${this.href}?${kvs.join('&')}`;
+    },
     share() {
-      console.log('share!', this.name, this.url);
+      const url = this.buildSharerUrl();
+      console.log('share!', this.name, url);
     },
   },
 };
