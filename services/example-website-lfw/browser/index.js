745f2bdb179625a9dfdc4c453342f23eb818fae9
Jacob Bare <jacob@limit0.io>
2019-12-13 13:02:30 -0600

    Display share icons on example site

--- services/example-website-lfw/browser/index.js
+++ services/example-website-lfw/browser/index.js
@@ -4,11 +4,13 @@ import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import GCSE from '@base-cms/marko-web-gcse/browser';
 import RevealAd from '@base-cms/marko-web-reveal-ad/browser';
+import SocialSharing from '@base-cms/marko-web-social-sharing/browser';
 
 DefaultTheme(Browser);
 GTM(Browser);
 GAM(Browser);
 GCSE(Browser);
 RevealAd(Browser);
+SocialSharing(Browser);
 
 export default Browser;
