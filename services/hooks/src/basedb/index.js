17cdaca96bff94d832b525d9bb8e0f5ecaed2f08
Jacob Bare <jacob@limit0.io>
2019-12-18 09:53:50 -0600

    Add gemenon stack

--- services/hooks/src/basedb/index.js
+++ services/hooks/src/basedb/index.js
@@ -1,9 +1,11 @@
 const aerilon = require('./aerilon');
 const caprica = require('./caprica');
 const picon = require('./picon');
+const gemenon = require('./gemenon');
 
 module.exports = tenant => ({
   aerilon: aerilon(tenant),
   caprica: caprica(tenant),
   picon: picon(tenant),
+  gemenon: gemenon(tenant),
 });
