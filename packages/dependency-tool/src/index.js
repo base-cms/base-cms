e9aee9a532963cd6e595613c6c5c05f358e06d0f
Jacob Bare <jacob@limit0.io>
2020-01-16 13:41:34 -0600

    Throw on unhandled rejections

--- packages/dependency-tool/src/index.js
+++ packages/dependency-tool/src/index.js
@@ -2,6 +2,8 @@ const program = require('yargs');
 const log = require('fancy-log');
 const commands = require('./commands');
 
+process.on('unhandledRejection', (e) => { throw e; });
+
 log('Dependency tool starting...');
 program
   .usage('Usage: $0 <command> [options]')
