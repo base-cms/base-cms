69b76155094ad1972a6c426595551ccf1dcf16d3
Josh Worden <solocommand@gmail.com>
2020-01-23 11:53:45 -0600

    Update Export CLI to use CLI Utils

--- packages/export-cli/src/commands/lint.js
+++ packages/export-cli/src/commands/lint.js
@@ -1,5 +1,5 @@
-const logCmd = require('../utils/log-command');
-const cwd = require('../utils/get-cwd');
+const logCmd = require('@base-cms/cli-utils/src/log-command');
+const cwd = require('@base-cms/cli-utils/src/get-cwd');
 const lint = require('../gulp/lint');
 
 module.exports = ({ path }) => {
