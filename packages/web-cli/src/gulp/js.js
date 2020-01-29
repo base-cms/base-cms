228f4e329a03fee681d416312311bb3cae91deb3
Jacob Bare <jacob@limit0.io>
2020-01-29 08:10:47 -0600

    Check for webpack config callback function

--- packages/web-cli/src/gulp/js.js
+++ packages/web-cli/src/gulp/js.js
@@ -48,89 +48,113 @@ const readRcFile = (cwd) => {
   return { exclude, targets, debug };
 };
 
+/**
+ * Loads a configuration callback in the current working
+ * directory that receives the base webpack config.
+ *
+ * This function can modify the webpack config in any way.
+ *
+ * @param {string} cwd
+ * @returns {function|null}
+ */
+const loadConfigCallback = (cwd) => {
+  const fileName = 'webpack.base-cms.js';
+  const fileLoc = join(cwd, fileName);
+  const fileExists = existsSync(fileLoc);
+  if (!fileExists) return null;
+  // eslint-disable-next-line import/no-dynamic-require
+  const fn = require(fileLoc);
+  return typeof fn === 'function' ? fn : null;
+};
+
 // @todo Determine how to combine CSS with the main CSS build
 // @todo Add sass to Vue components
 // @todo Add default polyfills to entry point
 module.exports = cwd => (cb) => {
   const { exclude, targets, debug } = readRcFile(cwd);
+  const configCallback = loadConfigCallback(cwd);
   const imagePattern = /\.(png|svg|jpg|gif|webp)$/;
   const { ifProduction, ifNotProduction } = getIfUtils(process.env.NODE_ENV || 'development');
-  pump([
-    src('browser/index.js', { cwd }),
-    webpack({
-      mode: ifProduction('production', 'development'),
-      cache: ifNotProduction(),
-      devtool: 'source-map',
-      output: {
-        library: 'CMSBrowserComponents',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        filename: 'index.[contenthash:8].js',
-        chunkFilename: '[name].[contenthash:8].js',
-        publicPath: '/dist/js/',
-      },
-      module: {
-        rules: [
-          {
-            test: /\.vue$/,
-            loader: require.resolve('vue-loader'),
-          },
-          {
-            test: /\.js$/,
-            loader: require.resolve('babel-loader'),
-            exclude,
-            options: {
-              presets: [
-                [
-                  require.resolve('@babel/preset-env'),
-                  {
-                    modules: false,
-                    useBuiltIns: false,
-                    targets,
-                    loose: false,
-                    debug,
-                  },
-                ],
+
+  let baseConfig = {
+    mode: ifProduction('production', 'development'),
+    cache: ifNotProduction(),
+    devtool: 'source-map',
+    output: {
+      library: 'CMSBrowserComponents',
+      libraryExport: 'default',
+      libraryTarget: 'umd',
+      filename: 'index.[contenthash:8].js',
+      chunkFilename: '[name].[contenthash:8].js',
+      publicPath: '/dist/js/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: require.resolve('vue-loader'),
+        },
+        {
+          test: /\.js$/,
+          loader: require.resolve('babel-loader'),
+          exclude,
+          options: {
+            presets: [
+              [
+                require.resolve('@babel/preset-env'),
+                {
+                  modules: false,
+                  useBuiltIns: false,
+                  targets,
+                  loose: false,
+                  debug,
+                },
               ],
-              plugins: [
-                [
-                  require.resolve('@babel/plugin-transform-runtime'),
-                  {
-                    regenerator: true,
-                    corejs: false,
-                    helpers: true,
-                    useESModules: false,
-                    absoluteRuntime,
-                  },
-                ],
+            ],
+            plugins: [
+              [
+                require.resolve('@babel/plugin-transform-runtime'),
+                {
+                  regenerator: true,
+                  corejs: false,
+                  helpers: true,
+                  useESModules: false,
+                  absoluteRuntime,
+                },
               ],
-            },
-          },
-          {
-            test: /\.css$/,
-            use: [
-              require.resolve('vue-style-loader'),
-              require.resolve('css-loader'),
             ],
           },
-          {
-            test: imagePattern,
-            loader: require.resolve('file-loader'),
-            options: {
-              name: '[name].[ext]',
-              outputPath: '../assets',
-            },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            require.resolve('vue-style-loader'),
+            require.resolve('css-loader'),
+          ],
+        },
+        {
+          test: imagePattern,
+          loader: require.resolve('file-loader'),
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../assets',
           },
-        ],
-      },
-      plugins: [
-        new VueLoaderPlugin(),
-        new ManifestPlugin({
-          publicPath: '',
-          filter: ({ name }) => !imagePattern.test(name),
-        }),
+        },
       ],
-    }, wp),
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new ManifestPlugin({
+        publicPath: '',
+        filter: ({ name }) => !imagePattern.test(name),
+      }),
+    ],
+  };
+  if (configCallback) baseConfig = configCallback({ baseConfig, ifProduction, ifNotProduction });
+
+  pump([
+    src('browser/index.js', { cwd }),
+    webpack(baseConfig, wp),
     dest('dist/js', { cwd }),
   ], e => completeTask(e, cb));
 };
