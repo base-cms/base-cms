a21cceb5bea87bf8ec00adb0d1d559eb898dd614
Josh Worden <solocommand@gmail.com>
2020-02-06 12:30:04 -0600

    Add leaders to LFW example site

---
+++ services/example-website-lfw/config/leaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Leadership in Lasers',
+  alias: 'leaders/2020',
+  calloutValue: 'Leading Companies',
+  header: {
+    imgSrc: 'https://base.imgix.net/files/base/pennwell/lfw/logo.png?h=90',
+  },
+};
