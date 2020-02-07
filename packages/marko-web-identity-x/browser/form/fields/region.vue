b161468763e80e37484318fb54d944c461538931
Jacob Bare <jacob@limit0.io>
2020-02-07 10:25:04 -0600

    Create region field

---
+++ packages/marko-web-identity-x/browser/form/fields/region.vue
@@ -0,0 +1,94 @@
+<template>
+  <form-group>
+    <label :for="id">{{ label }}</label>
+    <select
+      :id="id"
+      v-model="regionCode"
+      :readonly="isLoading"
+      :disabled="disabled"
+      :required="true"
+      class="custom-select"
+      autocomplete="address-level1"
+    >
+      <option dsiabled value="">
+        Select state/region...
+      </option>
+      <option v-for="region in countryRegions" :key="region.id" :value="region.code">
+        {{ region.name }}
+      </option>
+    </select>
+    <p v-if="error">
+      Unable to load regions: {{ error.message }}
+    </p>
+  </form-group>
+</template>
+
+<script>
+import FormGroup from '../common/form-group.vue';
+import get from '../../utils/get';
+import regionCountryCodes from '../../utils/region-country-codes';
+
+export default {
+  components: {
+    FormGroup,
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: 'State / Region',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    countryCode: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    id: 'sign-on-region',
+    isLoading: false,
+    error: null,
+    regions: [],
+  }),
+  computed: {
+    regionCode: {
+      get() {
+        return this.value;
+      },
+      set(regionCode) {
+        this.$emit('input', regionCode);
+      },
+    },
+    hasValidCountryCode() {
+      return regionCountryCodes.includes(this.countryCode);
+    },
+    countryRegions() {
+      if (!this.hasValidCountryCode) return [];
+      return this.regions.filter(region => region.country.id === this.countryCode);
+    },
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    async load() {
+      this.error = null;
+      this.isLoading = true;
+      try {
+        const res = await get('/regions');
+        this.regions = await res.json();
+      } catch (e) {
+        this.error = e;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+};
+</script>
