b55efdd892ff554b505ce7d54a13ee42588f91c5
Jacob Bare <jacob@limit0.io>
2020-02-07 10:48:37 -0600

    Create form label component

---
+++ packages/marko-web-identity-x/browser/form/common/form-label.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>
+    <slot />
+    <span v-if="required" class="ml-1 text-danger">*</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
