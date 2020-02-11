570a6368e2d9888ca4204be3be87b458e26154ae
Jacob Bare <jacob@limit0.io>
2020-02-11 08:13:59 -0600

    Add options constructor; server + client fields

--- packages/marko-web-identity-x/config.js
+++ packages/marko-web-identity-x/config.js
@@ -1,15 +1,42 @@
+const { get, getAsArray, getAsObject } = require('@base-cms/object-path');
+
 class IdentityXConfiguration {
   /**
    *
-   * @param {string} appId
+   * @param {object|string} options When a string, assumes an `appId`, else options object.
+   * @param {array} [options.requiredServerFields] Required fields, server enforced.
+   * @param {array} [options.requiredClientFields] Required fields, client-side only.
    */
-  constructor(appId) {
+  constructor(options) {
+    // BC check for when the constructor only had a single `appId` argument.
+    const appId = typeof options === 'string' ? options : get(options, 'appId');
     if (!appId) throw new Error('Unable to configure IdentityX: no Application ID was provided.');
     this.appId = appId;
+    this.options = options && typeof options === 'object' ? options : {};
   }
 
   getAppId() {
     return this.appId;
+  }
+
+  getRequiredServerFields() {
+    return this.getAsArray('requiredServerFields');
+  }
+
+  getRequiredClientFields() {
+    return this.getAsArray('requiredClientFields');
+  }
+
+  get(path, def) {
+    return get(this.options, path, def);
+  }
+
+  getAsArray(path) {
+    return getAsArray(this.options, path);
+  }
+
+  getAsObject(path) {
+    return getAsObject(this.options, path);
   }
 }
 
