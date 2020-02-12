af6c12b4f0e9befef8f7134dbb5a73274a69d140
Jacob Bare <jacob@limit0.io>
2020-02-12 07:46:04 -0600

    Remove unused form component

--- packages/marko-web-identity-x/browser/index.js
+++ packages/marko-web-identity-x/browser/index.js
@@ -2,14 +2,11 @@ const Authenticate = () => import(/* webpackChunkName: "identity-x-authenticate"
 const Logout = () => import(/* webpackChunkName: "identity-x-logout" */ './logout.vue');
 const Login = () => import(/* webpackChunkName: "identity-x-login" */ './login.vue');
 const Profile = () => import(/* webpackChunkName: "identity-x-profile" */ './profile.vue');
-const Form = () => import(/* webpackChunkName: "identity-x-form" */ './form.vue');
 
 export default (Browser, {
   CustomLoginComponent,
-  CustomFormComponent,
 } = {}) => {
   const LoginComponent = CustomLoginComponent || Login;
-  const FormComponent = CustomFormComponent || Form;
 
   const { EventBus } = Browser;
   Browser.register('IdentityXAuthenticate', Authenticate, {
@@ -23,8 +20,5 @@ export default (Browser, {
   });
   Browser.register('IdentityXProfile', Profile, {
     on: { action: (...args) => EventBus.$emit('identity-x-profile', ...args) },
-  });
-  Browser.register('IdentityXForm', FormComponent, {
-    on: { action: (...args) => EventBus.$emit('identity-x-submit', ...args) },
   });
 };
