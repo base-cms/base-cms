b62ede0ca1de9dddd71cce129044fcb4474653d4
Jacob Bare <jacob@limit0.io>
2020-02-13 10:35:26 -0600

    Log code and signal on close

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,11 @@ module.exports = ({
   const serve = async () => {
     if (node) node.kill();
     node = await spawn('node', [entry], { stdio: 'inherit' });
-    node.on('close', (code) => {
-      if (code === 8) {
-        log('Error detected, waiting for changes...');
-      }
+    node.on('close', (code, signal) => {
+      const exited = [];
+      if (code) exited.push(`code ${(code)}`);
+      if (signal) exited.push(`signal ${(signal)}`);
+      log(`Process ${('exited')} with ${exited.join(' ')}`);
     });
   };
 
