38a66ff9a1dac50fda828ee52367fd9b15174633
Jacob Bare <jacob@limit0.io>
2020-02-13 10:39:01 -0600

    Add timeout handling to rss service

--- services/rss/src/index.js
+++ services/rss/src/index.js
@@ -2,7 +2,13 @@ require('./newrelic');
 const http = require('http');
 const { createTerminus } = require('@godaddy/terminus');
 const newrelic = require('./newrelic');
-const { GRAPHQL_URI, PORT, EXPOSED_PORT } = require('./env');
+const {
+  GRAPHQL_URI,
+  PORT,
+  EXPOSED_PORT,
+  TERMINUS_TIMEOUT: timeout,
+  TERMINUS_SHUTDOWN_DELAY: beforeShutdownTimeout,
+} = require('./env');
 const app = require('./app');
 const pkg = require('../package.json');
 const { log } = require('./output');
@@ -11,11 +17,16 @@ const server = http.createServer(app);
 
 const run = async () => {
   createTerminus(server, {
-    timeout: 1000,
+    timeout,
     signals: ['SIGTERM', 'SIGINT', 'SIGHUP', 'SIGQUIT'],
     healthChecks: { '/_health': () => Promise.resolve(`${pkg.name} pinged successfully.`) },
     onSignal: () => {
       log('> Cleaning up...');
+    },
+    beforeShutdown: () => {
+      log(`> Delaying shutdown by ${beforeShutdownTimeout}ms...`);
+      return new Promise(resolve => setTimeout(resolve, beforeShutdownTimeout))
+        .then(() => log('> Shutdown delay complete.'));
     },
     onShutdown: () => log('> Cleanup finished. Shutting down.'),
   });
