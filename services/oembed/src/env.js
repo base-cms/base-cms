f620b9827b5058654e8ef20103c0327b3cde5e9a
Jacob Bare <jacob@limit0.io>
2020-02-13 10:42:00 -0600

    Add timeout handling to oembed service

--- services/oembed/src/env.js
+++ services/oembed/src/env.js
@@ -4,6 +4,7 @@ const {
   custom,
   cleanEnv,
   bool,
+  num,
 } = envalid;
 const { nonemptystr } = custom;
 
@@ -11,4 +12,6 @@ module.exports = cleanEnv(process.env, {
   EMBEDLY_API_KEY: nonemptystr({ desc: 'The Embed.ly API key..' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
+  TERMINUS_TIMEOUT: num({ desc: 'Number of milliseconds before forceful exiting', default: 30000 }),
+  TERMINUS_SHUTDOWN_DELAY: num({ desc: 'Number of milliseconds before the HTTP server starts its shutdown', default: 5000 }),
 });
--- services/oembed/src/index.js
+++ services/oembed/src/index.js
@@ -2,6 +2,10 @@ require('./newrelic');
 const http = require('http');
 const { createTerminus } = require('@godaddy/terminus');
 const newrelic = require('./newrelic');
+const {
+  TERMINUS_TIMEOUT: timeout,
+  TERMINUS_SHUTDOWN_DELAY: beforeShutdownTimeout,
+} = require('./env');
 const app = require('./app');
 const pkg = require('../package.json');
 
@@ -11,11 +15,16 @@ const server = http.createServer(app);
 
 const run = async () => {
   createTerminus(server, {
-    timeout: 1000,
+    timeout,
     signals: ['SIGTERM', 'SIGINT', 'SIGHUP', 'SIGQUIT'],
     healthChecks: { '/_health': () => Promise.resolve(`${pkg.name} pinged successfully.`) },
     onSignal: () => {
       log('> Cleaning up...');
+    },
+    beforeShutdown: () => {
+      log(`> Delaying shutdown by ${beforeShutdownTimeout}ms...`);
+      return new Promise(resolve => setTimeout(resolve, beforeShutdownTimeout))
+        .then(() => log('> Shutdown delay complete.'));
     },
     onShutdown: () => log('> Cleanup finished. Shutting down.'),
   });
