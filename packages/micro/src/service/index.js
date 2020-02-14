90090610c52ac842d55bdd3966152c0bd1b5eb1c
Jacob Bare <jacob@limit0.io>
2020-02-14 13:12:22 -0600

    Create micro json server with terminus wrapper

--- packages/micro/src/service/index.js
+++ packages/micro/src/service/index.js
@@ -1,9 +1,11 @@
 const json = require('./json-service');
+const jsonServer = require('./json-server');
 const createParamError = require('./param-error');
 const createRequiredParamError = require('./required-param-error');
 
 module.exports = {
   json,
+  jsonServer,
   createParamError,
   createRequiredParamError,
 };
