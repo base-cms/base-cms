bf36a5e3234d0624952a59d07437391aa5af4181
Brian Miller <bmiller@southcomm.com>
2020-03-09 14:16:53 -0500

    remove extra checks

--- services/graphql-server/src/graphql/query-builders/published-content.js
+++ services/graphql-server/src/graphql/query-builders/published-content.js
@@ -3,18 +3,8 @@ const { getPublishedContentCriteria } = require('@base-cms/utils');
 module.exports = ({ query }, { input }) => {
   const { since, after } = input;
 
-  if (since && after) {
+  if (since || after) {
     const criteria = getPublishedContentCriteria({ since, after });
-    return { query: { ...query, ...criteria } };
-  }
-
-  if (since) {
-    const criteria = getPublishedContentCriteria({ since });
-    return { query: { ...query, ...criteria } };
-  }
-
-  if (after) {
-    const criteria = getPublishedContentCriteria({ after });
     return { query: { ...query, ...criteria } };
   }
 
