304a17925ca8ac52bf0c9cc9e83b486b2da69987
Jacob Bare <jacob@limit0.io>
2020-03-10 08:32:53 -0500

    Create comment create component

---
+++ packages/marko-web-identity-x/browser/comments/create.vue
@@ -0,0 +1,57 @@
+<template>
+  <form :class="blockName" @submit.prevent="handleSubmit">
+    <fieldset :disabled="isLoading">
+      <display-name v-model="displayName" label="Posting As" />
+      <comment-body v-model="body" />
+      <button
+        type="submit"
+        class="btn btn-primary"
+      >
+        Submit
+      </button>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+import DisplayName from '../form/fields/display-name.vue';
+import CommentBody from '../form/fields/comment-body.vue';
+
+export default {
+  /**
+   *
+   */
+  components: { DisplayName, CommentBody },
+
+  /**
+   *
+   */
+  props: {
+    displayName: {
+      type: String,
+      required: true,
+    },
+  },
+
+  /**
+   *
+   */
+  data: () => ({
+    blockName: 'idx-create-comment',
+    isLoading: false,
+    body: '',
+  }),
+
+  /**
+   *
+   */
+  methods: {
+    /**
+     *
+     */
+    handleSubmit() {
+
+    },
+  },
+};
+</script>
