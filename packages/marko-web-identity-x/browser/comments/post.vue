85d96b670d81b97524643b82bafc0f37a5ffa65a
Jacob Bare <jacob@limit0.io>
2020-03-10 07:53:43 -0500

    Create post component

---
+++ packages/marko-web-identity-x/browser/comments/post.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="blockName" :data-id="id">
+    <div :class="element('header')">
+      <div :class="element('display-name')">
+        Posted by {{ displayName }}
+      </div>
+      <div :class="element('created-at')">
+        {{ createdAt }}
+      </div>
+    </div>
+    <div :class="element('body')">
+      {{ body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   *
+   */
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Number,
+      required: true,
+    },
+    approved: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  /**
+   *
+   */
+  data: () => ({
+    blockName: 'idx-comment-post',
+  }),
+
+  /**
+   *
+   */
+  methods: {
+    /**
+     *
+     */
+    element(name) {
+      return `${this.blockName}__${name}`;
+    },
+  },
+};
+</script>
