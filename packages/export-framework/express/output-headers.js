edfe578690d51d371037cce7af76ab0d55b90fe2
Brian Miller <bmiller@southcomm.com>
2020-03-11 08:34:26 -0500

    move time stamp to front of file name
    previously it would do fueling-email-export_index.csv-1583933396133 no it will do 1583933396133

--- packages/export-framework/express/output-headers.js
+++ packages/export-framework/express/output-headers.js
@@ -5,7 +5,7 @@ module.exports = ({ coreConfig, format, name }) => (req, res, next) => {
   Object.keys(headers).forEach(k => res.set(k, headers[k]));
 
   const download = get(req, 'query.download');
-  if (typeof download !== 'undefined') res.set('Content-Disposition', `attachment; filename="${name}-${Date.now()}"`);
+  if (typeof download !== 'undefined') res.set('Content-Disposition', `attachment; filename="${Date.now()}-${name}"`);
 
   next();
 };
