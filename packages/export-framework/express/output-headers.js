cf3d655d87e66c8cd45eac7f99579cabc4fa1296
Brian Miller <bmiller@southcomm.com>
2020-03-11 15:05:39 -0500

    update date to readable format
    will now be 2020-03-11T19_52-categories_index.txt

--- packages/export-framework/express/output-headers.js
+++ packages/export-framework/express/output-headers.js
@@ -1,11 +1,13 @@
 const { get } = require('@base-cms/object-path');
+const moment = require('moment');
 
 module.exports = ({ coreConfig, format, name }) => (req, res, next) => {
   const headers = coreConfig.getAsObject(`fileExtensions.${format}`);
   Object.keys(headers).forEach(k => res.set(k, headers[k]));
 
   const download = get(req, 'query.download');
-  if (typeof download !== 'undefined') res.set('Content-Disposition', `attachment; filename="${Date.now()}-${name}"`);
+  const date = moment().format('YYYY-MM-DDTHH:mm');
+  if (typeof download !== 'undefined') res.set('Content-Disposition', `attachment; filename="${date}-${name}"`);
 
   next();
 };
