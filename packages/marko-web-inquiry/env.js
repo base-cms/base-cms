a3565643544a416420a46b25d331669d8a140f26
Jacob Bare <jacob@limit0.io>
2020-04-10 10:06:03 -0500

    Update sendgrid api env var description

--- packages/marko-web-inquiry/env.js
+++ packages/marko-web-inquiry/env.js
@@ -3,6 +3,6 @@ const { cleanEnv, validators, str } = require('@base-cms/env');
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
-  SENDGRID_API_KEY: nonemptystr({ desc: 'An API key for sending email via SendGrid.' }),
+  SENDGRID_API_KEY: nonemptystr({ desc: 'An API key for sending inquiry notifications via SendGrid.' }),
   SENDGRID_DEV_TO: str({ desc: 'An email address to send development submissions to.', default: '' }),
 });
