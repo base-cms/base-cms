24ea322f2549b9546b97624336db6603a902b41e
Jacob Bare <jacob@limit0.io>
2020-05-15 10:26:52 -0500

    Add newrelic config and env vars

--- services/omail-link-processor/src/env.js
+++ services/omail-link-processor/src/env.js
@@ -1,4 +1,5 @@
 const {
+  bool,
   cleanEnv,
   num,
   port,
@@ -8,6 +9,8 @@ const {
 module.exports = cleanEnv(process.env, {
   GRAPHQL_URI: str({ desc: 'The BaseCMS GraphQL URL.' }),
   EXPOSED_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
+  NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
+  NEW_RELIC_LICENSE_KEY: str({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
   PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
   TERMINUS_TIMEOUT: num({ desc: 'Number of milliseconds before forceful exiting.', default: 1000 }),
   TERMINUS_SHUTDOWN_DELAY: num({ desc: 'Number of milliseconds before the HTTP server starts its shutdown.', default: 10000 }),
