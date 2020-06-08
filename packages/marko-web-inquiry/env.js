f41dbd3e8ff84d5ae31cf54e3a1a44670fefd81f
Brian Miller <bmiller@southcomm.com>
2020-06-08 14:18:26 -0500

    add required env variables for recaptcha

--- packages/marko-web-inquiry/env.js
+++ packages/marko-web-inquiry/env.js
@@ -3,6 +3,9 @@ const { cleanEnv, validators, str } = require('@base-cms/env');
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
+  // these are the google recaptcha test keys
+  RECAPTCHA_SITE_KEY: str({ desc: 'An site key for sending recaptcha validation.', default: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' }),
+  RECAPTCHA_SECRET_KEY: str({ desc: 'A secret key for sending recaptcha validation.', default: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' }),
   SENDGRID_API_KEY: nonemptystr({ desc: 'An API key for sending inquiry notifications via SendGrid.' }),
   SENDGRID_DEV_TO: str({ desc: 'An email address to send development submissions to.', default: '' }),
 });
