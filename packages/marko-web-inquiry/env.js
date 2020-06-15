a08499adcc3c2a99e66268177ab2ee6e770dd7ca
Brian Miller <bmiller@southcomm.com>
2020-06-15 08:49:43 -0500

    set dev defaults instead of defaults

--- packages/marko-web-inquiry/env.js
+++ packages/marko-web-inquiry/env.js
@@ -4,8 +4,8 @@ const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
   // these are the google recaptcha test keys
-  RECAPTCHA_SITE_KEY: str({ desc: 'An site key for sending recaptcha validation.', default: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' }),
-  RECAPTCHA_SECRET_KEY: str({ desc: 'A secret key for sending recaptcha validation.', default: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' }),
+  RECAPTCHA_SITE_KEY: str({ desc: 'An site key for sending recaptcha validation.', devDefault: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' }),
+  RECAPTCHA_SECRET_KEY: str({ desc: 'A secret key for sending recaptcha validation.', devDefault: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' }),
   SENDGRID_API_KEY: nonemptystr({ desc: 'An API key for sending inquiry notifications via SendGrid.' }),
   SENDGRID_DEV_TO: str({ desc: 'An email address to send development submissions to.', default: '' }),
 });
