9cbc85d87161ab8924429b9404ad32e4553ec636
Brandon Krigbaum <brandonbk@gmail.com>
2020-06-15 07:09:41 -0500

    Add flipboard vue icon

---
+++ packages/marko-web-icons/browser/flipboard.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <icon-wrapper
+    name="flipboard"
+    :tag="tag"
+    :block-name="blockName"
+    :class-name="className"
+    :modifiers="modifiers"
+  >
+    <!-- eslint-disable-next-line -->
+    <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Flipboard icon</title><path d="M0 0v24h24V0H0zm19.2 9.6h-4.8v4.8H9.6v4.8H4.8V4.8h14.4v4.8z"/></svg>
+  </icon-wrapper>
+</template>
+
+<script>
+import IconWrapper from './_wrapper.vue';
+
+export default {
+  components: { IconWrapper },
+  props: {
+    blockName: {
+      type: String,
+      default: 'marko-web-icon',
+    },
+    className: {
+      type: String,
+      default: null,
+    },
+    modifiers: {
+      type: Array,
+      default: () => ([]),
+    },
+    tag: {
+      type: String,
+      default: 'span',
+    },
+  },
+};
+</script>
