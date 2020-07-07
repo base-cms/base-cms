aecd7e04aa76f8086724e159ac4caf645b32ab26
Alysha Buselt <>
2020-07-07 13:13:26 -0400

    Initial server js updates for campaigns

--- services/graphql-server/src/graphql/definitions/email/campaign.js
+++ services/graphql-server/src/graphql/definitions/email/campaign.js
@@ -8,4 +8,17 @@ type EmailCampaignTestRecipient {
   email: String
 }
 
+type GetCampaigns {
+  id: ObjectID! @projection(localField: "_id") @value(localField: "_id")
+  deploymentDate: Date
+}
+
+input GetCampaignsQueryInput {
+  id: ObjectID!
+}
+
+extend type Query {
+  getCampaigns(input: GetCampaignsQueryInput!): GetCampaigns!
+}
+
 `;
