999681dbd9ce35727bddd0415a93a0c7eef84903
Jeremy Laird <Jeremy.Laird@cygnus.com>
2020-07-23 09:24:12 -0500

    Fixed AWS ENV name

--- services/algolia-sync/src/components/algolia/message-sync.js
+++ services/algolia-sync/src/components/algolia/message-sync.js
@@ -4,11 +4,13 @@ const apollo = require('../graphql/query');
 const { buildSections, boostResult } = require('./helpers');
 const { ALGOLIA_APPID, ALGOLIA_API_KEY } = require('../../env');
 
+const { log } = console;
+
 const client = algoliasearch(ALGOLIA_APPID, ALGOLIA_API_KEY);
 
 const query = message => (gql`
   query {
-      content(input:{ id: ${message.id} }) {
+      content(input:{ id: ${message.id}, status: any }) {
         id
         type
         created
@@ -26,18 +28,24 @@ const query = message => (gql`
 `);
 
 const upsertToIndex = async (message) => {
+  log(`Proccessing: ${message.id}`)
   const c = await apollo.queryFromBase(query(message), message.tenant);
-  if (c.websiteSchedules) {
-    c.set(await buildSections(c));
+  try {
+    if (c.websiteSchedules) {
+      c.set(await buildSections(c));
+    }
+
+    c.boost = await boostResult(c);
+
+    const index = client.initIndex(message.tenant);
+    index.saveObject({
+      objectID: c.content.id,
+      ...c.content,
+    });
+  } catch (e) {
+    throw (`Failed on: ${message.id}`, e);
   }
 
-  c.boost = await boostResult(c);
-
-  const index = client.initIndex(message.tenant);
-  index.saveObject({
-    objectID: c.content.id,
-    ...c.content,
-  });
 };
 
 module.exports = { upsertToIndex };
