ea62b41430e39fa88a4a44599171b71cce1ca5d7
Josh Worden <josh@limit0.io>
2020-07-28 16:29:48 -0500

    Filter the username

--- packages/db/src/utils/filter-dsn.js
+++ packages/db/src/utils/filter-dsn.js
@@ -9,7 +9,8 @@ const { get } = require('@base-cms/object-path');
 const filter = (connection) => {
   const url = get(connection, 's.url');
   const pwd = get(connection, 's.options.password');
-  if (pwd) return `${url}`.replace(pwd, '*****');
+  const usr = get(connection, 's.options.user');
+  if (pwd || usr) return `${url}`.replace(usr, '*****').replace(pwd, '*****');
   return url;
 };
 
