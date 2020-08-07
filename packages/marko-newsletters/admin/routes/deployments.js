a0b694a3acc4b30cc92927759145a735520b64e7
Alysha Buselt <>
2020-08-07 11:29:58 -0400

    Removed old deployments file

--- packages/marko-newsletters/admin/routes/deployments.js
+++
@@ -1,6 +0,0 @@
-const template = require('../templates/deployments');
-
-module.exports = (app) => {
-  app.get('/deployments', (_, res) => { res.marko(template); });
-  app.get('/deployments/*', (_, res) => { res.marko(template); });
-};
