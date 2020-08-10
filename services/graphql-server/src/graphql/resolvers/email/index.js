09733ea9aa6e26ca6a08ba981d2b095536bcafcd
Alysha Buselt <>
2020-08-10 16:58:16 -0400

    Removed referenced to unneeded file

--- services/graphql-server/src/graphql/resolvers/email/index.js
+++ services/graphql-server/src/graphql/resolvers/email/index.js
@@ -1,9 +1,7 @@
 const deepAssign = require('deep-assign');
 
 const schedule = require('./schedule');
-const campaign = require('./campaign');
 
 module.exports = deepAssign(
   schedule,
-  campaign,
 );
