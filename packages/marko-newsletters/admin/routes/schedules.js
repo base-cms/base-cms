2b5bd204a8a42315a1ecac001e89c59c5241e6a1
Alysha Buselt <>
2020-08-10 16:57:46 -0400

    Updated other campaign references

--- packages/marko-newsletters/admin/routes/schedules.js
+++ packages/marko-newsletters/admin/routes/schedules.js
@@ -1,15 +1,15 @@
 const { asyncRoute } = require('@base-cms/utils');
 const template = require('../templates/schedules');
 // const mapSchedules = require('../../utils/map-schedules');
-const { campaignList } = require('../../temp-db');
+
 
 module.exports = (router) => {
   router.get('/schedules', (_, res) => { res.marko(template); });
   router.get('/schedules/*', asyncRoute(async (_, res) => {
     // const { apollo } = res.locals;
 
-    // const { campaignList } = await mapSchedules(apollo);
+    // const { scheduleList } = await mapSchedules(apollo);
 
-    res.marko(template, { campaignList });
+    res.marko(template, { scheduleList });
   }));
 };
