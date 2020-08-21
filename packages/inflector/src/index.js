2b0e5843a2321984547d7f5fe5d646a3c4b449ab
Brandon Krigbaum <brandonbk@gmail.com>
2020-08-21 08:39:27 -0500

    Add pluralize to inflector

--- packages/inflector/src/index.js
+++ packages/inflector/src/index.js
@@ -1,11 +1,13 @@
 const camelize = require('./camelize');
 const dasherize = require('./dasherize');
+const pluralize = require('./pluralize');
 const titleize = require('./titleize');
 const underscore = require('./underscore');
 
 module.exports = {
   camelize,
   dasherize,
+  pluralize,
   titleize,
   underscore,
 };
