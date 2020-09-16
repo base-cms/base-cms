1bba344a5d541d1bfcd7a25026443d7c737a6723
Brian Miller <bmiller@southcomm.com>
2020-09-16 11:35:01 -0500

    Add alphaThemeConfigs to newsletter templateRouter
    alphaThemeConfigs {
      edges {
        node {
          id
          status
          accentFontColor
          headerLink
          socialIcons
          facebook
          linkedin
          twitter
          youtube
          instagram
          pinterest
          headerBgColor
          headerTextColor
          headerTemplate
          dateToggle
          footerColor
          footerTextColor
          manageSubscriptions
          contactUs
          advertise
          headerLeft {
            id
            src
          }
          headerRight {
            id
            src
          }
        }
      }
    }

--- packages/marko-newsletters/express/template-router.js
+++ packages/marko-newsletters/express/template-router.js
@@ -23,6 +23,40 @@ query WithMarkoNewsletter($input: EmailNewsletterAliasQueryInput!) {
     site {
       ...MarkoWebsiteContextFragment
     }
+    alphaThemeConfigs {
+      edges {
+        node {
+          id
+          status
+          accentFontColor
+          headerLink
+          socialIcons
+          facebook
+          linkedin
+          twitter
+          youtube
+          instagram
+          pinterest
+          headerBgColor
+          headerTextColor
+          headerTemplate
+          dateToggle
+          footerColor
+          footerTextColor
+          manageSubscriptions
+          contactUs
+          advertise
+          headerLeft {
+            id
+            src
+          }
+          headerRight {
+            id
+            src
+          }
+        }
+      }
+    }
   }
 }
 
