4777da070f4aed013101e691d8463cc80df93191
Jeremy Laird <Jeremy.Laird@cygnus.com>
2020-10-28 08:33:46 -0500

    Fixed definitions formatting

--- services/graphql-server/src/graphql/definitions/email/campaign.js
+++ services/graphql-server/src/graphql/definitions/email/campaign.js
@@ -1,80 +1,82 @@
 const gql = require('graphql-tag');
 
 module.exports = gql`
-  extend type Query {
-    emailCampaign(input: EmailCampaignQueryInput!): EmailCampaign
-      @findOne(model: "email.Campaign", using: { id: "_id" })
-    emailCampaigns(input: EmailCampaignQueryInput!): EmailCampaignConnection
-  }
 
-  input EmailCampaignQueryInput {
-    status: ModelStatus = active
-    id: ObjectID
-    productId: ObjectID
-    sort: EmailCampaignsSortInput = {}
-    pagination: PaginationInput = {}
-    beginning: CampaignBeginningInput = {}
-    ending: CampaignEndingInput = {}
-  }
+extend type Query {
+  emailCampaign(input: EmailCampaignQueryInput!): EmailCampaign @findOne(
+    model: "email.Campaign",
+    using: { id: "_id" }
+  )
+  emailCampaigns(input: EmailCampaignQueryInput!): EmailCampaignConnection
+}
 
-  input CampaignBeginningInput {
-    before: Date
-    after: Date
-  }
+type EmailCampaignConnection @projectUsing(type: "EmailCampaign") {
+  totalCount: Int!
+  edges: [EmailCampaignEdge]!
+  pageInfo: PageInfo!
+}
 
-  input CampaignEndingInput {
-    before: Date
-    after: Date
-  }
+type EmailCampaignEdge {
+  node: EmailCampaign!
+  cursor: String!
+}
 
-  enum EmailCampaignsSortField {
-    status
-    deploymentDate
-    scheduled
-  }
+type EmailCampaign {
+  id: ObjectID! @projection(localField: "_id") @value(localField: "_id")
+  status: Int @projection
+  name: String @projection
+  created: Date @projection
+  touched: Date @projection
+  updated: Date @projection
+  createdBy: Date @projection
+  deploymentDate: Date @projection
+  scheduled: Date @projection
+  htmlDate: Date @projection
+  externalId: String @projection
+  fromName: String @projection
+  html: String @projection
+  listId: Int @projection
+  listStatus: String @projection
+  locked: String @projection
+  subjectLine: String @projection
+  listMessage: String @projection
+  tests: [EmailCampaignTestRecipient] @projection
+}
 
-  input EmailCampaignsSortInput {
-    field: EmailCampaignsSortField = deploymentDate
-    order: SortOrder = desc
-  }
+type EmailCampaignTestRecipient {
+  firstName: String
+  lastName: String
+  email: String
+}
 
-  type EmailCampaignConnection @projectUsing(type: "EmailCampaign") {
-    totalCount: Int!
-    edges: [EmailCampaignEdge]!
-    pageInfo: PageInfo!
-  }
+enum EmailCampaignsSortField {
+  status
+  deploymentDate
+  scheduled
+}
 
-  type EmailCampaignEdge {
-    node: EmailCampaign!
-    cursor: String!
-  }
+input EmailCampaignQueryInput {
+  status: ModelStatus = active
+  id: ObjectID
+  productId: ObjectID
+  sort: EmailCampaignsSortInput = {}
+  pagination: PaginationInput = {}
+  beginning: CampaignBeginningInput = {}
+  ending: CampaignEndingInput = {}
+}
 
-  type EmailCampaign {
-    id: ObjectID! @projection(localField: "_id") @value(localField: "_id")
-    status: Int @projection
-    name: String @projection
-    created: Date @projection
-    touched: Date @projection
-    updated: Date @projection
-    createdBy: Date @projection
-    deploymentDate: Date @projection
-    scheduled: Date @projection
-    htmlDate: Date @projection
-    externalId: String @projection
-    fromName: String @projection
-    html: String @projection
-    listId: Int @projection
-    listStatus: String @projection
-    locked: String @projection
-    subjectLine: String @projection
-    listMessage: String @projection
-    tests: [EmailCampaignTestRecipient] @projection
-  }
+input CampaignBeginningInput {
+  before: Date
+  after: Date
+}
 
-  type EmailCampaignTestRecipient {
-    firstName: String
-    lastName: String
-    email: String
-  }
+input CampaignEndingInput {
+  before: Date
+  after: Date
+}
 
+input EmailCampaignsSortInput {
+  field: EmailCampaignsSortField = deploymentDate
+  order: SortOrder = desc
+}
 `;
