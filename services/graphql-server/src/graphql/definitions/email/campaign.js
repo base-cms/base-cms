390835717080a3f5bd585998f8c96ec2267d3115
Jeremy Laird <Jeremy.Laird@cygnus.com>
2020-10-28 12:14:44 -0500

    Fixed spacing

--- services/graphql-server/src/graphql/definitions/email/campaign.js
+++ services/graphql-server/src/graphql/definitions/email/campaign.js
@@ -1,74 +1,75 @@
 const gql = require('graphql-tag');
 
 module.exports = gql`
-  extend type Query {
-    emailCampaign(input: EmailCampaignQueryInput!): EmailCampaign
-      @findOne(model: "email.Campaign", using: { id: "_id" })
-    emailCampaigns(input: EmailCampaignsQuerysInput!): EmailCampaignConnection!
-  }
 
-  type EmailCampaignConnection @projectUsing(type: "EmailCampaign") {
-    totalCount: Int!
-    edges: [EmailCampaignEdge]!
-    pageInfo: PageInfo!
-  }
+extend type Query {
+  emailCampaign(input: EmailCampaignQueryInput!): EmailCampaign
+    @findOne(model: "email.Campaign", using: { id: "_id" })
+  emailCampaigns(input: EmailCampaignsQuerysInput!): EmailCampaignConnection!
+}
 
-  type EmailCampaignEdge {
-    node: EmailCampaign!
-    cursor: String!
-  }
+type EmailCampaignConnection @projectUsing(type: "EmailCampaign") {
+  totalCount: Int!
+  edges: [EmailCampaignEdge]!
+  pageInfo: PageInfo!
+}
 
-  type EmailCampaign {
-    id: ObjectID! @projection(localField: "_id") @value(localField: "_id")
-    status: Int @projection
-    name: String @projection
-    created: Date @projection
-    touched: Date @projection
-    updated: Date @projection
-    createdBy: User @projection @refOne(loader: "platformUser")
-    deploymentDate: Date @projection
-    scheduled: Date @projection
-    htmlDate: Date @projection
-    externalId: String @projection
-    fromName: String @projection
-    html: String @projection
-    listId: String @projection
-    listStatus: String @projection
-    locked: Boolean @projection
-    subjectLine: String @projection
-    listMessage: String @projection
-  }
+type EmailCampaignEdge {
+  node: EmailCampaign!
+  cursor: String!
+}
 
-  type EmailCampaignTestRecipient {
-    firstName: String
-    lastName: String
-    email: String
-  }
+type EmailCampaign {
+  id: ObjectID! @projection(localField: "_id") @value(localField: "_id")
+  status: Int @projection
+  name: String @projection
+  created: Date @projection
+  touched: Date @projection
+  updated: Date @projection
+  createdBy: User @projection @refOne(loader: "platformUser")
+  deploymentDate: Date @projection
+  scheduled: Date @projection
+  htmlDate: Date @projection
+  externalId: String @projection
+  fromName: String @projection
+  html: String @projection
+  listId: String @projection
+  listStatus: String @projection
+  locked: Boolean @projection
+  subjectLine: String @projection
+  listMessage: String @projection
+}
 
-  enum EmailCampaignsSortField {
-    status
-    deploymentDate
-    scheduled
-  }
+type EmailCampaignTestRecipient {
+  firstName: String
+  lastName: String
+  email: String
+}
 
-  input EmailCampaignQueryInput {
-    id: ObjectID
-    status: ModelStatus = active
-    productId: ObjectID
-  }
+enum EmailCampaignsSortField {
+  status
+  deploymentDate
+  scheduled
+}
 
-  input EmailCampaignsQuerysInput {
-    id: ObjectID
-    status: ModelStatus = active
-    productId: ObjectID
-    sort: EmailCampaignsSortInput = {}
-    pagination: PaginationInput = {}
-    scheduledAfter: Date
-    scheduledBefore: Date
-  }
+input EmailCampaignQueryInput {
+  id: ObjectID
+  status: ModelStatus = active
+  productId: ObjectID
+}
 
-  input EmailCampaignsSortInput {
-    field: EmailCampaignsSortField = scheduled
-    order: SortOrder = desc
-  }
+input EmailCampaignsQuerysInput {
+  id: ObjectID
+  status: ModelStatus = active
+  productId: ObjectID
+  sort: EmailCampaignsSortInput = {}
+  pagination: PaginationInput = {}
+  scheduledAfter: Date
+  scheduledBefore: Date
+}
+
+input EmailCampaignsSortInput {
+  field: EmailCampaignsSortField = scheduled
+  order: SortOrder = desc
+}
 `;
