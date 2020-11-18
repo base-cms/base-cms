0813112c37e500fa8e5dd38dd72dec183d3f35db
Brian Miller <bmiller@southcomm.com>
2020-11-18 14:34:09 -0600

    add ability to pull newsletter campaings
     - adjust map-templates to pull campaings with given newsletters
     - update layout to account for display of deployment list and latest link

--- packages/marko-newsletters/utils/map-templates.js
+++ packages/marko-newsletters/utils/map-templates.js
@@ -1,5 +1,6 @@
 const { getAsArray } = require('@base-cms/object-path');
 const gql = require('graphql-tag');
+const getCampaigns = require('./get-campaigns');
 
 const query = gql`
 
@@ -24,11 +25,21 @@ query MarkoNewslettersList {
 
 module.exports = async (apollo, { templates }) => {
   const { data } = await apollo.query({ query });
-  const newsletters = getAsArray(data, 'emailNewsletters.edges').map((edge) => {
+  const newsletters = await Promise.all(getAsArray(data, 'emailNewsletters.edges').map(async (edge) => {
     const node = { ...edge.node };
+    const now = new Date();
     node.templates = templates.filter(t => t.alias === node.alias).map(t => t.key);
+    node.campaigns = await getCampaigns(
+      apollo,
+      {
+        productId: node.id,
+        scheduledBefore: now.getTime(),
+        scheduledAfter: now.getTime() - (52 * 7 * 24 * 3600000),
+        limit: 365,
+      },
+    );
     return node;
-  });
+  }));
 
   const aliases = newsletters.map(n => n.alias);
   const staticTemplates = templates.filter(t => !aliases.includes(t.alias)).map(t => t.key);
